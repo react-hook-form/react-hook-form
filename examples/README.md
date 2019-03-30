9aec227e74b0210606f1cc5573d169bde5139031
Beier Luo <bluebill1049@hotmail.com>
2019-03-31 08:10:57 +1100

    include example for inital form value

--- examples/README.md
+++ examples/README.md
@@ -10,6 +10,7 @@ you can view the source code within the folder or visit code sand box to see how
 | Conditional Fields      | https://codesandbox.io/s/13ykqx4wx7 |
 | Async Submit Validation | https://codesandbox.io/s/xrjv48o0qp |
 | Async Field Validation  | https://codesandbox.io/s/m5pj55yj7x |
+| initial form value      | https://codesandbox.io/s/l3mxpvmm9  |
 | ValidationSchema        | https://codesandbox.io/s/928po918qr |
 | Validation On Blur      | https://codesandbox.io/s/w7p3km6nyw |
 | Validation On Change    | https://codesandbox.io/s/74zw1oqozx |
---
+++ examples/initalFormState.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import useForm from "react-hook-form";
+
+function App() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => {
+    alert(JSON.stringify(data));
+  };
+  const intialValues = {
+    firstName: "bill",
+    lastName: "luo",
+    email: "bluebill1049@hotmail.com"
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            defaultValue={intialValues.firstName}
+            name="firstName"
+            placeholder="bill"
+            ref={register}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            defaultValue={intialValues.lastName}
+            name="lastName"
+            placeholder="luo"
+            ref={register}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            defaultValue={intialValues.email}
+            name="email"
+            placeholder="bluebill1049@hotmail.com"
+            type="email"
+            ref={register}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
