053c6c0f5b60be6b40d05f0e33bddd1eb3eb8a0f
bill <bluebill1049@hotmail.com>
2019-04-03 15:00:44 +1100

    remove src code at website

--- website/src/src/logic/removeAllEventListeners.ts
+++
@@ -1,5 +0,0 @@
-export default (ref, validateWithStateUpdate): void => {
-  ref.removeEventListener('input', validateWithStateUpdate);
-  ref.removeEventListener('change', validateWithStateUpdate);
-  ref.removeEventListener('blur', validateWithStateUpdate);
-};
