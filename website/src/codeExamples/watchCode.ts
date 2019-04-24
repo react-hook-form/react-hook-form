c01963e4a11bfd7f9756af33be87cff7641889de
Beier Luo <bluebill1049@hotmail.com>
2019-04-25 08:24:51 +1000

    update code comment

--- website/src/codeExamples/watchCode.ts
+++ website/src/codeExamples/watchCode.ts
@@ -4,9 +4,9 @@ import useForm from 'react-hook-form'
 
 function YourForm(props) {
   const { register, watch } = useForm()
-  const watchYes = watch('yes', props.yes) // you can supply default value as second argument
-  const watchAllFields = watch() // when pass nothing as argument, you are watching everything
-  const watchFields = watch(['yes', 'number']) // you can also target specific fields by their names
+  const watchYes = watch('yes', props.yes) // supply default value as second argument
+  const watchAllFields = watch() // watching every fields in the form
+  const watchFields = watch(['yes', 'number']) // target specific fields by their names
   
   return (
     <form>
