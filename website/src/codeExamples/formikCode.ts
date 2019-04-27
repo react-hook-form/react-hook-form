112d9afcf949d28e8e92a4381c0a001a04c4459f
Beier Luo <bluebill1049@hotmail.com>
2019-04-27 20:13:19 +1000

    update formik code example

--- website/src/codeExamples/formikCode.ts
+++ website/src/codeExamples/formikCode.ts
@@ -3,19 +3,23 @@ import { Formik, Form, Field } from "formik";
 
 function validateEmail(value) {
   let error;
+  
   if (!value) {
     error = "Required";
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$/i.test(value)) {
     error = "Invalid email address";
   }
+  
   return error;
 }
 
 function validateUsername(value) {
   let error;
+  
   if (value === "admin") {
     error = "Nice try!";
   }
+  
   return error;
 }
 
