f09fbc1a85c82e06672383eccd8efbecff747fd5
bill <bluebill1049@hotmail.com>
2019-05-05 10:52:56 +1000

    update website for errors object table

--- website/src/Api.tsx
+++ website/src/Api.tsx
@@ -312,6 +312,46 @@ function Builder({ formData, showApi, toggleApi, apiButton, isMobile }: any) {
                     </h2>
                   </code>
                   <p>Object contain form errors or error messages belong to each input.</p>
+
+                  <TableWrapper>
+                    <Table>
+                      <tbody>
+                      <tr>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Description</th>
+                      </tr>
+                      <tr>
+                        <td>
+                          <code>type</code>
+                        </td>
+                        <td>
+                          <Type>string</Type>
+                        </td>
+                        <td>Error type which according to your validation rules. eg: required, min, max </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <code>message</code>
+                        </td>
+                        <td>
+                          <Type>string</Type>
+                        </td>
+                        <td>Register with validation and error message, then error message will return in this .</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <code>ref</code>
+                        </td>
+                        <td>
+                          <Type>React.RefObject</Type>
+                        </td>
+                        <td>Reference fo your error input element.</td>
+                      </tr>
+                      </tbody>
+                    </Table>
+                  </TableWrapper>
+
                   <SyntaxHighlighterWithCopy tabIndex={tabIndex} rawData={errorCode} url="https://codesandbox.io/s/nrr4n9p8n4" />
 
                   <hr />
