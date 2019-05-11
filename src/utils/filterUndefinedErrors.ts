c795ab1739cf6b660099a4bd71850ef543125883
Beier Luo <bluebill1049@hotmail.com>
2019-05-11 12:40:20 +1000

    fix a type warning

--- src/utils/filterUndefinedErrors.ts
+++ src/utils/filterUndefinedErrors.ts
@@ -1,7 +1,7 @@
 import { ErrorMessages } from '../types';
 
 export default (errors: ErrorMessages): ErrorMessages =>
-  Object.entries(errors).reduce((previous, [key, value]) => {
+  Object.entries(errors).reduce((previous, [key, value]): ErrorMessages => {
     if (value.type) previous[key] = value;
     return previous;
   }, {});
