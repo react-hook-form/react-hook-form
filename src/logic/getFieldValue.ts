746e04e1f03535dd273bae6979ad9f6183d2b001
Abi Hafshin <abi@hafs.in>
2019-05-26 13:47:05 +0700

    Add generic type to FieldsObject related methods

--- src/logic/getFieldValue.ts
+++ src/logic/getFieldValue.ts
@@ -1,10 +1,10 @@
 import getRadioValue from './getRadioValue';
 import getMultipleSelectValue from './getMultipleSelectValue';
-import { FieldsObject, FieldValue, Ref } from '../types';
+import { FieldsObject, FieldValue, Ref, DataType } from '../types';
 import isRadioInput from '../utils/isRadioInput';
 import isCheckBox from '../utils/isCheckBoxInput';
 
-export default function getFieldValue(fields: FieldsObject, { type, name, options, checked, value }: Ref): FieldValue {
+export default function getFieldValue<Data extends DataType>(fields: FieldsObject<Data>, { type, name, options, checked, value }: Ref): FieldValue {
   if (isRadioInput(type)) return getRadioValue(fields[name].options).value;
 
   if (type === 'select-multiple') return getMultipleSelectValue(options);
