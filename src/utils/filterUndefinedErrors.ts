6070cda16add9d0c33a09fe2fc8b82db25aca585
Beier Luo <bluebill1049@hotmail.com>
2019-06-02 22:03:18 +1000

    remove ts-ignore

--- src/utils/filterUndefinedErrors.ts
+++ src/utils/filterUndefinedErrors.ts
@@ -1,8 +1,7 @@
 import { ErrorMessages, DataType } from '../types';
 
 export default <Data extends DataType>(errors: ErrorMessages<Data>): ErrorMessages<Data> =>
-  // @ts-ignore
   Object.entries(errors).reduce((previous: ErrorMessages<Data>, [key, value]): ErrorMessages<Data> => {
-    if (value.type) previous[key] = value;
+    if (value && value.type) previous[key] = value;
     return previous;
   }, {});
