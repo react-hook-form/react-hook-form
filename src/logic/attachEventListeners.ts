3d8cbd9fb7d54b9364370e17cd37344c1df2f061
ilya.tychinin <elive@mail.ru>
2019-06-12 14:35:24 +0500

    feature: not add input listener onBlur mode.

--- src/logic/attachEventListeners.ts
+++ src/logic/attachEventListeners.ts
@@ -2,16 +2,21 @@ import { Field } from '../types';
 import isCheckBoxInput from '../utils/isCheckBoxInput';
 
 export default function attachEventListeners({
+  isOnBlurMode,
   field,
   validateAndStateUpdate,
   isRadio,
 }: {
+  isOnBlurMode: boolean;
   field: Field;
   isRadio: boolean;
   validateAndStateUpdate: Function | undefined;
 }): void {
   const { ref } = field;
   if (!ref.addEventListener) return;
-  ref.addEventListener(isCheckBoxInput(ref.type) || isRadio ? 'change' : 'input', validateAndStateUpdate);
+  if (!isOnBlurMode)
+    ref.addEventListener(
+      isCheckBoxInput(ref.type) || isRadio ? 'change' : 'input', validateAndStateUpdate
+    );
   ref.addEventListener('blur', validateAndStateUpdate);
 }
