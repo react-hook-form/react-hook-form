324a9e1376169a7d001b0b4d5e6da26dc16308cf
bill <bluebill1049@hotmail.com>
2019-06-12 12:19:03 +1000

    first commit

--- src/logic/combineFieldValues.ts
+++ src/logic/combineFieldValues.ts
@@ -1,19 +1,31 @@
 import { FieldValue } from '../types';
 
 export default function combineFieldValues(data): FieldValue {
-  const output = Object.entries(data).reduce((previous, [key, value]): FieldValue => {
-    const arrayIndex = key.match(/\[\d+\]$/gi);
+  const output = Object.entries(data).reduce(
+    (previous, [key, value]): FieldValue => {
+      const arrayIndex = key.match(/\[\d+\]$/gi);
 
-    if (arrayIndex) {
-      const index = arrayIndex[0].slice(1, -1);
-      const filedName = key.substr(0, key.indexOf('['));
-      if (!previous[filedName]) previous[filedName] = [];
-      previous[filedName][index] = value;
-    } else {
-      previous[key] = value;
-    }
-    return previous;
-  }, {});
+      if (arrayIndex) {
+        const index = arrayIndex[0].slice(1, -1);
+        const filedName = key.substr(0, key.indexOf('['));
+        if (!previous[filedName]) previous[filedName] = [];
+        previous[filedName][index] = value;
+      } else if (key.startsWith('{') && key.endsWith('}')) {
+        try {
+          const obj = JSON.parse(key);
+          const objKey = obj[0];
+          const objValue = obj[objKey];
+          previous[objKey][objValue] = value;
+        } catch (e) {
+          console.error(`Fields name paring error: ${e}`);
+        }
+      } else {
+        previous[key] = value;
+      }
+      return previous;
+    },
+    {},
+  );
 
   return Object.entries(output).reduce((previous, [key, value]): FieldValue => {
     if (Array.isArray(value)) {
