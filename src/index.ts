c76b9c3a708595e01ce9886d815421bc502612ca
Ramzan Chasygov <theliketurbo@gmail.com>
2019-06-15 21:14:19 +0300

    updated formState api

--- src/index.ts
+++ src/index.ts
@@ -475,6 +475,7 @@ export default function useForm<Data extends DataType>(
       submitCount: submitCountRef.current,
       touched: touchedFieldsRef.current,
       isSubmitting: isSubmittingRef.current,
+      isValid: isEmptyObject(errorsRef.current),
     },
   };
 }
