e7ce78bc17957806350f6e300e70a17052276b41
Beier Luo <bluebill1049@hotmail.com>
2019-06-15 20:15:34 +1000

    fix with babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,16 @@
 import typescript from 'rollup-plugin-typescript2';
+import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/index.ts',
   plugins: [
     typescript(),
+    babel({
+      exclude: 'node_modules/**',
+      extensions: ['.tsx', '.ts'],
+    }),
   ],
-  external: ['react'],
+  external: ['react', 'react-dom'],
   output: [
     {
       file: 'dist/index.js',
