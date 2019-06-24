2a78fa2000582124752ceb55358833fa3223cc7f
bill <bluebill1049@hotmail.com>
2019-06-24 16:03:32 +1000

    add type for displayNativeError.ts

--- src/logic/displayNativeError.ts
+++ src/logic/displayNativeError.ts
@@ -1,5 +1,6 @@
 import isString from "../utils/isString";
+import {Ref} from "../types";
 
-export default function displayNativeError(nativeValidation, ref, result) {
+export default function displayNativeError(nativeValidation: boolean, ref: Ref, result: string) {
   if (nativeValidation && isString(result)) ref.setCustomValidity(result);
 }
