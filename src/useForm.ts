410f2fcef760af27e6ce82a1e0e42af8d0d1761b
Barry <barrymay@Barrys-MacBook-Pro.local>
2019-06-29 00:51:21 -0400

    Stronger typing for GetValues, whcih should be based on the Data type used in the form

--- src/useForm.ts
+++ src/useForm.ts
@@ -493,7 +493,7 @@ export default function useForm<Data extends DataType>(
     reRenderForm({});
   };
 
-  const getValues = (): FormData => getFieldsValues(fieldsRef.current);
+  const getValues = (): Data => getFieldsValues<Data>(fieldsRef.current);
 
   useEffect((): VoidFunction => {
     return () => {
