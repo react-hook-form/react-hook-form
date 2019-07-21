cb9fbb7103638f31d20f64e94028df0c2b939c0a
Beier Luo <bluebill1049@hotmail.com>
2019-07-21 22:22:43 +1000

    code clean up

--- src/utils/flatArray.ts
+++ src/utils/flatArray.ts
@@ -1,8 +1,6 @@
-function flatArray<T>(list: T[]): T[] {
+export default function flatArray<T>(list: T[]): T[] {
   return list.reduce(
     (a: any, b: any) => a.concat(Array.isArray(b) ? flatArray(b) : b),
     [],
   );
 }
-
-export default flatArray;
--- src/utils/getPath.ts
+++ src/utils/getPath.ts
@@ -4,25 +4,23 @@ import isObject from './isObject';
 import { DataType } from '../types';
 
 function getPath(path: string, value: DataType | string[] | string): any {
-  if (Array.isArray(value)) {
-    return value.map((item, index) => {
-      const pathWithIndex = `${path}[${index}]`;
-      if (Array.isArray(item)) {
-        return getPath(pathWithIndex, item);
-      } else if (isObject(item)) {
-        return Object.entries(item).map(([key, objectValue]) =>
-          isString(objectValue)
-            ? `${pathWithIndex}.${key}`
-            : getPath(`${pathWithIndex}.${key}`, objectValue),
-        );
-      }
-      return `${path}[${index}]`;
-    });
-  }
-
-  return Object.entries(value).map(([key, objectValue]) =>
-    isString(objectValue) ? `${path}.${key}` : getPath(path, objectValue),
-  );
+  return Array.isArray(value)
+    ? value.map((item, index) => {
+        const pathWithIndex = `${path}[${index}]`;
+        if (Array.isArray(item)) {
+          return getPath(pathWithIndex, item);
+        } else if (isObject(item)) {
+          return Object.entries(item).map(([key, objectValue]) =>
+            isString(objectValue)
+              ? `${pathWithIndex}.${key}`
+              : getPath(`${pathWithIndex}.${key}`, objectValue),
+          );
+        }
+        return `${path}[${index}]`;
+      })
+    : Object.entries(value).map(([key, objectValue]) =>
+        isString(objectValue) ? `${path}.${key}` : getPath(path, objectValue),
+      );
 }
 
 export default (parentPath: string, value: any) =>
