f7168d575b95c3bd09e1400c84b0cf3e6b64910b
Beier Luo <bluebill1049@hotmail.com>
2019-07-21 21:58:34 +1000

    clean up getpath

--- src/utils/getPath.ts
+++ src/utils/getPath.ts
@@ -2,30 +2,27 @@ import flatten from './flatten';
 import isString from './isString';
 import isObject from './isObject';
 
-function getPath(parentPath: string, value: any): any {
+function getPath(path: string, value: any): any {
   if (Array.isArray(value)) {
     return value.map((item, index) => {
+      const pathWithIndex = `${path}[${index}]`;
       if (Array.isArray(item)) {
-        return getPath(`${parentPath}[${index}]`, item);
+        return getPath(pathWithIndex, item);
       } else if (isObject(item)) {
-        return Object.entries(item).map(([key, value]) => {
-          return isString(value)
-            ? `${parentPath}[${index}].${key}`
-            : getPath(`${parentPath}[${index}].${key}`, value);
-        });
+        return Object.entries(item).map(([key, objectValue]) =>
+          isString(objectValue)
+            ? `${pathWithIndex}.${key}`
+            : getPath(`${pathWithIndex}.${key}`, objectValue),
+        );
       }
-
-      return `${parentPath}[${index}]`;
+      return `${path}[${index}]`;
     });
   } else if (isObject(value)) {
-    return Object.entries(value).map(([key, value]) => {
-      return isString(value)
-        ? `${parentPath}.${key}`
-        : getPath(parentPath, value);
-    });
+    return Object.entries(value).map(([key, objectValue]) =>
+      isString(objectValue) ? `${path}.${key}` : getPath(path, objectValue),
+    );
   }
 }
 
-export default (parentPath: string, value: any) => {
-  return flatten(getPath(parentPath, value));
-};
+export default (parentPath: string, value: any) =>
+  flatten(getPath(parentPath, value));
