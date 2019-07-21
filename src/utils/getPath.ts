88bbf518253f75488ea05f1df5fbd0295ac43d49
Beier Luo <bluebill1049@hotmail.com>
2019-07-21 21:54:38 +1000

    clean up getpath

--- src/utils/getPath.ts
+++ src/utils/getPath.ts
@@ -1,27 +1,27 @@
 import flatten from './flatten';
+import isString from './isString';
+import isObject from './isObject';
 
 function getPath(parentPath: string, value: any): any {
   if (Array.isArray(value)) {
     return value.map((item, index) => {
       if (Array.isArray(item)) {
         return getPath(`${parentPath}[${index}]`, item);
-      } else if (typeof item === 'object') {
+      } else if (isObject(item)) {
         return Object.entries(item).map(([key, value]) => {
-          if (typeof value !== 'string') {
-            return getPath(`${parentPath}[${index}].${key}`, value);
-          }
-          return `${parentPath}[${index}].${key}`;
+          return isString(value)
+            ? `${parentPath}[${index}].${key}`
+            : getPath(`${parentPath}[${index}].${key}`, value);
         });
       }
 
       return `${parentPath}[${index}]`;
     });
-  } else if (typeof value === 'object') {
+  } else if (isObject(value)) {
     return Object.entries(value).map(([key, value]) => {
-      if (typeof value !== 'string') {
-        return getPath(parentPath, value);
-      }
-      return `${parentPath}.${key}`;
+      return isString(value)
+        ? `${parentPath}.${key}`
+        : getPath(parentPath, value);
     });
   }
 }
