4c691f7dd1a9b1d1d7052d2c22591bcc315d4bde
Chris Parton <chrisparton1991@gmail.com>
2019-07-25 09:44:00 +1000

    Fix min and max value validation when min/max is 0

--- src/logic/getValueAndMessage.test.ts
+++ src/logic/getValueAndMessage.test.ts
@@ -2,7 +2,9 @@ import getValueAndMessage from './getValueAndMessage';
 
 describe('getValueAndMessage', () => {
   it('should return message and value correctly', () => {
+    expect(getValueAndMessage(0).value).toEqual(0);
     expect(getValueAndMessage(3).value).toEqual(3);
+    expect(getValueAndMessage({ value: 0 }).value).toEqual(0);
     expect(getValueAndMessage({ value: 2 }).value).toEqual(2);
     expect(getValueAndMessage({ message: 'test' }).message).toEqual('test');
   });
