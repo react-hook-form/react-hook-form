5eb3179ee7531fe3cdb4b63fb3c02416b3242319
Barry <5514034+barrymay@users.noreply.github.com>
2019-07-25 17:39:12 -0400

    Add reasonable auto-defaults for vscode usage, including enabling for eslint plugin (#151)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
