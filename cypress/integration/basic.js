8367cfa5df45a49a0f96f90779a99f5dec790d5d
bluebill1049 <bluebill1049@hotmail.com>
2019-07-30 13:46:57 +1000

    working on basic

--- cypress/integration/basic.js
+++ cypress/integration/basic.js
@@ -1,5 +1,9 @@
 context('basic', () => {
   it('should validate basic form', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000');
+    cy.get('button').click();
+
+    cy.get('p.firstName').contains('firstName error');
+    cy.get('p.lastName').contains('lastName error');
   });
 });
