13848e9a602a00abd7d8f4bff2253d0ba0759c87
Beier Luo <bluebill1049@hotmail.com>
2019-08-03 17:28:10 +1000

    include basicSchemaValidation automation

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Basic from './basic';
 import Watch from './watch';
+import basicSchemaValidation from './basicSchemaValidation';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Route path="/" exact component={Basic} />
       <Route path="/watch" exact component={Watch} />
+      <Route path="/basicSchemaValidation" exact component={basicSchemaValidation} />
     </Router>
   );
 };
