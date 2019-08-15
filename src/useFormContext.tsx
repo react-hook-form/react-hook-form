9ce4eb1e33129721507278946193c1cab1be0faf
Michael Stramel <mstramel@coolfiresolutions.com>
2019-08-15 09:48:03 -0500

    Test casting instead of ignoring

--- src/useFormContext.tsx
+++ src/useFormContext.tsx
@@ -1,23 +1,18 @@
 import * as React from 'react';
 import { FormContextValues, FormProps, DataType } from './types';
 
-type FormContextType<T extends DataType = DataType> = FormContextValues<T>;
-
-export const FormGlobalContext = React.createContext<FormContextType | null>(
-  null,
-);
+const FormGlobalContext = React.createContext<FormContextValues<
+  DataType
+> | null>(null);
 
 export function useFormContext() {
-  const context = React.useContext(FormGlobalContext);
-  // TODO: Warn?
-  return context!;
+  return React.useContext(FormGlobalContext);
 }
 
 export function FormContext<T extends DataType>(props: FormProps<T>) {
   const { children, ...rest } = props;
   return (
-    // @ts-ignore
-    <FormGlobalContext.Provider value={rest}>
+    <FormGlobalContext.Provider value={rest as FormContextValues}>
       {children}
     </FormGlobalContext.Provider>
   );
