506b2b057fbd6380efe9ac24f021766397f45460
Beier Luo <bluebill1049@hotmail.com>
2019-08-17 13:00:06 +1000

    update watchDefaultValues automation

---
+++ cypress/integration/watchDefaultValues.ts
@@ -0,0 +1,15 @@
+context('watchDefaultValues', () => {
+  it('should return default value with watch', () => {
+    cy.visit('http://localhost:3000/watch-default-values');
+
+    cy.get('#watchAll').should(
+      'have.text',
+      '{"test":"test","test1":{"firstName":"firstName","lastName":["lastName0","lastName1"],"deep":{"nest":"nest"}},"flatName[1].whatever":"flat"}',
+    );
+    cy.get('#array').should('have.text', '{"test":"test","flatName[1].whatever":"flat"}');
+    cy.get('#getArray').should('have.text', '["lastName0","lastName1"]');
+    cy.get('#object').should('have.text', '{"test":"test","test1.firstName":"firstName"}');
+    cy.get('#single').should('have.text', '"firstName"');
+    cy.get('#singleDeepArray').should('have.text', '"lastName0"');
+  });
+});
