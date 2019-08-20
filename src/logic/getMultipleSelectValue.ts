5cea131970d38477f35fa3f88e05393f02ba7f01
Elijah Wilson <tizz98@users.noreply.github.com>
2019-08-20 08:50:38 +0200

    Fix getMultipleSelectValue for HTMLOptionsCollection (#229)
    * fix getMultipleSelectValue for HTMLOptionsCollection
    
    * Update src/logic/getMultipleSelectValue.ts
    
    Co-Authored-By: Bill <bluebill1049@hotmail.com>
    
    * Update getMultipleSelectValue.ts
    
    * Update getMultipleSelectValue.ts

--- src/logic/getMultipleSelectValue.ts
+++ src/logic/getMultipleSelectValue.ts
@@ -1,2 +1,2 @@
-export default (options: HTMLOptionElement[]): string[] =>
-  options.filter(({ selected }): boolean => selected).map(({ value }): string => value);
+export default (options: HTMLOptionElement[] | HTMLOptionsCollection): string[] =>
+  [...options].filter(({ selected }): boolean => selected).map(({ value }): string => value);
