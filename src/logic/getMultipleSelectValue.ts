f6ea2cd816213f9e163a268a001c6b6815e74156
Elijah Wilson <tizz98@users.noreply.github.com>
2019-08-20 07:57:13 +0200

    Update getMultipleSelectValue.ts

--- src/logic/getMultipleSelectValue.ts
+++ src/logic/getMultipleSelectValue.ts
@@ -1,6 +1,2 @@
 export default (options: HTMLOptionElement[] | HTMLOptionsCollection): string[] =>	const filterOptions = (options: HTMLOptionElement[]): string[] =>
   [...options].filter(({ selected }): boolean => selected).map(({ value }): string => value);
-  options.filter(({ selected }): boolean => selected).map(({ value }): string => value);
-
-export default (options: HTMLOptionElement[] | HTMLOptionsCollection): string[] =>
-  options instanceof HTMLOptionsCollection ? filterOptions([...options]) : filterOptions(options);
