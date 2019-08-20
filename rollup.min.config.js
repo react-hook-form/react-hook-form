40fc1d5195abafddf87f5fa3eddf889623769618
Beier Luo <bluebill1049@hotmail.com>
2019-08-21 07:57:43 +1000

    💔 fix the build for bundlesize

---
+++ rollup.min.config.js
@@ -0,0 +1,18 @@
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/index.ts',
+  plugins: [typescript(), terser()],
+  external: ['react', 'react-dom'],
+  output: [
+    {
+      file: 'dist/react-hook-form.min.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/react-hook-form.min.es.js',
+      format: 'es',
+    },
+  ],
+};
