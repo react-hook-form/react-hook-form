a5528dc491edf9a377dcb1eef15494d5d52fee6a
Michael Stramel <m.stramel89@gmail.com>
2019-08-20 22:06:07 -0500

    Fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,25 @@ import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
-export function getConfig() {
+export function getConfig({
+  tsconfig = './tsconfig.json',
+  output = [
+    {
+      file: `dist/${pkg.name}.js`,
+      format: 'cjs',
+    },
+    {
+      file: `dist/${pkg.name}.es.js`,
+      format: 'esm',
+    },
+  ],
+} = {}) {
   return {
     input: 'src/index.ts',
     external: ['react', 'react-dom'],
     plugins: [
       typescript({
+        tsconfig,
         clean: true,
       }),
       terser({
@@ -19,16 +32,7 @@ export function getConfig() {
         },
       }),
     ],
-    output: [
-      {
-        file: `dist/${pkg.name}.js`,
-        format: 'cjs',
-      },
-      {
-        file: `dist/${pkg.name}.es.js`,
-        format: 'es',
-      },
-    ],
+    output,
   };
 }
 
