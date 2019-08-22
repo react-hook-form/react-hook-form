c3e1b59b7f50a5f35d7c615cc42973d858633860
bluebill1049 <bluebill1049@hotmail.com>
2019-08-22 18:22:36 +1000

    fix build issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import typescript from 'rollup-plugin-typescript2';
-import { terser } from 'rollup-plugin-terser';
+// import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export function getConfig({
@@ -22,14 +22,6 @@ export function getConfig({
       typescript({
         tsconfig,
         clean: true,
-      }),
-      terser({
-        warnings: true,
-        mangle: {
-          properties: {
-            regex: /^__/,
-          },
-        },
       }),
     ],
     output,
