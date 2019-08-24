a44ebf5a13a7a8f63df08cc9570d784d19c49422
Beier Luo <bluebill1049@hotmail.com>
2019-08-24 22:32:21 +1000

    fix default value issue

--- src/logic/getValueAndMessage.ts
+++ src/logic/getValueAndMessage.ts
@@ -1,26 +1,28 @@
 import isNullOrUndefined from '../utils/isNullOrUndefined';
 import isObject from '../utils/isObject';
 import { ValidationTypes } from '../types';
-import isRegex from '../utils/isRegex';
+import isRegex from "../utils/isRegex";
 
 export default (
-  validationData:
+  validationData?:
     | ValidationTypes
-    | { value: ValidationTypes; message: string } = '',
+    | { value: ValidationTypes; message: string },
 ): {
   value: ValidationTypes;
   message: string;
-} => ({
-  value:
-    isObject(validationData) &&
-    !isRegex(validationData) &&
-    !isNullOrUndefined(validationData.value)
-      ? validationData.value
-      : (validationData as ValidationTypes),
-  message:
-    isObject(validationData) &&
-    !isRegex(validationData) &&
-    validationData.message
-      ? validationData.message
-      : '',
-});
+} => {
+  return {
+    value:
+      isObject(validationData) &&
+      !isRegex(validationData) &&
+      !isNullOrUndefined(validationData.value)
+        ? validationData.value
+        : validationData as any,
+    message:
+      isObject(validationData) &&
+      !(validationData instanceof RegExp) &&
+      validationData.message
+        ? validationData.message
+        : '',
+  };
+};
