d2875f2c3cc0e86de5aeb492de2629db492c9865
Beier Luo <bluebill1049@hotmail.com>
2019-09-01 12:15:25 +1000

    fix unit test and return ay for combine result for now

--- src/logic/combineFieldValues.ts
+++ src/logic/combineFieldValues.ts
@@ -1,7 +1,7 @@
 import set from '../utils/set';
 import { FieldValues } from '../types';
 
-export default (data: FieldValues) =>
+export default (data: FieldValues): any =>
   Object.entries(data).reduce(
     (previous: FieldValues, [key, value]): FieldValues => {
       if (key.match(/\[\d+\]/gi) || key.indexOf('.')) {
