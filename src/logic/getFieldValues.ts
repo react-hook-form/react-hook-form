30964bb51eacd6010740ec79d34961242ddad3f5
bluebill1049 <bluebill1049@hotmail.com>
2019-09-10 11:25:09 +1000

    fix unit tests

--- src/logic/getFieldValues.ts
+++ src/logic/getFieldValues.ts
@@ -1,9 +1,9 @@
 import getFieldValue from './getFieldValue';
-import { FieldValues, FieldValue, Ref } from '../types';
+import { FieldValues, Ref } from '../types';
 
 export default <Data extends FieldValues>(fields: FieldValues): Data =>
   Object.values(fields).reduce(
-    (previous: FieldValues, { ref, ref: { name } }: Ref): FieldValue => ({
+    (previous: FieldValues, { ref, ref: { name } }: Ref): any => ({
       ...previous,
       ...{ [name]: getFieldValue(fields, ref) },
     }),
