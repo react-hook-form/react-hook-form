b91941f7f46e1512e8617a82255893966b0cf388
bluebill1049 <bluebill1049@hotmail.com>
2019-09-10 11:28:54 +1000

    fix unit tests

--- src/logic/validateWithSchema.test.ts
+++ src/logic/validateWithSchema.test.ts
@@ -55,7 +55,7 @@ describe('validateWithSchema', () => {
     expect(
       await validateWithSchema(
         {
-          validate: () => {},
+          validate: () => new Promise(() => {}),
         },
         { abortEarly: false },
         {},
