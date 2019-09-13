99157ba7619b22e275ad09af9999ef5293f7c7e4
bluebill1049 <bluebill1049@hotmail.com>
2019-09-13 14:57:46 +1000

    ⛑ fix type error

--- src/logic/pickErrors.ts
+++ src/logic/pickErrors.ts
@@ -3,7 +3,7 @@ import { FieldErrors } from '../types';
 export default <FormValues>(
   errors: FieldErrors<FormValues>,
   pickList: (keyof FormValues)[],
-) =>
+): FieldErrors<FormValues> =>
   Object.entries(errors).reduce(
     (previous, [key, error]) => ({
       ...previous,
