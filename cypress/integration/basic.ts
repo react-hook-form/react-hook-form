5d6aff5efe5d840c75afe021b3ababc5d572ef3d
Beier Luo <bluebill1049@hotmail.com>
2019-10-05 08:34:42 +1000

    update automation with focus

--- cypress/integration/basic.ts
+++ cypress/integration/basic.ts
@@ -3,6 +3,8 @@ context('basic form validation', () => {
     cy.visit('http://localhost:3000/basic/onSubmit');
     cy.get('button#submit').click();
 
+    cy.focused().should('have.attr', 'name', 'firstName');
+
     cy.get('input[name="firstName"] + p').contains('firstName error');
     cy.get('input[name="lastName"] + p').contains('lastName error');
     cy.get('select[name="selectNumber"] + p').contains('selectNumber error');
