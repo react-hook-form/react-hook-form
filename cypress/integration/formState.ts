94106890cbfd2ab934f0846f88c92bc85bebb372
bluebill1049 <bluebill1049@hotmail.com>
2019-10-18 20:54:46 +1100

    update automation

--- cypress/integration/formState.ts
+++ cypress/integration/formState.ts
@@ -3,24 +3,24 @@ context('form state', () => {
     cy.visit('http://localhost:3000/formState/onSubmit');
 
     cy.get('#state').contains(
-      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":[],"isSubmitting":false,"isValid":true}',
+      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":[],"isSubmitting":false,"isValid":false}',
     );
 
     cy.get('input[name="firstName"]').type('test');
     cy.get('#state').contains(
-      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName"],"isSubmitting":false,"isValid":false}',
     );
 
     cy.get('input[name="firstName"]').clear();
     cy.get('#state').contains(
-      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":["firstName"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":["firstName"],"isSubmitting":false,"isValid":false}',
     );
 
     cy.get('input[name="firstName"]').type('test');
 
     cy.get('input[name="lastName"]').type('test');
     cy.get('#state').contains(
-      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName"],"isSubmitting":false,"isValid":false}',
     );
 
     cy.get('input[name="lastName"]').clear();
@@ -122,58 +122,59 @@ context('form state', () => {
     cy.get('input[name="lastName"]').type('test');
 
     cy.get('#state').contains(
-      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName"],"isSubmitting":false,"isValid":false}',
     );
 
     cy.get('input[name="firstName"]').clear();
     cy.get('input[name="lastName"]').clear();
 
     cy.get('#state').contains(
-      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName"],"isSubmitting":false,"isValid":false}',
     );
 
     cy.get('select[name="select"]').select('test1');
     cy.get('#state').contains(
-      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select"],"isSubmitting":false,"isValid":false}',
     );
     cy.get('select[name="select"]').select('test');
     cy.get('#state').contains(
-      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select"],"isSubmitting":false,"isValid":false}',
     );
 
     cy.get('input[name="checkbox"]').check('on');
     cy.get('#state').contains(
-      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select","checkbox"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select","checkbox"],"isSubmitting":false,"isValid":false}',
     );
     cy.get('input[name="checkbox"]').uncheck();
     cy.get('#state').contains(
-      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select","checkbox"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select","checkbox"],"isSubmitting":false,"isValid":false}',
     );
 
     cy.get('input[name="checkbox-checked"]').uncheck();
     cy.get('#state').contains(
-      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select","checkbox","checkbox-checked"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select","checkbox","checkbox-checked"],"isSubmitting":false,"isValid":false}',
     );
     cy.get('input[name="checkbox-checked"]').check();
     cy.get('#state').contains(
-      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select","checkbox","checkbox-checked"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select","checkbox","checkbox-checked"],"isSubmitting":false,"isValid":false}',
     );
 
     cy.get('input[name="radio"]').check();
     cy.get('#state').contains(
-      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select","checkbox","checkbox-checked","radio"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select","checkbox","checkbox-checked","radio"],"isSubmitting":false,"isValid":false}',
     );
 
     cy.get('select[name="select"]').select('test');
     cy.get('#state').contains(
-      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select","checkbox","checkbox-checked","radio"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName","select","checkbox","checkbox-checked","radio"],"isSubmitting":false,"isValid":false}',
     );
   });
 
   it('should reset dirty value when inputs reset back to default with onBlur mode', () => {
-    cy.visit('http://localhost:3000/formState/onSubmit');
+    cy.visit('http://localhost:3000/formState/onBlur');
     cy.get('input[name="firstName"]').type('test');
     cy.get('input[name="lastName"]').type('test');
+    cy.get('input[name="lastName"]').blur();
 
     cy.get('#state').contains(
       '{"dirty":true,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName"],"isSubmitting":false,"isValid":true}',
@@ -181,14 +182,15 @@ context('form state', () => {
 
     cy.get('input[name="firstName"]').clear();
     cy.get('input[name="lastName"]').clear();
+    cy.get('input[name="lastName"]').blur();
 
     cy.get('#state').contains(
-      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName"],"isSubmitting":false,"isValid":false}',
     );
   });
 
   it('should reset dirty value when inputs reset back to default with onChange mode', () => {
-    cy.visit('http://localhost:3000/formState/onSubmit');
+    cy.visit('http://localhost:3000/formState/onChange');
     cy.get('input[name="firstName"]').type('test');
     cy.get('input[name="lastName"]').type('test');
 
@@ -200,7 +202,7 @@ context('form state', () => {
     cy.get('input[name="lastName"]').clear();
 
     cy.get('#state').contains(
-      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName"],"isSubmitting":false,"isValid":true}',
+      '{"dirty":false,"isSubmitted":false,"submitCount":0,"touched":["firstName","lastName"],"isSubmitting":false,"isValid":false}',
     );
   });
 });
