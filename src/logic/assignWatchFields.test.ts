8ce6c1d61563570511b78b027db6ccdd62b6fe64
bluebill1049 <bluebill1049@hotmail.com>
2019-10-28 10:33:48 +1100

    fix unit test

--- src/logic/assignWatchFields.test.ts
+++ src/logic/assignWatchFields.test.ts
@@ -2,20 +2,26 @@ import assignWatchFields from './assignWatchFields';
 
 describe('assignWatchFields', () => {
   test('should return undefined when field values is empty object or undefined', () => {
-    expect(assignWatchFields({}, '', {})).toEqual(undefined);
+    expect(assignWatchFields({}, '', new Set(''))).toEqual(undefined);
   });
 
   test('should return watched value and update watchFields', () => {
-    const watchFields = {};
-    expect(assignWatchFields({ test: '' }, 'test', watchFields)).toEqual('');
-    expect(watchFields).toEqual({ test: true });
+    const watchFields = new Set();
+    expect(assignWatchFields({ test: '' }, 'test', watchFields as any)).toEqual(
+      '',
+    );
+    expect(watchFields).toEqual(new Set(['test']));
   });
 
   test('should get array fields value', () => {
-    const watchFields = {};
+    const watchFields = new Set();
     expect(
-      assignWatchFields({ 'test[0]': '', 'test[1]': '' }, 'test', watchFields),
+      assignWatchFields(
+        { 'test[0]': '', 'test[1]': '' },
+        'test',
+        watchFields as any,
+      ),
     ).toEqual(['', '']);
-    expect(watchFields).toEqual({ 'test[0]': true, 'test[1]': true });
+    expect(watchFields).toEqual(new Set(['test[0]', 'test[1]']));
   });
 });
