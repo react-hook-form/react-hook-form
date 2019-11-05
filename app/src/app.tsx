15d66683a4ac46d13e02a127b2a1195a196b49c7
bluebill1049 <bluebill1049@hotmail.com>
2019-11-05 13:25:37 +1100

    📸 include automation for conditional fields

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -12,7 +12,8 @@ import WatchDefaultValues from './watchDefaultValues';
 import Reset from './reset';
 import TriggerValidation from './triggerValidation';
 import ReValidateMode from './reValidateMode';
-import SetValueCustomRegister from "./setValueCustomRegister";
+import SetValueCustomRegister from './setValueCustomRegister';
+import ConditionalField from './conditionalField';
 
 const App: React.FC = () => {
   return (
@@ -35,9 +36,14 @@ const App: React.FC = () => {
         component={BasicSchemaValidation}
       />
       <Route path="/setError" exact component={SetError} />
+      <Route path="/conditionalField" exact component={ConditionalField} />
       <Route path="/reset" exact component={Reset} />
       <Route path="/setValue" exact component={SetValue} />
-      <Route path="/SetValueCustomRegister" exact component={SetValueCustomRegister} />
+      <Route
+        path="/SetValueCustomRegister"
+        exact
+        component={SetValueCustomRegister}
+      />
       <Route path="/formState/:mode" exact component={FormState} />
       <Route path="/default-values" exact component={DefaultValues} />
       <Route path="/trigger-validation" exact component={TriggerValidation} />
