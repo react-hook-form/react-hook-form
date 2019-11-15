dcb05e178b81f5212cac497e6ba7010cbb489540
bluebill1049 <bluebill1049@hotmail.com>
2019-11-16 10:52:13 +1100

    improve setError with callback and automation test

--- cypress/integration/setError.ts
+++ cypress/integration/setError.ts
@@ -5,6 +5,12 @@ context('form setError', () => {
     cy.get('#error0').contains('0 wrong');
     cy.get('#error1').contains('1 wrong');
     cy.get('#error2').contains('2 wrong');
+    cy.get('#error3').contains('3 test');
+    cy.get('#error4').contains('4 required');
+    cy.get('#error5').contains('5 minLength');
+    cy.get('#error').contains(
+      'testMessageThis is required.Minlength is 10This is requiredThis is minLength',
+    );
   });
 
   it('should clear individual error', () => {
