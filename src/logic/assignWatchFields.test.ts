283b893460c71ac58e208923fe160df51dc11c62
Beier Luo <bluebill1049@hotmail.com>
2019-11-16 08:11:16 +1100

    fix unit test

--- src/logic/assignWatchFields.test.ts
+++ src/logic/assignWatchFields.test.ts
@@ -2,14 +2,14 @@ import assignWatchFields from './assignWatchFields';
 
 describe('assignWatchFields', () => {
   test('should return undefined when field values is empty object or undefined', () => {
-    expect(assignWatchFields({}, '', new Set(''))).toEqual(undefined);
+    expect(assignWatchFields({}, '', new Set(''), {})).toEqual(undefined);
   });
 
   test('should return watched value and update watchFields', () => {
     const watchFields = new Set();
-    expect(assignWatchFields({ test: '' }, 'test', watchFields as any)).toEqual(
-      '',
-    );
+    expect(
+      assignWatchFields({ test: '' }, 'test', watchFields as any, {}),
+    ).toEqual('');
     expect(watchFields).toEqual(new Set(['test']));
   });
 
@@ -20,8 +20,15 @@ describe('assignWatchFields', () => {
         { 'test[0]': '', 'test[1]': '' },
         'test',
         watchFields as any,
+        {},
       ),
     ).toEqual(['', '']);
     expect(watchFields).toEqual(new Set(['test[0]', 'test[1]']));
+  });
+
+  test('should return default value correctly', () => {
+    expect(
+      assignWatchFields({ a: true }, 'b', new Set(), { b: true } as any),
+    ).toEqual(true);
   });
 });
