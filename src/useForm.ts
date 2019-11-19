7d2da23b0c17d0debea06ff96448895aa1ba3aaa
bluebill1049 <bluebill1049@hotmail.com>
2019-11-19 15:34:29 +1100

    fix #531 issue around lose input focus

--- src/useForm.ts
+++ src/useForm.ts
@@ -280,12 +280,16 @@ export default function useForm<FormValues extends FieldValues = FieldValues>({
         setInternalValue(name, value);
       }
 
+      if (shouldRender) {
+        render();
+      }
+
       const error = await validateFieldCurry(field);
-      renderBaseOnError(name, error, shouldRender);
+      renderBaseOnError(name, error);
 
       return isEmptyObject(error);
     },
-    [renderBaseOnError, setInternalValue, validateFieldCurry],
+    [render, renderBaseOnError, setInternalValue, validateFieldCurry],
   );
 
   const executeSchemaValidation = useCallback(
@@ -576,7 +580,11 @@ export default function useForm<FormValues extends FieldValues = FieldValues>({
     fieldNames?: FieldName<FormValues> | FieldName<FormValues>[],
     defaultValue?: string | Partial<FormValues>,
   ): FieldValue<FormValues> | Partial<FormValues> | string | undefined {
-    const combinedDefaultValues = defaultValue || defaultValues || {};
+    const combinedDefaultValues = isUndefined(defaultValue)
+      ? isUndefined(defaultValues)
+        ? {}
+        : defaultValues
+      : defaultValue;
     const fieldValues = getFieldsValues<FormValues>(fieldsRef.current);
     const watchFields = watchFieldsRef.current;
 
