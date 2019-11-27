d933e4a09e3546716232bb9556640b06384f1fd6
bluebill1049 <bluebill1049@hotmail.com>
2019-11-27 11:07:51 +1100

    revert change on unregister

--- src/useForm.test.tsx
+++ src/useForm.test.tsx
@@ -40,7 +40,7 @@ describe('useForm', () => {
       act(() => {
         expect(
           result.current.register({ name: 'test', type: 'input' }, {}),
-        ).toMatchSnapshot();
+        ).toBeUndefined();
       });
     });
 
