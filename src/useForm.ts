0ae0628022bc94167f8153cd2e80684c193f6819
Beier Luo <bluebill1049@hotmail.com>
2019-12-01 23:52:44 +1100

    fix #603 isValid issue

--- src/useForm.ts
+++ src/useForm.ts
@@ -1022,7 +1022,7 @@ export default function useForm<FormValues extends FieldValues = FieldValues>({
               validFieldsRef.current.size >=
                 fieldsWithValidationRef.current.size &&
               isEmptyObject(errorsRef.current)
-            : false,
+            : !isEmptyObject(fieldsRef.current),
         }),
   };
 
