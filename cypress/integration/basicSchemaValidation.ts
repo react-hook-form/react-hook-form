8386bfa4ed4759df194a6a9a0c012df9cdf9d995
bluebill1049 <bluebill1049@hotmail.com>
2019-12-04 10:39:32 +1100

    fix automation

--- cypress/integration/basicSchemaValidation.ts
+++ cypress/integration/basicSchemaValidation.ts
@@ -48,7 +48,7 @@ context('basicSchemaValidation form validation', () => {
     cy.get('input[name="checkbox"]').check();
 
     cy.get('p').should('have.length', 0);
-    cy.get('#renderCount').contains('26');
+    cy.get('#renderCount').contains('27');
   });
 
   it('should validate the form with onBlur mode', () => {
@@ -108,7 +108,7 @@ context('basicSchemaValidation form validation', () => {
     cy.get('input[name="checkbox"]').check();
 
     cy.get('p').should('have.length', 0);
-    cy.get('#renderCount').contains('29');
+    cy.get('#renderCount').contains('30');
   });
 
   it('should validate the form with onChange mode', () => {
@@ -159,6 +159,6 @@ context('basicSchemaValidation form validation', () => {
     cy.get('input[name="checkbox"]').check();
 
     cy.get('p').should('have.length', 0);
-    cy.get('#renderCount').contains('34');
+    cy.get('#renderCount').contains('35');
   });
 });
