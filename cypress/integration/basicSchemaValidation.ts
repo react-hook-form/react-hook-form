b06b4833ec462b5c6b469d64d7ee9abb7b8badc4
bluebill1049 <bluebill1049@hotmail.com>
2019-12-06 15:52:21 +1100

    remove unused variable

--- cypress/integration/basicSchemaValidation.ts
+++ cypress/integration/basicSchemaValidation.ts
@@ -159,6 +159,6 @@ context('basicSchemaValidation form validation', () => {
     cy.get('input[name="checkbox"]').check();
 
     cy.get('p').should('have.length', 0);
-    cy.get('#renderCount').contains('31');
+    cy.get('#renderCount').contains('32');
   });
 });
