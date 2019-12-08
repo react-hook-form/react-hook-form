06c6d3a010b6a185d43cb53f72ee10adef0a49c2
Beier Luo <bluebill1049@hotmail.com>
2019-12-08 19:26:18 +1100

    revert

--- src/logic/setNativeValue.ts
+++ src/logic/setNativeValue.ts
@@ -1,13 +1,10 @@
 // reference https://github.com/facebook/react/issues/10135#issuecomment-401496776
 export default function setNativeValue(element: HTMLInputElement, value: any) {
-  const valueAttribute = 'value';
   const { set: valueSetter } =
-    Object.getOwnPropertyDescriptor(element, valueAttribute) || {};
+    Object.getOwnPropertyDescriptor(element, 'value') || {};
+  const prototype = Object.getPrototypeOf(element);
   const { set: prototypeValueSetter } =
-    Object.getOwnPropertyDescriptor(
-      Object.getPrototypeOf(element),
-      valueAttribute,
-    ) || {};
+    Object.getOwnPropertyDescriptor(prototype, 'value') || {};
 
   if (prototypeValueSetter && valueSetter !== prototypeValueSetter) {
     prototypeValueSetter.call(element, value);
