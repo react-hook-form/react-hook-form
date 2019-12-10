4c4e9eae50a116ec208b26ced34638f75b61c49c
Bill <bluebill1049@hotmail.com>
2019-12-10 11:49:51 +1100

    🥂 final fix isValid (#639)
    * fix isValid
    
    * add more automation
    
    * remove validate schema check at reset
    
    * skip revalidate if isVald false
    
    * fix validateSchemaIsValid

--- app/src/isValid.tsx
+++ app/src/isValid.tsx
@@ -15,6 +15,7 @@ const validationSchema = yup.object().shape({
 
 const IsValid: React.FC = (props: any) => {
   const isBuildInValidation = props.match.params.mode === 'build-in';
+  const [show, setShow] = React.useState(true);
   const { register, handleSubmit, formState } = useForm<{
     firstName: string;
     lastName: string;
@@ -51,18 +52,37 @@ const IsValid: React.FC = (props: any) => {
             ref={register({ required: true })}
             placeholder="lastName"
           />
+          {!show && (
+            <input
+              name="hidden"
+              ref={register({ required: true })}
+              placeholder="hidden"
+            />
+          )}
           <input name="age" ref={register} placeholder="age" />
         </>
       ) : (
         <>
           <input name="location" ref={register} placeholder="location" />
-          <input name="firstName" ref={register} placeholder="firstName" />
+          {show && (
+            <input name="firstName" ref={register} placeholder="firstName" />
+          )}
           <input name="lastName" ref={register} placeholder="lastName" />
           <input name="age" ref={register} placeholder="age" />
         </>
       )}
       <div id="isValid">{JSON.stringify(formState.isValid)}</div>
       <div id="renderCount">{renderCounter}</div>
+
+      <button
+        type="button"
+        id="toggle"
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        Add
+      </button>
     </form>
   );
 };
