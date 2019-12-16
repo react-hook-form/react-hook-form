09b5873be21ebb2e9dd4b5d03462372246b967df
Bill <bluebill1049@hotmail.com>
2019-12-16 14:20:59 +1100

    🐞 fix #670 reset issue with context (#673)

--- src/useFormContext.tsx
+++ src/useFormContext.tsx
@@ -12,11 +12,10 @@ export function useFormContext<T extends FieldValues>(): FormContextValues<T> {
 
 export function FormContext<T extends FieldValues>(props: FormProps<T>) {
   const { children, formState, errors, ...restMethods } = props;
-  const restRef = React.useRef(restMethods);
 
   return (
     <FormGlobalContext.Provider
-      value={{ ...restRef.current, formState, errors } as FormContextValues}
+      value={{ ...restMethods, formState, errors } as FormContextValues}
     >
       {children}
     </FormGlobalContext.Provider>
