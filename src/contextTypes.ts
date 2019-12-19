b099431bb545b278ba353a0de1c8f682fdc41f48
bluebill1049 <bluebill1049@hotmail.com>
2019-12-19 16:47:34 +1100

    revert back on how context type was defined :(

--- src/contextTypes.ts
+++ src/contextTypes.ts
@@ -3,16 +3,13 @@ import {
   FieldValues,
   FormStateProxy,
   FieldErrors,
-  Register,
   OnSubmit,
-  Unregister,
-  Watch,
-  SetError,
-  ClearError,
-  TriggerValidation,
-  SetValue,
-  Reset,
-  GetValues,
+  ElementLike,
+  ValidationOptions,
+  FieldName,
+  FieldValue,
+  ManualFieldError,
+  MultipleFieldErrors,
 } from './types';
 
 export interface FormProps<FormValues extends FieldValues = FieldValues>
@@ -23,17 +20,72 @@ export interface FormProps<FormValues extends FieldValues = FieldValues>
 export interface FormContextValues<
   FormValues extends FieldValues = FieldValues
 > {
-  register: Register<FormValues>;
-  unregister: Unregister<FormValues>;
-  watch: Watch<FormValues>;
-  setError: SetError<FormValues>;
-  clearError: ClearError<FormValues>;
-  setValue: SetValue<FormValues>;
-  triggerValidation: TriggerValidation<FormValues>;
+  register<Element extends ElementLike = ElementLike>(
+    validationOptions: ValidationOptions,
+  ): (ref: Element | null) => void;
+  register<Element extends ElementLike = ElementLike>(
+    name: FieldName<FormValues>,
+    validationOptions?: ValidationOptions,
+  ): void;
+  register<Element extends ElementLike = ElementLike>(
+    namesWithValidationOptions: Record<
+      FieldName<FormValues>,
+      ValidationOptions
+    >,
+  ): void;
+  register<Element extends ElementLike = ElementLike>(
+    ref: Element,
+    validationOptions?: ValidationOptions,
+  ): void;
+  register<Element extends ElementLike = ElementLike>(
+    refOrValidationOptions: ValidationOptions | Element | null,
+    validationOptions?: ValidationOptions,
+  ): ((ref: Element | null) => void) | void;
+  unregister(name: FieldName<FormValues>): void;
+  unregister(names: FieldName<FormValues>[]): void;
+  unregister(names: FieldName<FormValues> | FieldName<FormValues>[]): void;
+  watch(): FormValues;
+  watch(option: { nest: boolean }): FormValues;
+  watch<T extends FieldName<FormValues>>(
+    field: T & string,
+    defaultValue?: string,
+  ): FormValues[T];
+  watch(
+    fields: FieldName<FormValues>[] | string[],
+    defaultValues?: Partial<FormValues>,
+  ): Partial<FormValues>;
+  watch(
+    fieldNames?:
+      | FieldName<FormValues>
+      | FieldName<FormValues>[]
+      | { nest: boolean },
+    defaultValue?: string | Partial<FormValues>,
+  ): FieldValue<FormValues> | Partial<FormValues> | string | undefined;
+  setError(name: ManualFieldError<FormValues>[]): void;
+  setError(name: FieldName<FormValues>, type: MultipleFieldErrors): void;
+  setError(name: FieldName<FormValues>, type: string, message?: string): void;
+  setError(
+    name: FieldName<FormValues> | ManualFieldError<FormValues>[],
+    type: string | MultipleFieldErrors,
+    message?: string,
+  ): void;
+  clearError(): void;
+  clearError(name: FieldName<FormValues>): void;
+  clearError(names: FieldName<FormValues>[]): void;
+  clearError(name?: FieldName<FormValues> | FieldName<FormValues>[]): void;
+  setValue: <Name extends FieldName<FormValues>>(
+    name: Name,
+    value: FormValues[Name],
+    shouldValidate?: boolean,
+  ) => void | Promise<boolean>;
+  triggerValidation: (
+    payload?: FieldName<FormValues> | FieldName<FormValues>[] | string,
+    shouldRender?: boolean,
+  ) => Promise<boolean>;
   errors: FieldErrors<FormValues>;
   formState: FormStateProxy<FormValues>;
-  reset: Reset<FormValues>;
-  getValues: GetValues<FormValues>;
+  reset: (values?: Partial<FormValues>) => void;
+  getValues: (payload?: { nest: boolean }) => FormValues;
   handleSubmit: (
     callback: OnSubmit<FormValues>,
   ) => (e: React.BaseSyntheticEvent) => Promise<void>;
