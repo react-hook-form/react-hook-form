4b2945f2856b7b8ff52ce880fe6e08f11ff5ab79
bluebill1049 <bluebill1049@hotmail.com>
2020-01-02 15:41:51 +1100

    update code review

--- src/logic/reportFieldNotFound.test.ts
+++ src/logic/reportFieldNotFound.test.ts
@@ -1,9 +1,5 @@
 import reportFieldNotFound from './reportFieldNotFound';
 
-jest.mock('../constants', () => ({
-  IS_DEV_ENV: true,
-}));
-
 describe('reportFieldNotFound', () => {
   const env = process.env.NODE_ENV;
   beforeEach(() => {
