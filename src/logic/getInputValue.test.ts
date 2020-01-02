e0b76f52fd74fd84b9dd9f5bdadc1140683d1007
Bill <bluebill1049@hotmail.com>
2020-01-02 19:37:44 +1100

    🤺 close #757 with null and undefined check for value (#758)
    * close #757 with null and undefined check for value
    
    * update unit test

--- src/logic/getInputValue.test.ts
+++ src/logic/getInputValue.test.ts
@@ -5,4 +5,6 @@ test('getInputValue should return correct value', () => {
   expect(getInputValue({ checked: true }, false)).toEqual({ checked: true });
   expect(getInputValue({ value: 'test' }, false)).toEqual('test');
   expect(getInputValue({ data: 'test' }, false)).toEqual({ data: 'test' });
+  expect(getInputValue(undefined, false)).toEqual(undefined);
+  expect(getInputValue(null, false)).toEqual(null);
 });
--- src/logic/getInputValue.ts
+++ src/logic/getInputValue.ts
@@ -1,6 +1,11 @@
 import isUndefined from '../utils/isUndefined';
+import isNullOrUndefined from '../utils/isNullOrUndefined';
 
 export default (target: any, isCheckbox: boolean) => {
+  if (isNullOrUndefined(target)) {
+    return target;
+  }
+
   return isCheckbox
     ? isUndefined(target.checked)
       ? target
