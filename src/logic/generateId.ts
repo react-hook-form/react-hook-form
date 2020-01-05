8cf3b5a2d0de3670d355f1c423defd0f23240047
bluebill1049 <bluebill1049@hotmail.com>
2020-01-05 20:08:50 +1100

    ts-ignore generate ID function

--- src/logic/generateId.ts
+++ src/logic/generateId.ts
@@ -1,5 +1,5 @@
 export default () =>
-  ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+  ([1e7] + (-1e3 as any) + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c: any) =>
     (
       c ^
       (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
