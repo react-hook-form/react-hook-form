304aabdca8ad6078eede2f896169fec4b8587355
bluebill1049 <bluebill1049@hotmail.com>
2020-01-05 19:43:23 +1100

    update generate ID function

--- src/logic/generateId.ts
+++ src/logic/generateId.ts
@@ -1,5 +1,7 @@
 export default () =>
-  'x'.repeat(40).replace(/x/g, c => {
-    const r = (Math.random() * 16) | 0;
-    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
-  });
+  ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16),
+  );
