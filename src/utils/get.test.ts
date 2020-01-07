732ecb67ab9dff05d0e06acee5f5d885bec36c15
bluebill1049 <bluebill1049@hotmail.com>
2020-01-07 22:35:56 +1100

    include more unit tests

--- src/utils/get.test.ts
+++ src/utils/get.test.ts
@@ -6,11 +6,14 @@ describe('get', () => {
       bill: [1, 2, 3],
       luo: [1, 3, { betty: 'test' }],
       betty: { test: { test1: [{ test2: 'bill' }] } },
+      'betty.test.test1[0].test1': 'test',
     };
     expect(get(test, 'bill')).toEqual([1, 2, 3]);
     expect(get(test, 'bill[0]')).toEqual(1);
     expect(get(test, 'luo[2].betty')).toEqual('test');
     expect(get(test, 'betty.test.test1[0].test2')).toEqual('bill');
+    expect(get(test, 'betty.test.test1[0].test1')).toEqual('test');
+    expect(get(test, 'betty.test.test1[0].test3')).toEqual(undefined);
   });
 
   it('should get from the flat data', () => {
