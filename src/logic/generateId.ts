e84983ba120260fb74621d147ed2f41182429d80
bluebill1049 <bluebill1049@hotmail.com>
2020-01-08 21:51:08 +1100

    remove generate id with crypto

--- src/logic/generateId.ts
+++ src/logic/generateId.ts
@@ -1,7 +1,5 @@
 export default () =>
-  ([1e7] + (-1e3 as any) + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c: any) =>
-    (
-      c ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-    ).toString(16),
-  );
+  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = (Math.random() * 16) | 0;
+    return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+  });
