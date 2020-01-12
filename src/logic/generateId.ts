6c840c2a6dc59661dccd1308df59fbc4add9f11f
bluebill1049 <bluebill1049@hotmail.com>
2020-01-12 15:00:45 +1100

    clean up generateID

--- src/logic/generateId.ts
+++ src/logic/generateId.ts
@@ -1,7 +1,7 @@
 export default () => {
   const d = performance.now() * 1000;
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    const r = (d + Math.random() * 16) % 16 | 0;
+    const r = (Math.random() * 16 + d) % 16 | 0;
     return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
   });
 };
