1b5f549289e9ea206c7b54bb40f0232dd65beea8
bluebill1049 <bluebill1049@hotmail.com>
2020-01-12 14:58:59 +1100

    remove unnecessary variable

--- src/logic/generateId.ts
+++ src/logic/generateId.ts
@@ -1,9 +1,7 @@
-const byte = 16;
-
 export default () => {
   const d = performance.now() * 1000;
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    const r = (d + Math.random() * byte) % byte | 0;
-    return (c == 'x' ? r : (r & 0x3) | 0x8).toString(byte);
+    const r = (d + Math.random() * 16) % 16 | 0;
+    return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
   });
 };
