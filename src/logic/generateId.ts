8e170524631ee1a5ae043edc8aa4a390b6c77ed4
bluebill1049 <bluebill1049@hotmail.com>
2020-01-12 14:57:11 +1100

    include generate id with performance.now

--- src/logic/generateId.ts
+++ src/logic/generateId.ts
@@ -1,5 +1,9 @@
-export default () =>
-  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    const r = (Math.random() * 16) | 0;
-    return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+const byte = 16;
+
+export default () => {
+  const d = performance.now() * 1000;
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = (d + Math.random() * byte) % byte | 0;
+    return (c == 'x' ? r : (r & 0x3) | 0x8).toString(byte);
   });
+};
