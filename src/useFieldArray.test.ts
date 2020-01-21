f0619b8f0c1d2b288a154abec6cf01190243ca5c
Bill <bluebill1049@hotmail.com>
2020-01-21 19:18:33 +1100

    🥂 fix useFieldArray issue with default value (#876)
    * fix remvoe issue with default value
    
    * fix unit tests
    
    * fix insert data test

--- src/useFieldArray.test.ts
+++ src/useFieldArray.test.ts
@@ -163,6 +163,12 @@ describe('useFieldArray', () => {
           defaultValuesRef: {
             current: { test: [{ test: '1' }, { test: '2' }] },
           },
+          fieldsRef: {
+            current: {
+              'test[0]': { ref: { name: 'test[0]', value: { test: '1' } } },
+              'test[1]': { ref: { name: 'test[1]', value: { test: '2' } } },
+            },
+          },
         }),
         name: 'test',
       }),
@@ -197,6 +203,12 @@ describe('useFieldArray', () => {
           defaultValuesRef: {
             current: { test: [{ test: '1' }, { test: '2' }] },
           },
+          fieldsRef: {
+            current: {
+              'test[0]': { ref: { name: 'test[0]', value: { test: '1' } } },
+              'test[1]': { ref: { name: 'test[1]', value: { test: '2' } } },
+            },
+          },
         }),
         name: 'test',
       }),
@@ -211,17 +223,6 @@ describe('useFieldArray', () => {
       { id: '1', test: '3' },
       { id: '1', test: '2' },
     ]);
-
-    act(() => {
-      result.current.insert(1, {});
-    });
-
-    expect(result.current.fields).toEqual([
-      { id: '1', test: '1' },
-      { id: '1' },
-      { id: '1', test: '3' },
-      { id: '1', test: '2' },
-    ]);
   });
 
   it('should swap data order', () => {
@@ -230,6 +231,12 @@ describe('useFieldArray', () => {
         control: reconfigureControl({
           defaultValuesRef: {
             current: { test: [{ test: '1' }, { test: '2' }] },
+          },
+          fieldsRef: {
+            current: {
+              'test[0]': { ref: { name: 'test[0]', value: { test: '1' } } },
+              'test[1]': { ref: { name: 'test[1]', value: { test: '2' } } },
+            },
           },
         }),
         name: 'test',
@@ -253,6 +260,13 @@ describe('useFieldArray', () => {
           defaultValuesRef: {
             current: { test: [{ test: '1' }, { test: '2' }, { test: '3' }] },
           },
+          fieldsRef: {
+            current: {
+              'test[0]': { ref: { name: 'test[0]', value: { test: '1' } } },
+              'test[1]': { ref: { name: 'test[1]', value: { test: '2' } } },
+              'test[2]': { ref: { name: 'test[2]', value: { test: '3' } } },
+            },
+          },
         }),
         name: 'test',
       }),
@@ -265,16 +279,6 @@ describe('useFieldArray', () => {
     expect(result.current.fields).toEqual([
       { id: '1', test: '3' },
       { id: '1', test: '1' },
-      { id: '1', test: '2' },
-    ]);
-
-    act(() => {
-      result.current.move(1, 0);
-    });
-
-    expect(result.current.fields).toEqual([
-      { id: '1', test: '1' },
-      { id: '1', test: '3' },
       { id: '1', test: '2' },
     ]);
   });
