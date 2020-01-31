fd85ba7fc7858cb141b03616519ab319f2a47b63
Beier Luo <bluebill1049@hotmail.com>
2020-02-01 08:26:05 +1100

    improve #938 watch perf with search array

--- src/logic/getFieldsValues.test.ts
+++ src/logic/getFieldsValues.test.ts
@@ -1,11 +1,12 @@
 import getFieldsValues from './getFieldsValues';
+import getFieldValue from './getFieldValue';
 
-jest.mock('./getFieldValue', () => ({
-  default: () => 'test',
-}));
+jest.mock('./getFieldValue');
 
 describe('getFieldsValues', () => {
   it('should return all fields value', () => {
+    // @ts-ignore
+    getFieldValue.mockImplementation(() => 'test');
     expect(
       getFieldsValues({
         test: {
@@ -18,6 +19,52 @@ describe('getFieldsValues', () => {
     ).toEqual({
       test: 'test',
       test1: 'test',
+    });
+  });
+
+  it('should return searched string with fields value', () => {
+    expect(
+      getFieldsValues(
+        {
+          test: {
+            ref: { name: 'test' },
+          },
+          tex: {
+            ref: { name: 'test1' },
+          },
+          tex123: {
+            ref: { name: 'test1' },
+          },
+        },
+        'test',
+      ),
+    ).toEqual({
+      test: 'test',
+    });
+  });
+
+  it('should return searched array string with fields value', () => {
+    expect(
+      getFieldsValues(
+        {
+          test: {
+            ref: { name: 'test' },
+          },
+          tex: {
+            ref: { name: 'test1' },
+          },
+          123: {
+            ref: { name: 'test1' },
+          },
+          1456: {
+            ref: { name: 'test1' },
+          },
+        },
+        ['test', 'tex'],
+      ),
+    ).toEqual({
+      test: 'test',
+      tex: 'test',
     });
   });
 });
