733a2f8811e94742a87b62888cf5d14473413fe4
Bill <bluebill1049@hotmail.com>
2020-02-01 08:55:27 +1100

    🕺 improve #938 watch perf with search array (#946)

--- src/logic/getFieldsValues.ts
+++ src/logic/getFieldsValues.ts
@@ -1,19 +1,29 @@
 import getFieldValue from './getFieldValue';
-import { FieldName, FieldValues, FieldRefs } from '../types';
 import isString from '../utils/isString';
+import isArray from '../utils/isArray';
+import isUndefined from '../utils/isUndefined';
+import { FieldName, FieldValues, FieldRefs } from '../types';
 
 export default <FormValues extends FieldValues>(
   fields: FieldRefs<FormValues>,
-  search?: any,
+  search?: unknown,
 ) => {
   const output = {} as FormValues;
   const isSearchString = isString(search);
+  const isSearchArray = isArray(search);
 
   for (const name in fields) {
-    if (!isSearchString || (isSearchString && name.startsWith(search))) {
+    if (
+      isUndefined(search) ||
+      (isSearchString && name.startsWith(search as string)) ||
+      (isSearchArray &&
+        (search as string[]).find((data: string) =>
+          name.startsWith(data as string),
+        ))
+    ) {
       output[name as FieldName<FormValues>] = getFieldValue(
         fields,
-        (fields[name] || {}).ref,
+        fields[name]!.ref,
       );
     }
   }
