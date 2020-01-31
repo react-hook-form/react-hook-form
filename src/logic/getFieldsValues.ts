932a67b452be6ec13a47c5168eb9ad00f117a5d4
bluebill1049 <bluebill1049@hotmail.com>
2020-01-31 13:22:38 +1100

    close #938 performance enhancement for watch

--- src/logic/getFieldsValues.ts
+++ src/logic/getFieldsValues.ts
@@ -1,13 +1,18 @@
 import getFieldValue from './getFieldValue';
-import { FieldValues, FieldRefs, Field } from '../types';
+import { FieldValues, FieldRefs } from '../types';
+import isString from '../utils/isString';
 
 export default <FormValues extends FieldValues>(
   fields: FieldRefs<FormValues>,
-) =>
-  (Object.values(fields) as Field[]).reduce(
-    (previous, { ref, ref: { name } }) => ({
-      ...previous,
-      ...{ [name]: getFieldValue(fields, ref) },
-    }),
-    {} as FormValues,
-  );
+  search?: any,
+) => {
+  const output: Record<string, FormValues> = {};
+
+  for (const name in fields) {
+    if (!search || (isString(search) && name.startsWith(search))) {
+      output[name] = getFieldValue(fields, (fields[name] || {}).ref);
+    }
+  }
+
+  return output as FormValues;
+};
