9d7af8e085907eed153b4351895b54bcad982240
Kotaro Sugawara <kotarella1110@gmail.com>
2020-01-31 16:05:21 +0900

    improve type for getFieldsValues

--- src/logic/getFieldsValues.ts
+++ src/logic/getFieldsValues.ts
@@ -1,19 +1,22 @@
 import getFieldValue from './getFieldValue';
-import { FieldValues, FieldRefs } from '../types';
+import { FieldName, FieldValues, FieldRefs } from '../types';
 import isString from '../utils/isString';
 
 export default <FormValues extends FieldValues>(
   fields: FieldRefs<FormValues>,
   search?: any,
 ) => {
-  const output: Record<string, FormValues> = {};
+  const output = {} as FormValues;
   const isSearchString = isString(search);
 
   for (const name in fields) {
     if (!isSearchString || (isSearchString && name.startsWith(search))) {
-      output[name] = getFieldValue(fields, (fields[name] || {}).ref);
+      output[name as FieldName<FormValues>] = getFieldValue(
+        fields,
+        (fields[name] || {}).ref,
+      );
     }
   }
 
-  return output as FormValues;
+  return output;
 };
