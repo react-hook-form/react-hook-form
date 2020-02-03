91a4c405df3dc21bfac3546325563df2ddbd0688
bluebill1049 <bluebill1049@hotmail.com>
2020-02-03 21:00:48 +1100

    close #951 include keyname prop for custom id

--- src/types.ts
+++ src/types.ts
@@ -290,9 +290,6 @@ export type ErrorMessageProps<
 
 export type UseFieldArrayProps<ControlProp extends Control = Control> = {
   control?: ControlProp;
+  keyName?: string;
   name: string;
 };
-
-export type WithFieldId<Value> = {
-  id?: string;
-} & Value;
