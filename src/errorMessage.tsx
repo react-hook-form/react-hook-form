c55a3e3b339b06d9b08a12168f7dc28cabe0899a
Kotaro Sugawara <kotarella1110@gmail.com>
2020-02-14 14:07:26 +0900

    ⛩ fix #1033 props type for ErrorMessage

--- src/errorMessage.tsx
+++ src/errorMessage.tsx
@@ -14,6 +14,7 @@ const ErrorMessage = <
   As extends
     | undefined
     | React.ReactElement
+    | React.ComponentType<any>
     | keyof JSX.IntrinsicElements = undefined
 >({
   as: InnerComponent,
