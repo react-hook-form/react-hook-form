334f41d33ad447721cdd7d7d0148d4545aa76a87
bluebill1049 <bluebill1049@hotmail.com>
2020-02-25 10:45:11 +1100

    check type

--- src/logic/getInputValue.ts
+++ src/logic/getInputValue.ts
@@ -3,7 +3,9 @@ import isUndefined from '../utils/isUndefined';
 import isObject from '../utils/isObject';
 
 export default (event: any, isCheckboxInput: boolean) =>
-  isNullOrUndefined(event) || !isObject(event.target)
+  isNullOrUndefined(event) ||
+  !isObject(event.target) ||
+  (isObject(event.target) && !event.target.type)
     ? event
     : isCheckboxInput || isUndefined(event.target.value)
     ? event.target.checked
