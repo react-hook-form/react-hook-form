d816af6110a387e916c0d9138a0679f9357617e1
bluebill1049 <bluebill1049@hotmail.com>
2020-02-25 10:46:35 +1100

    update unit tests

--- src/logic/getInputValue.test.ts
+++ src/logic/getInputValue.test.ts
@@ -1,8 +1,15 @@
 import getInputValue from './getInputValue';
 
 test('getInputValue should return correct value', () => {
-  expect(getInputValue({ target: { checked: true } }, false)).toEqual(true);
-  expect(getInputValue({ target: { value: 'test' } }, false)).toEqual('test');
+  expect(
+    getInputValue({ target: { checked: true, type: 'test' } }, false),
+  ).toEqual(true);
+  expect(getInputValue({ target: { checked: true } }, false)).toEqual({
+    target: { checked: true },
+  });
+  expect(
+    getInputValue({ target: { value: 'test', type: 'test' } }, false),
+  ).toEqual('test');
   expect(getInputValue({ data: 'test' }, false)).toEqual({ data: 'test' });
   expect(getInputValue(undefined, false)).toEqual(undefined);
   expect(getInputValue(null, false)).toEqual(null);
