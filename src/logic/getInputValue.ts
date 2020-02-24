9dc92e523c597f320fc5e97e9950331b0b72da61
bluebill1049 <bluebill1049@hotmail.com>
2020-02-25 10:43:45 +1100

    Revert "include target type check"
    This reverts commit b109595a6fe8da33dc00a54de299213541c136da.

--- src/logic/getInputValue.ts
+++ src/logic/getInputValue.ts
@@ -1,12 +1,10 @@
+import isNullOrUndefined from '../utils/isNullOrUndefined';
+import isUndefined from '../utils/isUndefined';
 import isObject from '../utils/isObject';
-import isPrimitive from '../utils/isPrimitive';
 
 export default (event: any, isCheckboxInput: boolean) =>
-  !isPrimitive(event) &&
-  isObject(event.target) &&
-  (event.target.value || event.target.checked) &&
-  event.target.type
-    ? isCheckboxInput
-      ? event.target.checked
-      : event.target.value
-    : event;
+  isNullOrUndefined(event) || !isObject(event.target)
+    ? event
+    : isCheckboxInput || isUndefined(event.target.value)
+    ? event.target.checked
+    : event.target.value;
