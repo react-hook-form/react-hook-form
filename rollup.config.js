16464f832e233731c0bc647bcd27ff89c71150bd
bluebill1049 <bluebill1049@hotmail.com>
2020-03-02 16:42:43 +1100

    install google closure compiler

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from 'rollup-plugin-typescript2';
+import compiler from '@ampproject/rollup-plugin-closure-compiler';
 import pkg from './package.json';
 
 export function getConfig({
@@ -24,7 +25,8 @@ export function getConfig({
         tsconfig,
         clean: true,
       }),
-      ...plugins
+      compiler(),
+      ...plugins,
     ],
     output,
   };
