d45d627e984366e9e7eb33effac91f5e3083beec
Kotaro Sugawara <kotarella1110@gmail.com>
2020-03-19 13:32:50 +0900

    fix EmptyObject type

--- src/types.ts
+++ src/types.ts
@@ -43,7 +43,7 @@ export type SchemaValidateOptions = Partial<{
   context: any;
 }>;
 
-export type EmptyObject = { [key: string]: never };
+export type EmptyObject = { [key in string | number]: never };
 
 export type SchemaValidationSuccess<
   FormValues extends FieldValues = FieldValues
