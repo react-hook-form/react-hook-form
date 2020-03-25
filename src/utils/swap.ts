872eeddeccff0ca43b7ef8804434b5d9d606d67e
Nick Artemov <alukard1986@gmail.com>
2020-03-26 00:25:27 +0200

    fix #1276 a issue caused by testcafe's runtime (#1277)
    Co-authored-by: Mykola Artemov <mykola.artemov@digitallyinspired.com>

--- src/utils/swap.ts
+++ src/utils/swap.ts
@@ -1,5 +1,9 @@
 import isArray from './isArray';
 
-export default (fields: any, indexA: number, indexB: number) =>
-  isArray(fields) &&
-  ([fields[indexA], fields[indexB]] = [fields[indexB], fields[indexA]]);
+export default (fields: any, indexA: number, indexB: number) => {
+  if (isArray(fields)) {
+    const temp = [fields[indexB], fields[indexA]];
+    fields[indexA] = temp[0];
+    fields[indexB] = temp[1];
+  }
+};
