16b8712a390be24522bec0508e3ec01b7f5576c7
Bill <bluebill1049@hotmail.com>
2020-04-03 12:16:38 +1100

    🐞 fix/compare error object (#1326)
    * improve TS
    
    * fix #1325 issue around errors object compare

--- src/utils/compareObject.test.ts
+++ src/utils/compareObject.test.ts
@@ -9,3 +9,9 @@ test('should return false when object are not the same', () => {
   expect(compareObject({ min: 'test' }, { min: 'test1' })).toBeFalsy();
   expect(compareObject({ min: true }, { min: false })).toBeFalsy();
 });
+
+test('should return false when object length is not equal', () => {
+  expect(
+    compareObject({ min: 'test' }, { min: 'test', what: 'test' }),
+  ).toBeFalsy();
+});
--- src/utils/compareObject.ts
+++ src/utils/compareObject.ts
@@ -1,6 +1,12 @@
-export default (objectA: any = {}, objectB: any = {}) =>
-  Object.entries(objectA).reduce(
-    (previous, [key, value]) =>
-      previous ? objectB[key] && objectB[key] === value : false,
-    true,
+export default (objectA: any = {}, objectB: any = {}) => {
+  const objectAData = Object.entries(objectA);
+  const objectBData = Object.keys(objectB);
+  return (
+    objectAData.length === objectBData.length &&
+    objectAData.reduce(
+      (previous, [key, value]) =>
+        previous ? objectB[key] && objectB[key] === value : false,
+      true,
+    )
   );
+};
