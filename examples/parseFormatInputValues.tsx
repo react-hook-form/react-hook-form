f169fd7174dfc33f3b157c7df8ca96833dd386e7
Bill <bluebill1049@hotmail.com>
2020-04-03 14:59:21 +1100

    Update parseFormatInputValues.tsx

--- examples/parseFormatInputValues.tsx
+++ examples/parseFormatInputValues.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Controller, useForm } from 'react-hook-form';
 
 const ParseFormatTextarea = ({ value = [], onChange }) => {
   const [text, setText] = React.useState<string>(value.join("\n"));
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const value = e.target.value;
+    const value = e.target.value.split("\n");
 
     setText(value);
-    onChange(value.split("\n"));
+    onChange(value);
   };
 
   return <textarea onChange={handleChange} value={text} />;
@@ -18,22 +17,20 @@ const ParseFormatTextarea = ({ value = [], onChange }) => {
 export default function App() {
   const { errors, control, handleSubmit } = useForm();
   const onSubmit = data => {
-    alert(JSON.stringify(data));
+    console.log(data);
   };
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {errors.emails && (
-          <ul>
-            {errors.emails.filter(message => !!message).map(({ message }, index) => (
-              <li>Email #{index}: {message}</li>
-            ))}
-          </ul>
-        )}
-        <Controller name="emails" as={ParseFormatTextarea} control={control} />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {errors.emails && (
+        <ul>
+          {errors.emails.filter(message => !!message).map(({ message }, index) => (
+            <li>Email #{index}: {message}</li>
+          ))}
+        </ul>
+      )}
+      <Controller name="emails" as={ParseFormatTextarea} control={control} />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
