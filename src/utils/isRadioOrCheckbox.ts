16e9c629b065add6869897cfecad60ace1107f7d
Bill <bluebill1049@hotmail.com>
2020-04-04 23:28:31 +1100

    Update src/utils/isRadioOrCheckbox.ts
    Co-Authored-By: Kotaro Sugawara <kotarella1110@gmail.com>

--- src/utils/isRadioOrCheckbox.ts
+++ src/utils/isRadioOrCheckbox.ts
@@ -2,5 +2,5 @@ import isRadioInput from './isRadioInput';
 import isCheckBoxInput from './isCheckBoxInput';
 import { FieldElement } from '../types';
 
-export default (ref: FieldElement): boolean =>
+export default (ref: FieldElement): ref is HTMLInputElement =>
   isRadioInput(ref) || isCheckBoxInput(ref);
