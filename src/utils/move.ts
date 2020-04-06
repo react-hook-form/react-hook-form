d9168418b4b02d2093ae99ba888e355f65592b6a
Kotaro Sugawara <kotarella1110@gmail.com>
2020-04-06 22:21:30 +0900

    remove any from move

--- src/utils/move.ts
+++ src/utils/move.ts
@@ -1,4 +1,4 @@
 import isArray from './isArray';
 
-export default (data: any, from: number, to: number) =>
+export default <T>(data: T[], from: number, to: number): T[] =>
   isArray(data) ? data.splice(to, 0, data.splice(from, 1)[0]) : [];
