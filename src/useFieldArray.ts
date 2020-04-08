1b7ab8ebb92e0902e0f32058a8f800f16ac40768
Kotaro Sugawara <kotarella1110@gmail.com>
2020-04-08 22:28:01 +0900

    remove any from useFieldArray

--- src/useFieldArray.ts
+++ src/useFieldArray.ts
@@ -54,7 +54,7 @@ export const useFieldArray = <
     fieldArrayDefaultValues,
     validateSchemaIsValid,
   } = control || methods.control;
-  const memoizedDefaultValues = useRef(
+  const memoizedDefaultValues = useRef<Partial<FormArrayValues>[]>(
     fieldArrayDefaultValues.current[name] || [
       ...get(defaultValuesRef.current, name, []),
     ],
@@ -77,7 +77,9 @@ export const useFieldArray = <
   const appendValueWithKey = (value: Partial<FormArrayValues>[]) =>
     value.map((v: Partial<FormArrayValues>) => appendId(v, keyName));
 
-  const commonTasks = (fieldsValues: any) => {
+  const commonTasks = (
+    fieldsValues: Partial<ArrayField<FormArrayValues, KeyName>>[],
+  ) => {
     setField(fieldsValues);
 
     if (readFormStateRef.current.isValid && validateSchemaIsValid) {
@@ -105,7 +107,9 @@ export const useFieldArray = <
   };
 
   const mapCurrentFieldsValueWithState = () => {
-    const currentFieldsValue = getValues({ nest: true })[name];
+    const currentFieldsValue: Partial<FormArrayValues>[] = getValues({
+      nest: true,
+    })[name];
 
     if (isArray(currentFieldsValue)) {
       for (let i = 0; i < currentFieldsValue.length; i++) {
@@ -187,7 +191,7 @@ export const useFieldArray = <
     }
 
     if (readFormStateRef.current.dirty) {
-      dirtyFieldsRef.current.forEach((dirtyField) => {
+      dirtyFieldsRef.current.forEach(dirtyField => {
         if (isUndefined(name) || dirtyField.startsWith(`${name}[${index}]`)) {
           dirtyFieldsRef.current.delete(dirtyField);
         }
