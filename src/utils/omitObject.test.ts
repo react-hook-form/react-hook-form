ca16ee9d89a4abd99ebeca8e8c8c38600d231e8c
bluebill1049 <bluebill1049@hotmail.com>
2020-04-11 10:53:35 +1000

    code clean up and remove file is not used

--- src/utils/omitObject.test.ts
+++
@@ -1,9 +0,0 @@
-import omitObject from './omitObject';
-
-describe('omitObject', () => {
-  it('should omit object correct', () => {
-    expect(omitObject({ test: 'what', test1: 'test1' }, 'test')).toEqual({
-      test1: 'test1',
-    });
-  });
-});
