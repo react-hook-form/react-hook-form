868f208a98b5291a23595a557c032205551bfdaa
Beier Luo <bluebill1049@hotmail.com>
2020-04-12 09:33:53 +1000

    update build

--- rollup.min.config.js
+++ rollup.min.config.js
@@ -1,6 +1,5 @@
 import typescript from 'rollup-plugin-typescript2';
 import compiler from "@ampproject/rollup-plugin-closure-compiler";
-import cleanup from 'rollup-plugin-cleanup';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
@@ -9,7 +8,6 @@ export default {
     typescript(),
     compiler(),
     terser(),
-    cleanup(),
   ],
   external: ['react'],
   output: [
