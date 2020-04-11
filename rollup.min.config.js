384c31244ffbb0109aec654a158babe93faa7829
bluebill1049 <bluebill1049@hotmail.com>
2020-04-11 20:24:55 +1000

    fix with min build

--- rollup.min.config.js
+++ rollup.min.config.js
@@ -2,12 +2,9 @@ import typescript from 'rollup-plugin-typescript2';
 import compiler from "@ampproject/rollup-plugin-closure-compiler";
 
 export default {
-  tsconfig: './tsconfig.json',
   input: 'src/index.ts',
   plugins: [
-    typescript({
-      tsconfig,
-    }),
+    typescript(),
     compiler(),
   ],
   external: ['react'],
