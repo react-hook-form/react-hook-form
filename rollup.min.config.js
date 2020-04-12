cd9bc3615906ed0db0ad840903d222aae8b668e8
Jerome De Leon <32805276+JeromeDeLeon@users.noreply.github.com>
2020-04-12 12:45:54 +0800

    Remove react-dom to rollup min config
    Forgot we have three configs :laughing:

--- rollup.min.config.js
+++ rollup.min.config.js
@@ -4,7 +4,7 @@ import { terser } from 'rollup-plugin-terser';
 export default {
   input: 'src/index.ts',
   plugins: [typescript(), terser()],
-  external: ['react', 'react-dom'],
+  external: ['react'],
   output: [
     {
       file: 'dist/react-hook-form.min.es.js',
