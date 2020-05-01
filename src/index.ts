96f87630e74e076c2da768d1ee9b010cadf932be
Kotaro Sugawara <kotarella1110@gmail.com>
2020-05-01 09:10:51 +0900

    🛎 remove ErrorMessage component (#1544)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export * from './useForm';
 export * from './useFieldArray';
 export * from './controller';
-export * from './errorMessage';
 export * from './useFormContext';
 export * from './types';
 export * from './contextTypes';
