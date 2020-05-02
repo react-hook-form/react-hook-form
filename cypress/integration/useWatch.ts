767e9525bf2b43f29bebcade73ac0e691aad41d4
bluebill1049 <bluebill1049@hotmail.com>
2020-05-02 11:08:25 +1000

    fix automation

--- cypress/integration/useWatch.ts
+++ cypress/integration/useWatch.ts
@@ -7,14 +7,14 @@ context('useWatch', () => {
     cy.get('#childCounter').contains('1');
     cy.get('#grandChildCounter').contains('2');
     cy.get('#grandChild1Counter').contains('2');
-    cy.get('#grandChild2Counter').contains('1');
+    cy.get('#grandChild2Counter').contains('2');
     cy.get('#grandchild01').contains('t');
     cy.get('#grandchild00').contains('t');
 
     cy.get('input[name="test"]').type('h');
     cy.get('#grandchild00').contains('th');
     cy.get('#grandchild01').contains('th');
-    cy.get('#grandchild2').should('be.empty');
+    cy.get('#grandchild2').contains('t');
   });
 
   it('should only trigger render when interact with input 2', () => {
@@ -25,24 +25,24 @@ context('useWatch', () => {
     cy.get('#childCounter').contains('1');
     cy.get('#grandChildCounter').contains('1');
     cy.get('#grandChild1Counter').contains('2');
-    cy.get('#grandChild2Counter').contains('1');
+    cy.get('#grandChild2Counter').contains('2');
 
     cy.get('input[name="test1"]').type('h');
     cy.get('input[name="test"]').type('h');
     cy.get('#grandchild00').contains('h');
     cy.get('#grandchild01').contains('h');
     cy.get('#grandchild1').contains('hh');
-    cy.get('#grandchild2').should('be.empty');
+    cy.get('#grandchild2').contains('hhh');
   });
 
   it('should only trigger render when interact with input 3', () => {
     cy.visit('http://localhost:3000/useWatch');
     cy.get('input[name="test2"]').type('e');
 
-    cy.get('#parentCounter').contains('2');
-    cy.get('#childCounter').contains('2');
-    cy.get('#grandChildCounter').contains('2');
-    cy.get('#grandChild1Counter').contains('2');
+    cy.get('#parentCounter').contains('1');
+    cy.get('#childCounter').contains('1');
+    cy.get('#grandChildCounter').contains('1');
+    cy.get('#grandChild1Counter').contains('1');
     cy.get('#grandChild2Counter').contains('2');
 
     cy.get('input[name="test2"]').type('eh');
@@ -52,6 +52,6 @@ context('useWatch', () => {
     cy.get('#grandchild00').contains('eh');
     cy.get('#grandchild01').contains('eh');
     cy.get('#grandchild1').contains('eh');
-    cy.get('#grandchild2').should('be.empty');
+    cy.get('#grandchild2').contains('eheheeh');
   });
 });
