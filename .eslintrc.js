2c0e44cc22aec7d4ef8a631e14ed560e866d0b53
Bill <bluebill1049@hotmail.com>
2020-06-13 23:28:11 +1000

    🪒 remove duplicated config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
     '@typescript-eslint/ban-ts-ignore': 'warn',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-object-literal-type-assertion': 'off',
     '@typescript-eslint/generic-type-naming': [
       'error',
