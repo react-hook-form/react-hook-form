aa20b9f4672f35363787a9c06e44b57560dc1f12
keiya sasaki <34934510+keiya01@users.noreply.github.com>
2020-06-20 10:25:16 +0900

    🎯 improve unit tests coverage (#1923)
    * increased attachEventListeners test
    
    * refactor findRemovedFieldAndRemoveLitener
    
    * increased findRemovedFieldAndRemoveListener test
    
    * update snapshot for findRemovedFieldAndRemoveListener
    
    * increased mapIds test
    
    * increased skipValidation

--- src/logic/attachEventListeners.test.ts
+++ src/logic/attachEventListeners.test.ts
@@ -1,8 +1,8 @@
 import attachEventListeners from './attachEventListeners';
+import isHTMLElement from '../utils/isHTMLElement';
 
-jest.mock('../utils/isHTMLElement', () => ({
-  default: () => true,
-}));
+jest.mock('../utils/isHTMLElement');
+(isHTMLElement as any).mockReturnValue(true);
 
 describe('attachEventListeners', () => {
   it('should attach change event for radio and return undefined', () => {
@@ -208,18 +208,50 @@ describe('attachEventListeners', () => {
     expect(fields.test.eventAttached).toBeTruthy();
   });
 
-  it('should return undefined when addEventListener is not found', () => {
+  it('should not call addEventListener if ref is not HTMLElement type', () => {
+    (isHTMLElement as any).mockReturnValue(false);
+    const addEventListener = jest.fn();
+    const fields = {
+      test: {
+        ref: {
+          name: 'test',
+          addEventListener,
+        },
+        eventAttached: [],
+        name: 'test',
+      },
+    };
+
     expect(
       attachEventListeners({
-        field: {
-          ref: {
-            name: 'test',
-            addEventListener,
-          },
-        },
+        field: fields.test as any,
         isRadioOrCheckbox: false,
         handleChange: () => {},
       }),
     ).toBeUndefined();
+    expect(addEventListener).not.toBeCalled();
+  });
+
+  it('should not call addEventListener if handleChange is undefined', () => {
+    (isHTMLElement as any).mockReturnValue(false);
+    const addEventListener = jest.fn();
+    const fields = {
+      test: {
+        ref: {
+          name: 'test',
+          addEventListener,
+        },
+        eventAttached: [],
+        name: 'test',
+      },
+    };
+
+    expect(
+      attachEventListeners({
+        field: fields.test as any,
+        isRadioOrCheckbox: false,
+      }),
+    ).toBeUndefined();
+    expect(addEventListener).not.toBeCalled();
   });
 });
