41edf1e3c011f41e469850f29920e53241ca998c
keiya01 <keiya.s.0210@gmail.com>
2020-06-20 12:10:54 +0900

    reduced code with unset

--- src/utils/unset.ts
+++ src/utils/unset.ts
@@ -25,14 +25,7 @@ function baseSlice(array: string | string[], start: number, end: number) {
   let index = -1;
   let length = array.length;
 
-  if (start < 0) {
-    start = -start > length ? 0 : length + start;
-  }
-  end = end > length ? length : end;
-  if (end < 0) {
-    end += length;
-  }
-  length = start > end ? 0 : end - start;
+  length = end + length - start || 0;
 
   const result = Array(length);
   while (++index < length) {
