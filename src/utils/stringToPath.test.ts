f033f8e4d2b86679c3002f5ba29ef7581eed92a5
Beier Luo <bluebill1049@hotmail.com>
2020-06-21 21:10:31 +1000

    improve stringToPath and build

---
+++ src/utils/stringToPath.test.ts
@@ -0,0 +1,12 @@
+import stringToPath from './stringToPath';
+
+describe('stringToPath', () => {
+  it('should convert string to path', () => {
+    expect(stringToPath('test.test[2].data')).toEqual([
+      'test',
+      'test',
+      2,
+      'data',
+    ]);
+  });
+});
