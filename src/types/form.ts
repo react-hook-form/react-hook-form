ade10a5e3361e4434b17a228473b85556ebbfa7d
Kotaro Sugawara <kotarella1110@gmail.com>
2020-06-25 15:24:42 +0900

    revert removed getValues from control

--- src/types/form.ts
+++ src/types/form.ts
@@ -226,7 +226,7 @@ export type FieldValuesFromControl<
 
 export type Control<TFieldValues extends FieldValues = FieldValues> = Pick<
   UseFormMethods<TFieldValues>,
-  'register' | 'unregister' | 'setValue' | 'trigger' | 'formState'
+  'register' | 'unregister' | 'setValue' | 'getValues' | 'trigger' | 'formState'
 > & {
   reRender: () => void;
   removeFieldEventListener: (field: Field, forceDelete?: boolean) => void;
