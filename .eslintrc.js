5855bf44707a5ea83ea75a20d3031260a509fbf6
Kotaro Sugawara <kotarella1110@gmail.com>
2020-06-29 12:37:33 +0900

    fix .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
