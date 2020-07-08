1c9a0b210bb4fe41cb2492a622acf3077b073162
Bill <bluebill1049@hotmail.com>
2020-07-08 17:49:37 +1000

    🐞 fix #2112 controller re-render bug (#2115)
    * fix #2112 controller re-render bug
    
    * make sure multiple inputs update will trigger re-render
    
    * fix automation render counts

--- src/useForm.ts
+++ src/useForm.ts
@@ -410,10 +410,10 @@ export function useForm<
     ): boolean | void => {
       if (fieldsRef.current[name]) {
         setFieldValue(fieldsRef.current[name] as Field, value);
-
         return config.shouldDirty && setDirty(name);
       } else if (!isPrimitive(value)) {
         setInternalValues(name, value, config);
+        return true;
       }
 
       unmountFieldsStateRef.current[name] = value;
@@ -456,7 +456,6 @@ export function useForm<
   ): void {
     const shouldRender =
       setInternalValue(name, value as TFieldValues[string], config) ||
-      !isPrimitive(value) ||
       isFieldWatched(name);
 
     renderWatchedInputs(name);
