8380cc0c15b199bef0f10580ddcad653f15ab2ef
keiya sasaki <34934510+keiya01@users.noreply.github.com>
2020-07-11 17:02:08 +0900

    :santa: Improve jest config (#2165)
    * fixed display color
    
    * fixed command

--- jest.config.js
+++ jest.config.js
@@ -21,21 +21,30 @@ const common = {
 
 const web = {
   ...common,
-  displayName: 'Web',
+  displayName: {
+    name: 'Web',
+    color: 'cyan',
+  },
   testMatch: [getTestMatch()],
   setupFilesAfterEnv: ['<rootDir>/setup.ts'],
 };
 
 const server = {
   ...common,
-  displayName: 'Server',
+  displayName: {
+    name: 'Server',
+    color: 'blue',
+  },
   testMatch: [getTestMatch('server')],
   testEnvironment: 'node',
 };
 
 const native = {
   ...common,
-  displayName: 'Native',
+  displayName: {
+    name: 'Native',
+    color: 'magenta',
+  },
   preset: '@testing-library/react-native',
   testMatch: [getTestMatch('native')],
   transform: {
@@ -56,6 +65,22 @@ const native = {
   setupFilesAfterEnv: ['<rootDir>/setup.native.ts'],
 };
 
+const getProjects = () => {
+  const testEnv = process.env.TEST_ENV;
+  if (!testEnv) {
+    return [web, server, native];
+  }
+
+  switch (testEnv) {
+    case 'web':
+      return [web];
+    case 'server':
+      return [server];
+    case 'native':
+      return [native];
+  }
+};
+
 module.exports = {
-  projects: [web, server, native],
+  projects: getProjects(),
 };
