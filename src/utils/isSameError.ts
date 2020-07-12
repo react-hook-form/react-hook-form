5b22734deaa0abdb546548bd3a677ba91116cb1f
J30 <jerome.deleon107@gmail.com>
2020-07-12 13:50:33 +0800

    increase test coverage for isSameError

--- src/utils/isSameError.ts
+++ src/utils/isSameError.ts
@@ -3,12 +3,12 @@ import { FieldError } from '../types/form';
 
 export default (
   error: FieldError | undefined,
-  { type, types, message }: FieldError,
+  { type, types = {}, message }: FieldError,
 ): boolean =>
   isObject(error) &&
   error.type === type &&
   error.message === message &&
-  Object.keys(error.types || {}).length === Object.keys(types || {}).length &&
+  Object.keys(error.types || {}).length === Object.keys(types).length &&
   Object.entries(error.types || {}).every(
-    ([key, value]) => (types || {})[key] === value,
+    ([key, value]) => types[key] === value,
   );
