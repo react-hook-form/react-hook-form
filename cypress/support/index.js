ca327b868078f85d9669f35814ed5df5f703e2b7
bluebill1049 <bluebill1049@hotmail.com>
2020-07-14 18:50:26 +1000

    remove cypress support folder

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import 'cypress/support/commands';
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
