addef46028e34d1e99f4442f409a69b78c44aa91
bluebill1049 <bluebill1049@hotmail.com>
2020-07-17 13:09:17 +1000

    update type

--- src/utils/move.ts
+++ src/utils/move.ts
@@ -1,7 +1,7 @@
 import isUndefined from './isUndefined';
 import isArray from './isArray';
 
-export default <T>(data: T[], from: number, to: number): T[] => {
+export default <T>(data: T[], from: number, to: number): (T | undefined)[] => {
   if (isArray(data)) {
     if (isUndefined(data[to])) {
       data[to] = undefined as any;
