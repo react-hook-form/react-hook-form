56ec4f787aab8a087c561c2a7f8b87d56973ca9f
Kotaro Sugawara <kotarella1110@gmail.com>
2020-08-08 02:01:50 +0900

    create global.d.ts for ReactNative

---
+++ src/types/global.d.ts
@@ -0,0 +1,13 @@
+/*
+React Hook Form projects that don't include the DOM library need these interfaces to compile.
+React Native applications use React, but there is no DOM available. The JavaScript runtime
+is ES6/ES2015 only. These definitions allow such projects to compile with only `--lib ES6`.
+
+Warning: all of these interfaces are empty. If you want type definitions for various properties,
+you need to add `--lib DOM` (via command line or tsconfig.json).
+*/
+
+interface Node {}
+interface MutationObserverInit {}
+interface HTMLOptionsCollection {}
+interface FileList {}
