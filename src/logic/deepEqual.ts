c0400c6f61ee2a29f5a9dbf4debd5d28219b62b7
bluebill1049 <bluebill1049@hotmail.com>
2020-08-11 18:18:50 +1000

    fix #2548 issue with isDirty formState

---
+++ src/logic/deepEqual.ts
@@ -0,0 +1,27 @@
+import isObject from '../utils/isObject';
+import isArray from '../utils/isArray';
+
+export default function deepEqual(object1: any, object2: any) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    const val1 = object1[key];
+    const val2 = object2[key];
+    const areObjects =
+      (isObject(val1) || isArray(val1)) && (isObject(val2) || isArray(val2));
+
+    if (
+      (areObjects && !deepEqual(val1, val2)) ||
+      (!areObjects && val1 !== val2)
+    ) {
+      return false;
+    }
+  }
+
+  return true;
+}
