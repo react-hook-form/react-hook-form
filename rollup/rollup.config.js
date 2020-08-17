a5a62cafc249b917cc8fc11845d2d5c10aee75b6
Kotaro Sugawara <kotarella1110@gmail.com>
2020-08-17 23:22:30 +0900

    👷🏻‍♂️ improve rollup config (#2587)

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,24 +1,29 @@
 import { createRollupConfig } from './createRollupConfig';
 import pkg from '../package.json';
 
+const name = 'index';
 const options = [
   {
+    name,
     format: 'cjs',
     env: 'development',
     input: pkg.source,
   },
   {
+    name,
     format: 'cjs',
     env: 'production',
     input: pkg.source,
   },
-  { format: 'esm', input: pkg.source },
+  { name, format: 'esm', input: pkg.source },
   {
+    name,
     format: 'umd',
     env: 'development',
     input: pkg.source,
   },
   {
+    name,
     format: 'umd',
     env: 'production',
     input: pkg.source,
---
+++ rollup/safePackageName.js
@@ -0,0 +1,4 @@
+export const safePackageName = (name) =>
+  name
+    .toLowerCase()
+    .replace(/(^@.*\/)|((^[^a-zA-Z]+)|[^\w.-])|([^a-zA-Z0-9]+$)/g, '');
