9f60f93aa573cc2bdcb529498c793749526867a0
bluebill1049 <bluebill1049@hotmail.com>
2020-08-17 17:57:31 +1000

    fix bug with useFieldArray unmountedFormState

--- src/useFieldArray.ts
+++ src/useFieldArray.ts
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useFormContext } from './useFormContext';
 import { isMatchFieldArrayName } from './logic/isNameInFieldArray';
+import { deepMerge } from './utils/deepMerge';
 import generateId from './logic/generateId';
 import isObject from './utils/isObject';
 import deepEqual from './logic/deepEqual';
@@ -71,6 +72,8 @@ export const useFieldArray = <
     formStateRef: {
       current: { dirtyFields, touched, errors },
     },
+    shouldUnregister,
+    unmountFieldsStateRef,
     updateFormState,
     readFormStateRef,
     watchFieldsRef,
@@ -482,6 +485,11 @@ export const useFieldArray = <
     append: React.useCallback(append, [name, errors]),
     remove: React.useCallback(remove, [fields, name, errors]),
     insert: React.useCallback(insert, [name, errors]),
-    fields,
+    fields: shouldUnregister
+      ? fields
+      : (deepMerge(get(unmountFieldsStateRef.current, name), fields).slice(
+          0,
+          fields.length,
+        ) as Partial<ArrayField<TFieldArrayValues, TKeyName>>[]),
   };
 };
--- src/useForm.ts
+++ src/useForm.ts
@@ -1190,6 +1190,7 @@ export function useForm<
     defaultValuesRef,
     unmountFieldsStateRef,
     updateFormState,
+    shouldUnregister,
     validateResolver: resolver ? validateResolver : undefined,
     ...commonProps,
   };
