bcd3f24bf608e1ff6780770f58815e3c6654d611
bluebill1049 <bluebill1049@hotmail.com>
2020-08-21 19:11:56 +1000

    improve types

--- src/utils/onDomRemove.ts
+++ src/utils/onDomRemove.ts
@@ -1,10 +1,13 @@
 import * as React from 'react';
-import { FieldRefs } from '../types/form';
+import { Field, FieldRefs } from '../types/form';
 import isDetached from './isDetached';
 
 export default function onDomRemove<TFieldValues>(
   fieldsRef: React.MutableRefObject<FieldRefs<TFieldValues>>,
-  removeFieldEventListenerAndRef: any,
+  removeFieldEventListenerAndRef: (
+    field: Field | undefined,
+    forceDelete?: boolean,
+  ) => void,
 ): MutationObserver {
   const observer = new MutationObserver((): void => {
     for (const field of Object.values(fieldsRef.current)) {
