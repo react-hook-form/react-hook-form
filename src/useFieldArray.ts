f49e3a858296bc2195e7f92bb1ff49c962b252bd
bluebill1049 <bluebill1049@hotmail.com>
2020-08-25 20:58:42 +1000

    fix issue caught by dirty fields test

--- src/useFieldArray.ts
+++ src/useFieldArray.ts
@@ -468,7 +468,7 @@ export const useFieldArray = <
     swap: React.useCallback(swap, [name, errors]),
     move: React.useCallback(move, [name, errors]),
     prepend: React.useCallback(prepend, [name, errors]),
-    append: React.useCallback(append, [name, errors]),
+    append: React.useCallback(append, [name, errors, fields]),
     remove: React.useCallback(remove, [fields, name, errors]),
     insert: React.useCallback(insert, [name, errors]),
     fields,
