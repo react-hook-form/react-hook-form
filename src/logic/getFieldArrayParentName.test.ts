f120c2506df355c06f0bc02438fe4ee3ae7adc0a
Beier Luo <bluebill1049@hotmail.com>
2020-08-25 22:14:19 +1000

    🧪 minor test improvement

--- src/logic/getFieldArrayParentName.test.ts
+++ src/logic/getFieldArrayParentName.test.ts
@@ -3,6 +3,8 @@ import getFieldArrayParentName from './getFieldArrayParentName';
 describe('getFieldArrayParentName', () => {
   it('should return parent name when name is field array', () => {
     expect(getFieldArrayParentName('test[0]')).toBe('test');
+    expect(getFieldArrayParentName('test[0].data[0]')).toBe('test');
+    expect(getFieldArrayParentName('test.data[0]')).toBe('test.data');
   });
 
   it('should return empty string when name is not field array', () => {
