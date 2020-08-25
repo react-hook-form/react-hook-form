4acae8ef87a5c94f878d772c5345b74fce9662b7
Kotaro Sugawara <kotarella1110@gmail.com>
2020-08-25 12:41:41 +0900

    improve rollup config to support ie11

--- rollup/rollup.ie11.config.js
+++ rollup/rollup.ie11.config.js
@@ -1,5 +1,4 @@
 import { createRollupConfig } from './createRollupConfig';
-import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import pkg from '../package.json';
 
 const name = 'index';
@@ -23,28 +22,4 @@ const options = [
   },
 ];
 
-export default options.map((option) =>
-  createRollupConfig(option, (config) => ({
-    ...config,
-    plugins: config.plugins
-      .reduce(
-        (previous, current) => [
-          ...previous,
-          current,
-          current.name === 'rpt2' &&
-            getBabelOutputPlugin({
-              plugins: [
-                [
-                  '@babel/plugin-transform-runtime',
-                  {
-                    corejs: 3,
-                  },
-                ],
-              ],
-            }),
-        ],
-        [],
-      )
-      .filter(Boolean),
-  })),
-);
+export default options.map((option) => createRollupConfig(option));
