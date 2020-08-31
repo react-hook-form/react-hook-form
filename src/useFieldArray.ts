eb67340204a1caeaf9a0f4926414bd5e872538e3
Michał Bundyra <contact@webimpress.com>
2020-08-31 14:24:09 +0100

    Removes redundant dependencies in append and remove of useFieldArray
    Fixes #2778

--- src/useFieldArray.ts
+++ src/useFieldArray.ts
@@ -244,7 +244,11 @@ export const useFieldArray = <
       readFormStateRef.current.isDirty
     ) {
       set(dirtyFields, name, [
-        ...get(dirtyFields, name, fillEmptyArray(fields)),
+        ...get(
+          dirtyFields,
+          name,
+          fillEmptyArray(new Array(allFields.current.length)),
+        ),
         ...filterBooleanArray(value),
       ]);
       updateFormState({
@@ -426,8 +430,8 @@ export const useFieldArray = <
     swap: React.useCallback(swap, [name, errors]),
     move: React.useCallback(move, [name, errors]),
     prepend: React.useCallback(prepend, [name, errors]),
-    append: React.useCallback(append, [name, errors, fields]),
-    remove: React.useCallback(remove, [name, errors, fields]),
+    append: React.useCallback(append, [name, errors]),
+    remove: React.useCallback(remove, [name, errors]),
     insert: React.useCallback(insert, [name, errors]),
     fields,
   };
