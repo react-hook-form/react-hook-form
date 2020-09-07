69f862285af32439130ab02505a38e159a8d5a39
bluebill1049 <bluebill1049@hotmail.com>
2020-09-07 13:30:08 +1000

    minor type improvement

--- src/utils/get.ts
+++ src/utils/get.ts
@@ -2,7 +2,7 @@ import isUndefined from './isUndefined';
 import isNullOrUndefined from './isNullOrUndefined';
 import filterOutFalsy from './filterOutFalsy';
 
-export default (obj: any, path: string, defaultValue?: any) => {
+export default (obj: any, path: string, defaultValue?: unknown) => {
   const result = filterOutFalsy(path.split(/[,[\].]+?/)).reduce(
     (result, key) => (isNullOrUndefined(result) ? result : result[key]),
     obj,
