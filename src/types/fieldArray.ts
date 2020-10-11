5796df8ac223b931e92b36ed497ffd14e89391e1
Bill <bluebill1049@hotmail.com>
2020-10-11 11:45:03 +1100

    🥁 include Type useFieldArrayMethods (#3145)
    * include useFieldArrayMethods
    
    * fix type

--- src/types/fieldArray.ts
+++ src/types/fieldArray.ts
@@ -16,3 +16,26 @@ export type ArrayField<
   TFieldArrayValues extends FieldValues = FieldValues,
   TKeyName extends string = 'id'
 > = TFieldArrayValues & Record<TKeyName, string>;
+
+export type UseFieldArrayMethods<
+  TFieldArrayValues extends FieldValues = FieldValues,
+  TKeyName extends string = 'id'
+> = {
+  swap: (indexA: number, indexB: number) => void;
+  move: (indexA: number, indexB: number) => void;
+  prepend: (
+    value: Partial<TFieldArrayValues> | Partial<TFieldArrayValues>[],
+    shouldFocus?: boolean,
+  ) => void;
+  append: (
+    value: Partial<TFieldArrayValues> | Partial<TFieldArrayValues>[],
+    shouldFocus?: boolean,
+  ) => void;
+  remove: (index?: number | number[]) => void;
+  insert: (
+    index: number,
+    value: Partial<TFieldArrayValues> | Partial<TFieldArrayValues>[],
+    shouldFocus?: boolean,
+  ) => void;
+  fields: Partial<ArrayField<TFieldArrayValues, TKeyName>>[];
+};
