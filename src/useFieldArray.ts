1f7b7b5c63d3ebb48ab16410d624686d493665cb
Bill <bluebill1049@hotmail.com>
2020-10-28 22:27:33 +1100

    🐞 fix bug with useFieldArray shallow state
    * improve Controller focus on error
    
    * fix bug with useFieldArray shallow state
    
    * consistent code style for if
    
    * revert change

--- src/useFieldArray.ts
+++ src/useFieldArray.ts
@@ -313,12 +313,11 @@ export const useFieldArray = <
       });
     }
 
-    if (!shouldUnregister) {
-      shallowFieldsStateRef.current[name] = [
+    !shouldUnregister &&
+      set(shallowFieldsStateRef.current, name, [
         ...(shallowFieldsStateRef.current[name] || []),
         value,
-      ];
-    }
+      ]);
     focusIndexRef.current = shouldFocus ? allFields.current.length : -1;
   };
 
