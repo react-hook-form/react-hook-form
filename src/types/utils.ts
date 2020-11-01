db57885b6e93db9728198428ff399c0f046cacc7
Bill <bluebill1049@hotmail.com>
2020-11-02 09:37:53 +1100

    ⌨️ fix #3313 interface extend Record (#3318)

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -31,7 +31,7 @@ export type DeepPartial<T> = T extends Array<infer U>
   ? Array<DeepPartial<U>>
   : T extends ReadonlyArray<infer U>
   ? ReadonlyArray<DeepPartial<U>>
-  : T extends Record<string, unknown>
+  : T extends { [key in keyof T]: T[key] }
   ? {
       [K in keyof T]?: DeepPartial<T[K]>;
     }
