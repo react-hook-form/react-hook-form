6640953bc986bd5673b25c02b01fe15a6d9598e2
Bill <bluebill1049@hotmail.com>
2020-11-17 15:09:10 +1100

    📖 update example on defaultValues

--- examples/initalFormState.tsx
+++ examples/initalFormState.tsx
@@ -1,60 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import ReactDOM from "react-dom";
+import { useForm } from "react-hook-form";
 
-export default function App() {
+import "./styles.css";
+
+const defaultValues = {
+  firstName: "bill",
+  lastName: "luo",
+  email: "bluebill1049@hotmail.com"
+};
+
+function App() {
   const { register, handleSubmit } = useForm();
   // or you can set up the defaultValues at useForm
   // const { register, handleSubmit } = useForm({
-  //   defaultValues: {
-  //     firstName: 'Bill',
-  //     lastName: 'luo',
-  //   }
+  //   defaultValues,
   // });
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     alert(JSON.stringify(data));
-  };
-  const initialValues = {
-    firstName: 'bill',
-    lastName: 'luo',
-    email: 'bluebill1049@hotmail.com',
   };
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="firstName">First Name</label>
-          <input
-            defaultValue={initialValues.firstName}
-            name="firstName"
-            placeholder="bill"
-            ref={register}
-          />
-        </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        defaultValue={defaultValues.firstName}
+        name="firstName"
+        placeholder="bill"
+        ref={register}
+      />
 
-        <div>
-          <label htmlFor="lastName">Last Name</label>
-          <input
-            defaultValue={initialValues.lastName}
-            name="lastName"
-            placeholder="luo"
-            ref={register}
-          />
-        </div>
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        defaultValue={defaultValues.lastName}
+        name="lastName"
+        placeholder="luo"
+        ref={register}
+      />
 
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            defaultValue={initialValues.email}
-            name="email"
-            placeholder="bluebill1049@hotmail.com"
-            type="email"
-            ref={register}
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      <label htmlFor="email">Email</label>
+      <input
+        defaultValue={defaultValues.email}
+        name="email"
+        placeholder="bluebill1049@hotmail.com"
+        type="email"
+        ref={register}
+      />
+
+      <input type="submit" />
+    </form>
   );
 }
