2b74dd6a1af0565d18a04af028123207cfbafc04
bluebill1049 <bluebill1049@hotmail.com>
2020-11-20 22:50:24 +1100

    test for code reduce

--- src/utils/remove.ts
+++ src/utils/remove.ts
@@ -1,19 +1,17 @@
 import isUndefined from './isUndefined';
 import compact from './compact';
 
-function removeAtIndexes<T>(data: T[], indexes: number[]): T[] {
-  let i = 0;
-  const temp = [...data];
-
-  for (const index of indexes) {
-    temp.splice(index - i, 1);
-    i++;
-  }
-
-  return compact(temp).length ? temp : [];
-}
-
 export default <T>(data: T[], index?: number | number[]): T[] =>
   isUndefined(index)
     ? []
-    : removeAtIndexes(data, (Array.isArray(index) ? index : [index]).sort());
+    : ((data: T[], indexes: number[]): T[] => {
+        let i = 0;
+        const temp = [...data];
+
+        for (const index of indexes) {
+          temp.splice(index - i, 1);
+          i++;
+        }
+
+        return compact(temp).length ? temp : [];
+      })(data, (Array.isArray(index) ? index : [index]).sort());
