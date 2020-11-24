83041c97e6bf60a282e51965388fba63e8a81cc7
Beier Luo <bluebill1049@hotmail.com>
2020-11-24 18:09:06 +1100

    fix #3508 state update with watch

--- src/useFieldArray.test.tsx
+++ src/useFieldArray.test.tsx
@@ -784,6 +784,7 @@ describe('useFieldArray', () => {
             control,
             formState: tempFormState,
             setValue: tempSetValue,
+            watch,
           } = useForm({
             defaultValues: {
               test: [
@@ -794,6 +795,7 @@ describe('useFieldArray', () => {
             },
           });
           const { fields } = useFieldArray({ name: 'test', control });
+          watch();
 
           setValue = tempSetValue;
           formState = tempFormState;
