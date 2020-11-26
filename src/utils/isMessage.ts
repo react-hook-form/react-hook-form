279061c7df264dd0549d050a294e57dc509b3447
Bill <bluebill1049@hotmail.com>
2020-11-26 21:16:51 +1100

    🧛‍♂️ minor code reduction (#3526)
    * minor code adjustment
    
    * make assign watch field simple
    
    * covert to array function
    
    * simplify fillBooleanArray
    
    * remove isKey Array check
    
    * fix isMessage function to have React isValidElement inline

--- src/utils/isMessage.ts
+++ src/utils/isMessage.ts
@@ -1,7 +1,6 @@
-import { isValidElement } from 'react';
+import * as React from 'react';
 import isString from '../utils/isString';
-import isObject from '../utils/isObject';
 import { Message } from '../types';
 
 export default (value: unknown): value is Message =>
-  isString(value) || (isObject(value) && isValidElement(value));
+  isString(value) || React.isValidElement(value as JSX.Element);
