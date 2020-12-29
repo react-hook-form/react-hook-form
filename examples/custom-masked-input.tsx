89d3977e4e729fec369e1d41ff3c26872a69c036
Bill <bluebill1049@hotmail.com>
2020-12-29 11:10:30 +1100

    focus on the usage

--- examples/custom-masked-input.tsx
+++ examples/custom-masked-input.tsx
@@ -1,78 +1,5 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import styled from "styled-components";
-
-export const Wrapper = styled.div`
-  position: relative;
-  margin: 10px;
-`;
-export const Form = styled.form``;
-export const Button = styled.button`
-  font-size: 14px;
-  font-weight: 600;
-  border-radius: 4px;
-  border: none;
-  height: 50px;
-  width: 100%;
-  color: #474c51;
-  margin-top: 10px;
-  cursor: pointer;
-
-  &:focus {
-    outline: 0;
-  }
-`;
-
-export const InputWrapper = styled.div`
-  position: relative;
-  margin: 20px 0;
-`;
-
-export const Label = styled.label`
-  font-size: 14px;
-  font-weight: 600;
-  color: #999999;
-  position: absolute;
-  pointer-events: none;
-  top: 18px;
-  left: 19px;
-  transition: all 0.2s ease;
-  opacity: 0.5;
-  display: flex;
-  align-items: center;
-  padding: 0 10px;
-`;
-
-export const Input = styled.input`
-  border-radius: 4px;
-  height: 50px;
-  border: 1px solid #c7c7c7;
-  font-size: 14px;
-  font-weight: 600;
-  color: #474c51;
-  width: 100%;
-  padding-left: 19px;
-
-  &:focus,
-  &:active {
-    outline: 0;
-  }
-
-  &:focus + ${Label}, &:not(:placeholder-shown) + ${Label} {
-    opacity: 1;
-    transform: translateY(-170%);
-    font-size: 12px;
-    font-weight: 600;
-    background-color: white;
-  }
-
-  /* For IE Browsers*/
-  &:focus + ${Label}, &:not(:-ms-input-placeholder) + ${Label} {
-    opacity: 1;
-    transform: translateY(-130%);
-    background-color: white;
-  }
-`;
 
 export const maskPhoneNumber = (phone) => {
   //Example: 0(999) 999 99 99
@@ -88,26 +15,19 @@ export const maskPhoneNumber = (phone) => {
 
 export default function App() {
   const { register, handleSubmit, setValue } = useForm();
-
   const onSubmit = (data) => console.log(data);
 
   return (
-    <Wrapper>
-      <Form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-        <InputWrapper>
-          <Input
-            type="tel"
-            placeholder=" "
-            name="phoneNumber"
-            ref={register}
-            onChange={(e) =>
-              setValue("phoneNumber", maskPhoneNumber(e.target.value))
-            }
-          />
-          <Label>Cep Telefonu Numarası</Label>
-          <Button>Kaydet</Button>
-        </InputWrapper>
-      </Form>
-    </Wrapper>
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="tel"
+        name="phoneNumber"
+        ref={register}
+        onChange={(e) =>
+          setValue("phoneNumber", maskPhoneNumber(e.target.value))
+        }
+      />
+      <button>Submit</button>
+    </Form>
   );
 };
