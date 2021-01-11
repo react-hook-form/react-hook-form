7ef9d875c174abd83a7e03381db9e0114a7a2c9d
Beier Luo <bluebill1049@hotmail.com>
2021-01-12 09:38:03 +1100

    fix cypress test with contains

--- cypress/integration/useFieldArrayNested.tsx
+++ cypress/integration/useFieldArrayNested.tsx
@@ -216,24 +216,24 @@ describe('useFieldArrayNested', () => {
       '{"test":[{"firstName":true,"lastName":true,"keyValue":[{"name":true},{"name":true}]},{"firstName":true,"lastName":true},{"firstName":true,"lastName":true},{"firstName":true,"lastName":true}]}',
     );
 
-    // cy.get('#remove').click();
-    // cy.get('#remove').click();
-    // cy.get('#remove').click();
-    //
-    // cy.get('#dirty-nested-0').contains(
-    //   '{"test":[{"firstName":true,"lastName":true,"keyValue":[{"name":true},{"name":true}]}]}',
-    // );
-    //
-    // cy.get('#touched-nested-0').contains('{"test":[{"firstName":true}]}');
-    //
-    // cy.get('#count-nest-0').contains('24');
-    //
-    // cy.get('#removeAll').click();
-    //
-    // cy.get('#dirty-nested-0').should('not.exist');
-    //
-    // cy.get('#touched-nested-0').should('not.exist');
-    //
-    // cy.get('#count').contains('9');
+    cy.get('#remove').click();
+    cy.get('#remove').click();
+    cy.get('#remove').click();
+
+    cy.get('#dirty-nested-0').contains(
+      '{"test":[{"firstName":true,"lastName":true,"keyValue":[{"name":true},{"name":true}]}]}',
+    );
+
+    cy.get('#touched-nested-0').contains('{"test":[{"firstName":true}]}');
+
+    cy.get('#count-nest-0').contains('24');
+
+    cy.get('#removeAll').click();
+
+    cy.get('#dirty-nested-0').should('not.exist');
+
+    cy.get('#touched-nested-0').should('not.exist');
+
+    cy.get('#count').contains('9');
   });
 });
