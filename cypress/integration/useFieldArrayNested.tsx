172e03f3326297ee1d1c6b3547c1c3232a16fd0a
Beier Luo <bluebill1049@hotmail.com>
2021-01-11 22:52:11 +1100

    test cypress build

--- cypress/integration/useFieldArrayNested.tsx
+++ cypress/integration/useFieldArrayNested.tsx
@@ -165,21 +165,21 @@ describe('useFieldArrayNested', () => {
     //   'has.value',
     //   'append',
     // );
-    //
-    // cy.get('#touched-nested-3').should(($state) =>
-    //   expect(JSON.parse($state.text())).to.be.deep.equal({
-    //     test: [
-    //       {
-    //         firstName: true,
-    //         keyValue: [{ name: true }, { name: true }, { name: true }],
-    //       },
-    //       { firstName: true },
-    //       { firstName: true },
-    //       { keyValue: [{ name: true }, { name: true }] },
-    //     ],
-    //   }),
-    // );
-    //
+
+    cy.get('#touched-nested-3').should(($state) =>
+      expect(JSON.parse($state.text())).to.be.deep.equal({
+        test: [
+          {
+            firstName: true,
+            keyValue: [{ name: true }, { name: true }, { name: true }],
+          },
+          { firstName: true },
+          { firstName: true },
+          { keyValue: [{ name: true }, { name: true }] },
+        ],
+      }),
+    );
+
     // cy.get('#dirty-nested-0').should(($state) =>
     //   expect(JSON.parse($state.text())).to.be.deep.equal({
     //     test: [
