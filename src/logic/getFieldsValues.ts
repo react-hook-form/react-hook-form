fe4380ff528c6c6e7b2f584660429aec4c48ddd7
Beier Luo <bluebill1049@hotmail.com>
2021-01-12 17:05:57 +1100

    remove transformToNestObject

--- src/logic/getFieldsValues.ts
+++ src/logic/getFieldsValues.ts
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import transformToNestObject from './transformToNestObject';
+import set from '../utils/set';
 import { FieldRefs } from '../types';
 
-export default (fieldsRef: React.MutableRefObject<FieldRefs>) => {
+export default (fieldsRef: React.MutableRefObject<FieldRefs>): any => {
   const output: Record<string, unknown> = {};
 
   for (const name in fieldsRef.current) {
     const field = fieldsRef.current[name]!.__field;
 
     if (field && !field.ref.disabled) {
-      output[name] = field.value;
+      set(output, name, field.value);
     }
   }
 
-  return transformToNestObject(output);
+  return output;
 };
