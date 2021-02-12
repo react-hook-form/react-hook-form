54a46211320ef3794f6ba00706b9c4ccdfd32fa3
Bill <bluebill1049@hotmail.com>
2021-02-12 22:00:22 +1100

    improve controller with native checkbox (#4176)
    * improve controller with native checkbox
    
    * revert example
    
    * revert test app
    
    * fix getControllerValue test
    
    * include unit test for checkbox with useController

--- src/__tests__/logic/getControllerValue.test.ts
+++ src/__tests__/logic/getControllerValue.test.ts
@@ -2,11 +2,15 @@ import getControllerValue from '../../logic/getControllerValue';
 
 test('getControllerValue should return correct value', () => {
   expect(
-    getControllerValue({ target: { checked: true }, type: 'test' }),
+    getControllerValue({
+      target: { checked: true, type: 'checkbox' },
+    }),
   ).toEqual(true);
-  expect(getControllerValue({ target: { checked: true } })).toEqual({
-    target: { checked: true },
-  });
+  expect(
+    getControllerValue({
+      target: { checked: true, type: 'checkbox', value: 'test' },
+    }),
+  ).toEqual(true);
   expect(
     getControllerValue({ target: { value: 'test' }, type: 'test' }),
   ).toEqual('test');
--- src/__tests__/useController.test.tsx
+++ src/__tests__/useController.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useForm } from '../useForm';
 import { useController } from '../useController';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 
 describe('useController', () => {
   it('should render input correctly', () => {
@@ -21,5 +21,127 @@ describe('useController', () => {
     };
 
     render(<Component />);
+  });
+
+  describe('checkbox', () => {
+    it('should work for checkbox by spread the field object', async () => {
+      const watchResult: unknown[] = [];
+      const Component = () => {
+        const { control, watch } = useForm<{
+          test: string;
+        }>();
+
+        watchResult.push(watch());
+
+        const { field } = useController({
+          name: 'test',
+          control,
+          defaultValue: '',
+        });
+
+        return <input type="checkbox" {...field} />;
+      };
+
+      render(<Component />);
+
+      expect(watchResult).toEqual([{}]);
+
+      await act(async () => {
+        fireEvent.click(screen.getByRole('checkbox'));
+      });
+
+      expect(watchResult).toEqual([{}, { test: true }]);
+
+      await act(async () => {
+        fireEvent.click(screen.getByRole('checkbox'));
+      });
+
+      expect(watchResult).toEqual([{}, { test: true }, { test: false }]);
+    });
+
+    it('should work for checkbox by assign checked', async () => {
+      const watchResult: unknown[] = [];
+      const Component = () => {
+        const { control, watch } = useForm<{
+          test: string;
+        }>();
+
+        watchResult.push(watch());
+
+        const { field } = useController({
+          name: 'test',
+          control,
+          defaultValue: '',
+        });
+
+        return (
+          <input
+            type="checkbox"
+            checked={field.value}
+            onChange={(e) => field.onChange(e.target.checked)}
+          />
+        );
+      };
+
+      render(<Component />);
+
+      expect(watchResult).toEqual([{}]);
+
+      await act(async () => {
+        fireEvent.click(screen.getByRole('checkbox'));
+      });
+
+      expect(watchResult).toEqual([{}, { test: true }]);
+
+      await act(async () => {
+        fireEvent.click(screen.getByRole('checkbox'));
+      });
+
+      expect(watchResult).toEqual([{}, { test: true }, { test: false }]);
+    });
+
+    it('should work for checkbox by assign value manually', async () => {
+      const watchResult: unknown[] = [];
+      const Component = () => {
+        const { control, watch } = useForm<{
+          test: string;
+        }>();
+
+        watchResult.push(watch());
+
+        const { field } = useController({
+          name: 'test',
+          control,
+          defaultValue: '',
+        });
+
+        return (
+          <input
+            value="on"
+            type="checkbox"
+            checked={field.value}
+            onChange={(e) =>
+              field.onChange(e.target.checked ? e.target.value : false)
+            }
+          />
+        );
+      };
+
+      render(<Component />);
+
+      expect(watchResult).toEqual([{}]);
+
+      await act(async () => {
+        fireEvent.click(screen.getByRole('checkbox'));
+      });
+
+      expect(watchResult).toEqual([{}, { test: 'on' }]);
+
+      await act(async () => {
+        fireEvent.click(screen.getByRole('checkbox'));
+      });
+
+      expect(watchResult).toEqual([{}, { test: 'on' }, { test: false }]);
+    });
   });
 });
