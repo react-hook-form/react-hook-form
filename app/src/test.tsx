f00dab38d7600a11123068cb1613bb77b39f4162
Beier Luo <bluebill1049@hotmail.com>
2021-02-12 20:59:19 +1100

    revert example

--- app/src/test.tsx
+++ app/src/test.tsx
@@ -1,5 +1,44 @@
 import * as React from 'react';
-import { useController, useForm } from 'react-hook-form';
+import { useFieldArray, useForm } from 'react-hook-form';
+import { Control } from '../../src/types';
+
+type FormValues = {
+  nest: {
+    test: {
+      value: string;
+      nestedArray: {
+        value: string;
+      }[];
+    }[];
+  };
+};
+const ChildComponent = ({
+  index,
+  control,
+}: {
+  control: Control<FormValues>;
+  index: number;
+}) => {
+  const { fields } = useFieldArray<FormValues>({
+    name: `nest.test.${index}.nestedArray` as const,
+    control,
+  });
+
+  return (
+    <div>
+      {fields.map((item, i) => (
+        <input
+          key={item.id}
+          {...control.register(
+            `nest.test.${index}.nestedArray.${i}.value` as const,
+          )}
+          // @ts-ignore
+          defaultValue={item.value}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Component = () => {
   const { control, watch } = useForm<{
