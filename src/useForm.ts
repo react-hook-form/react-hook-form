cc732cccedac8ad67f9dadf0dbe1526cfd5d1bfd
Beier Luo <bluebill1049@hotmail.com>
2021-03-08 19:05:37 +1100

    fix index issue

--- src/useForm.ts
+++ src/useForm.ts
@@ -927,16 +927,16 @@ export function useForm<
     parentName = '',
   ) => {
     Array.isArray(values) &&
-      values.forEach((appendValueItem, localIndex) =>
+      values.forEach((appendValueItem, valueIndex) =>
         Object.entries(appendValueItem).forEach((fieldNameAndValue) => {
           if (fieldNameAndValue) {
             const [key, value] = fieldNameAndValue;
             const inputName = `${parentName || name}.${
-              parentName ? localIndex : index
+              parentName ? valueIndex : index + valueIndex
             }.${key}`;
 
             Array.isArray(value)
-              ? registerFieldArray(name, value, localIndex, inputName)
+              ? registerFieldArray(name, value, valueIndex, inputName)
               : set(fieldsRef.current, inputName, {
                   _f: {
                     ref: {
