7314fa105ef26541fe3d212952990c734ab57318
Bill <bluebill1049@hotmail.com>
2021-03-25 09:01:07 +1100

    Update examples/V7/basicValidation.tsx
    Co-authored-by: Phillip ED Maier <pmaier983@gmail.com>

--- examples/V7/basicValidation.tsx
+++ examples/V7/basicValidation.tsx
@@ -15,21 +15,21 @@ export default function App() {
         <label>First name</label>
         <input
           type="text"
-          {...register('FirstName', { required: true, maxLength: 80 })}
+          {...register('firstName', { required: true, maxLength: 80 })}
         />
       </div>
       <div>
         <label>Last name</label>
         <input
           type="text"
-          {...register('LastName', { required: true, maxLength: 100 })}
+          {...register('lastName', { required: true, maxLength: 100 })}
         />
       </div>
       <div>
         <label>Email</label>
         <input
           type="text"
-          {...register('Email', {
+          {...register('email', {
             required: true,
             pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
           })}
@@ -39,7 +39,7 @@ export default function App() {
         <label>Mobile number</label>
         <input
           type="tel"
-          {...register('MobileNumber', {
+          {...register('mobileNumber', {
             required: true,
             maxLength: 11,
             minLength: 8,
@@ -48,7 +48,7 @@ export default function App() {
       </div>
       <div>
         <label>Title</label>
-        <select {...register('Title', { required: true })}>
+        <select {...register('title', { required: true })}>
           <option value="Mr">Mr</option>
           <option value="Mrs">Mrs</option>
           <option value="Miss">Miss</option>
