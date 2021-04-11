b075ffbc039e208f150783b50d4f35825a9c6c50
Beier Luo <bluebill1049@hotmail.com>
2021-04-11 12:42:29 +1000

    simplify the build

--- scripts/rollup/createRollupConfig.js
+++ scripts/rollup/createRollupConfig.js
@@ -1,43 +1,20 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import path from 'path';
 import external from 'rollup-plugin-peer-deps-external';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
-import ts from 'typescript';
-
-import pkg from '../../package.json';
-
-import { pascalcase } from './pascalcase';
-import { safePackageName } from './safePackageName';
 
 export function createRollupConfig(options, callback) {
-  const name = options.name || safePackageName(pkg.name);
-  const umdName = options.umdName || pascalcase(safePackageName(pkg.name));
-  const tsconfigPath = options.tsconfig || 'tsconfig.json';
-  const tsconfigJSON = ts.readConfigFile(tsconfigPath, ts.sys.readFile).config;
-  const tsCompilerOptions = ts.parseJsonConfigFileContent(
-    tsconfigJSON,
-    ts.sys,
-    './',
-  ).options;
-
-  const outputName = [
-    path.join(tsCompilerOptions.outDir, name),
-    options.formatName || options.format,
-    options.env,
-    'js',
-  ]
-    .filter(Boolean)
-    .join('.');
+  const name = options.name;
+  const outputName = 'dist/' + [name, options.format, 'js'].join('.');
 
   const config = {
     input: options.input,
     output: {
       file: outputName,
       format: options.format,
-      name: umdName,
+      name: 'ReactHookForm',
       sourcemap: true,
       globals: { react: 'React' },
       exports: 'named',
--- scripts/rollup/safePackageName.js
+++
@@ -1,4 +0,0 @@
-export const safePackageName = (name) =>
-  name
-    .toLowerCase()
-    .replace(/(^@.*\/)|((^[^a-zA-Z]+)|[^\w.-])|([^a-zA-Z0-9]+$)/g, '');
