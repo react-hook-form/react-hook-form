866e1f1dd626fd9848e4bbef3f79a84424fe1aae
Bill <bluebill1049@hotmail.com>
2021-04-19 23:46:25 +1000

    🐞 fix #4859 issue with reset trigger unregister subscription (#4860)
    * fix #4859 issue with reset trigger unregister subscribution
    
    * update snapshot
    
    * update tests

--- src/__tests__/controller.test.tsx
+++ src/__tests__/controller.test.tsx
@@ -463,7 +463,9 @@ describe('Controller', () => {
 
   it('should not set initial state from unmount state when input is part of field array', () => {
     const Component = () => {
-      const { control } = useForm();
+      const { control } = useForm<{
+        test: { value: string }[];
+      }>();
       const { fields, append, remove } = useFieldArray({
         name: 'test',
         control,
@@ -474,8 +476,8 @@ describe('Controller', () => {
           {fields.map((field, i) => (
             <Controller
               key={field.id}
-              defaultValue=""
-              name={`test.${i}.value`}
+              defaultValue={field.value}
+              name={`test.${i}.value` as const}
               render={({ field }) => <input {...field} />}
               control={control}
             />
@@ -500,7 +502,7 @@ describe('Controller', () => {
 
     fireEvent.click(screen.getByRole('button', { name: /append/i }));
 
-    expect(screen.getByRole('textbox')).not.toHaveValue();
+    expect(screen.getByRole('textbox')).toHaveValue('test');
   });
 
   it('should not assign default value when field is removed with useFieldArray', () => {
