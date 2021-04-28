623879800b0989c0e49f82a1b22edaaeb9aa7bc6
Beier Luo <bluebill1049@hotmail.com>
2021-04-28 20:39:20 +1000

    improve get

--- src/utils/get.ts
+++ src/utils/get.ts
@@ -1,12 +1,15 @@
 import compact from './compact';
 import isNullOrUndefined from './isNullOrUndefined';
 import isUndefined from './isUndefined';
+import isString from './isString';
 
 export default (obj: any = {}, path: string, defaultValue?: unknown) => {
-  const result = compact(path.split(/[,[\].]+?/)).reduce(
-    (result, key) => (isNullOrUndefined(result) ? result : result[key]),
-    obj,
-  );
+  const result =
+    isString(path) &&
+    compact(path.split(/[,[\].]+?/)).reduce(
+      (result, key) => (isNullOrUndefined(result) ? result : result[key]),
+      obj,
+    );
 
   return isUndefined(result) || result === obj
     ? isUndefined(obj[path])
