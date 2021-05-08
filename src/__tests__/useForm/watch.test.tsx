c42730af0a334bfb559c68c28ec630092dd758ba
Beier Luo <bluebill1049@hotmail.com>
2021-05-08 19:59:30 +1000

    include unit test for watch API

--- src/__tests__/useForm/watch.test.tsx
+++ src/__tests__/useForm/watch.test.tsx
@@ -351,4 +351,48 @@ describe('watch', () => {
 
     screen.getByText('False');
   });
+
+  describe('when defaultValuesStrategy set to deep', () => {
+    it('should return deeply nested field values with defaultValues', async () => {
+      let data;
+
+      function App() {
+        const { register, watch } = useForm<{
+          test: {
+            firstName: string;
+            lastName: string;
+          };
+        }>({
+          defaultValuesStrategy: 'deep',
+          defaultValues: {
+            test: { lastName: '', firstName: '' },
+          },
+        });
+        data = watch();
+
+        return (
+          <form>
+            <input {...register('test.lastName')} />
+          </form>
+        );
+      }
+
+      render(<App />);
+
+      await act(async () => {
+        fireEvent.change(screen.getByRole('textbox'), {
+          target: {
+            value: '1234',
+          },
+        });
+      });
+
+      expect(data).toEqual({
+        test: {
+          firstName: '',
+          lastName: '1234',
+        },
+      });
+    });
+  });
 });
