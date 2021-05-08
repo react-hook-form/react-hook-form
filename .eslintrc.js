5ae384b90c329a440820e193d35fc74e9fd03d87
Joris <reixjoris@gmail.com>
2021-05-08 23:24:48 +0200

    chore: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
     'plugin:cypress/recommended',
   ],
