b47b2501fd70215daa5cd95edf55a870e781d891
Beier Luo <bluebill1049@hotmail.com>
2021-06-02 22:27:32 +1000

    fix type

--- src/logic/removeAllEventListeners.ts
+++ src/logic/removeAllEventListeners.ts
@@ -6,7 +6,11 @@ export default (
   ref: Ref,
   validateWithStateUpdate?: EventListenerOrEventListenerObject,
 ): void => {
-  if (isHTMLElement(ref) && ref.removeEventListener) {
+  if (
+    isHTMLElement(ref) &&
+    ref.removeEventListener &&
+    validateWithStateUpdate
+  ) {
     ref.removeEventListener(EVENTS.INPUT, validateWithStateUpdate);
     ref.removeEventListener(EVENTS.CHANGE, validateWithStateUpdate);
     ref.removeEventListener(EVENTS.BLUR, validateWithStateUpdate);
