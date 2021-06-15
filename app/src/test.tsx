18a4b19de98f1e9b51d4f9c553224f5e1e9805aa
Beier Luo <bluebill1049@hotmail.com>
2021-06-15 13:01:23 +1000

    revert test file

--- app/src/test.tsx
+++ app/src/test.tsx
@@ -1,48 +1,87 @@
-// @ts-nocheck
 import * as React from 'react';
 import { useFieldArray, useForm } from 'react-hook-form';
 import { Control } from '../../src/types';
 
-const App = () => {
-  const { control, register, handleSubmit } = useForm<{
+type FormValues = {
+  nest: {
     test: {
-      yourDetails: {
-        firstName: string[];
-        lastName: string[];
-      };
+      value: string;
+      nestedArray: {
+        value: string;
+      }[];
     }[];
-  }>();
-  const { fields, append } = useFieldArray({
+  };
+};
+const ChildComponent = ({
+  index,
+  control,
+}: {
+  control: Control<FormValues>;
+  index: number;
+}) => {
+  const { fields } = useFieldArray<FormValues>({
+    name: `nest.test.${index}.nestedArray` as const,
     control,
-    name: 'test',
   });
 
   return (
-    <form
-      onSubmit={handleSubmit((data) => {
-        console.log('wtf', data);
-      })}
-    >
-      {fields.map((field, index) => {
-        console.log('render...');
-        return <div key={field.id} />;
-      })}
-      <button
-        type={'button'}
-        onClick={() => {
-          append({
-            yourDetails: {
-              firstName: ['test', 'test1'],
-              lastName: ['test', 'test1'],
-            },
-          });
-        }}
-      >
-        append
-      </button>
-      <button>submit</button>
-    </form>
+    <div>
+      {fields.map((item, i) => (
+        <input
+          key={item.id}
+          {...control.register(
+            `nest.test.${index}.nestedArray.${i}.value` as const,
+          )}
+          // @ts-ignore
+          defaultValue={item.value}
+        />
+      ))}
+    </div>
   );
 };
 
-export default App;
+const Component = () => {
+  const { register, control } = useForm({
+    defaultValues: {
+      nest: {
+        test: [
+          { value: '1', nestedArray: [{ value: '2' }] },
+          { value: '3', nestedArray: [{ value: '4' }] },
+        ],
+      },
+    },
+  });
+  const { fields, remove, append } = useFieldArray({
+    name: 'nest.test',
+    control,
+  });
+
+  return (
+    <div>
+      {fields.map((item, i) => (
+        <div key={item.id}>
+          <input
+            {...register(`nest.test.${i}.value` as const)}
+            defaultValue={item.value}
+          />
+
+          <ChildComponent control={control} index={i} />
+
+          <button
+            type={'button'}
+            onClick={() => remove(i)}
+            data-testid={item.value}
+          >
+            remove
+          </button>
+        </div>
+      ))}
+
+      <button type={'button'} onClick={() => append({ value: 'test' })}>
+        append
+      </button>
+    </div>
+  );
+};
+
+export default Component;
