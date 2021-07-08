b98848b05afdff669c309f515747e3c0e2e4d56b
Beier Luo <bluebill1049@hotmail.com>
2021-07-08 20:12:47 +1000

    - update packages to the latest - include more automation tests for watch/useWatch API

--- cypress/integration/watchUseFieldArrayNested.ts
+++ cypress/integration/watchUseFieldArrayNested.ts
@@ -16,12 +16,18 @@ describe('watchUseFieldArrayNested', () => {
       '[{"firstName":"Bill","keyValue":[{"name":"insert"},{"name":"prepend"},{"name":"1a"},{"name":"1c"},{"name":"append"}],"lastName":"Luo"}]',
     );
 
+    cy.get('#nest-update-0').click();
+
+    cy.get('#result').contains(
+      '[{"firstName":"Bill","keyValue":[{"name":"billUpdate"},{"name":"prepend"},{"name":"1a"},{"name":"1c"},{"name":"append"}],"lastName":"Luo"}]',
+    );
+
     cy.get(`#nest-remove-0`).click();
 
     cy.get('#submit').click();
 
     cy.get('#result').contains(
-      '[{"firstName":"Bill","keyValue":[{"name":"insert"},{"name":"1a"},{"name":"1c"},{"name":"append"}],"lastName":"Luo"}]',
+      '[{"firstName":"Bill","keyValue":[{"name":"billUpdate"},{"name":"1a"},{"name":"1c"},{"name":"append"}],"lastName":"Luo"}]',
     );
 
     cy.get('#prepend').click();
@@ -30,7 +36,7 @@ describe('watchUseFieldArrayNested', () => {
     cy.get('#insert').click();
 
     cy.get('#result').contains(
-      '[{"firstName":"prepend","keyValue":[]},{"firstName":"insert","keyValue":[]},{"firstName":"append","keyValue":[]},{"firstName":"Bill","keyValue":[{"name":"insert"},{"name":"1a"},{"name":"1c"},{"name":"append"}],"lastName":"Luo"}]',
+      '[{"firstName":"prepend","keyValue":[]},{"firstName":"insert","keyValue":[]},{"firstName":"append","keyValue":[]},{"firstName":"Bill","keyValue":[{"name":"billUpdate"},{"name":"1a"},{"name":"1c"},{"name":"append"}],"lastName":"Luo"}]',
     );
 
     cy.get(`#nest-append-0`).click();
@@ -40,14 +46,14 @@ describe('watchUseFieldArrayNested', () => {
     cy.get(`#nest-move-0`).click();
 
     cy.get('#result').contains(
-      '[{"firstName":"prepend","keyValue":[{"name":"insert"},{"name":"prepend"},{"name":"append"}]},{"firstName":"insert","keyValue":[]},{"firstName":"append","keyValue":[]},{"firstName":"Bill","keyValue":[{"name":"insert"},{"name":"1a"},{"name":"1c"},{"name":"append"}],"lastName":"Luo"}]',
+      '[{"firstName":"prepend","keyValue":[{"name":"insert"},{"name":"prepend"},{"name":"append"}]},{"firstName":"insert","keyValue":[]},{"firstName":"append","keyValue":[]},{"firstName":"Bill","keyValue":[{"name":"billUpdate"},{"name":"1a"},{"name":"1c"},{"name":"append"}],"lastName":"Luo"}]',
     );
 
     cy.get('#nest-remove-3').click();
     cy.get('#nest-remove-3').click();
 
     cy.get('#result').contains(
-      '[{"firstName":"prepend","keyValue":[{"name":"insert"},{"name":"prepend"},{"name":"append"}]},{"firstName":"insert","keyValue":[]},{"firstName":"append","keyValue":[]},{"firstName":"Bill","keyValue":[{"name":"insert"},{"name":"append"}],"lastName":"Luo"}]',
+      '[{"firstName":"prepend","keyValue":[{"name":"insert"},{"name":"prepend"},{"name":"append"}]},{"firstName":"insert","keyValue":[]},{"firstName":"append","keyValue":[]},{"firstName":"Bill","keyValue":[{"name":"billUpdate"},{"name":"append"}],"lastName":"Luo"}]',
     );
 
     cy.get('#nest-remove-all-3').click();
@@ -59,13 +65,19 @@ describe('watchUseFieldArrayNested', () => {
       '[{"firstName":"prepend","keyValue":[]},{"firstName":"insert","keyValue":[]},{"firstName":"append","keyValue":[]},{"firstName":"Bill","keyValue":[],"lastName":"Luo"}]',
     );
 
+    cy.get('#update').click();
+
+    cy.get('#result').contains(
+      '[{"firstName":"BillUpdate","keyValue":[]},{"firstName":"insert","keyValue":[]},{"firstName":"append","keyValue":[]},{"firstName":"Bill","keyValue":[],"lastName":"Luo"}]',
+    );
+
     cy.get('#remove').click();
     cy.get('#remove').click();
     cy.get('#remove').click();
 
-    cy.get('#result').contains('[{"firstName":"prepend","keyValue":[]}]');
+    cy.get('#result').contains('[{"firstName":"BillUpdate","keyValue":[]}]');
 
-    cy.get('#count').contains('33');
+    cy.get('#count').contains('36');
 
     cy.get('#removeAll').click();
 
