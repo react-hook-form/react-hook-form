1ac324d6a5441f5c20e4bec0d368a8a169e04b13
Bill <bluebill1049@hotmail.com>
2021-07-28 17:20:06 +1000

    🐞 fix #6105 deep equal issue with undefined node (#6107)

--- src/utils/deepEqual.ts
+++ src/utils/deepEqual.ts
@@ -1,15 +1,9 @@
-import * as React from 'react';
-
 import isObject from '../utils/isObject';
 
 import isDateObject from './isDateObject';
 import isPrimitive from './isPrimitive';
 
-export default function deepEqual(
-  object1: any,
-  object2: any,
-  isErrorObject?: boolean,
-) {
+export default function deepEqual(object1: any, object2: any) {
   if (
     isPrimitive(object1) ||
     isPrimitive(object2) ||
@@ -19,28 +13,30 @@ export default function deepEqual(
     return object1 === object2;
   }
 
-  if (!React.isValidElement(object1)) {
-    const keys1 = Object.keys(object1);
-    const keys2 = Object.keys(object2);
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
 
-    if (keys1.length !== keys2.length) {
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    const val1 = object1[key];
+
+    if (!keys2.includes(key)) {
       return false;
     }
 
-    for (const key of keys1) {
-      const val1 = object1[key];
+    if (key !== 'ref') {
+      const val2 = object2[key];
 
-      if (!(isErrorObject && key === 'ref')) {
-        const val2 = object2[key];
-
-        if (
-          (isObject(val1) || Array.isArray(val1)) &&
-          (isObject(val2) || Array.isArray(val2))
-            ? !deepEqual(val1, val2, isErrorObject)
-            : val1 !== val2
-        ) {
-          return false;
-        }
+      if (
+        (isObject(val1) || Array.isArray(val1)) &&
+        (isObject(val2) || Array.isArray(val2))
+          ? !deepEqual(val1, val2)
+          : val1 !== val2
+      ) {
+        return false;
       }
     }
   }
