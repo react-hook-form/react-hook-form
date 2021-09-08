10be65f59b170a205c03ad817977f67c0bfabd3b
Kotaro Sugawara <kotarella1110@gmail.com>
2021-09-09 00:38:51 +0900

    fix: add File and FileList interface for ReactNative

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,6 +1,26 @@
 import { FieldValues } from './fields';
 import { NestedValue } from './form';
 
+/*
+Projects that React Hook Form installed don't include the DOM library need these interfaces to compile.
+React Native applications is no DOM available. The JavaScript runtime is ES6/ES2015 only.
+These definitions allow such projects to compile with only --lib ES6.
+
+Warning: all of these interfaces are empty.
+If you want type definitions for various properties, you need to add `--lib DOM` (via command line or tsconfig.json).
+*/
+
+interface File extends Blob {
+  readonly lastModified: number;
+  readonly name: string;
+}
+
+interface FileList {
+  readonly length: number;
+  item(index: number): File | null;
+  [index: number]: File;
+}
+
 export type Primitive =
   | null
   | undefined
