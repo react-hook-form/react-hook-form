558e466c5cbae46c8942c6002dab0eadbe5b7c97
Yoswaris Lawpaiboon <22832362+kiznick@users.noreply.github.com>
2021-09-15 14:06:07 +0700

    Change Regex

--- src/__tests__/logic/validateField.test.tsx
+++ src/__tests__/logic/validateField.test.tsx
@@ -1028,8 +1028,7 @@ describe('validateField', () => {
 
   it('should return pattern error when not matching', async () => {
     const emailRegex =
-      /(?:[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFa-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFa-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFa-z0-9](?:[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFa-z0-9-]*[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFa-z0-9])?\.)+[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFa-z0-9](?:[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFa-z0-9-]*[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFa-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFa-z0-9-]*[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFa-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-
+     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFaa-zA-Z\-0-9]+\.)+[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFaa-zA-Z]{2,}))$/;
     expect(
       await validateField(
         {
