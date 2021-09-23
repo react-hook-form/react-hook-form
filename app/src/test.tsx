fed470ea6b99ed902857e6fbd62a12e905abe656
Beier Luo <bluebill1049@hotmail.com>
2021-09-23 19:34:57 +1000

    revert example

--- app/src/test.tsx
+++ app/src/test.tsx
@@ -1,52 +1,82 @@
-// @ts-nocheck
 import * as React from 'react';
 import { useFieldArray, useForm } from 'react-hook-form';
 import { Control } from '../../src/types';
-import { Controller } from '../../src';
 
-export default () => {
-  const {
+type FormData = {
+  nest: {
+    test: {
+      value: string;
+      nestedArray: {
+        value: string;
+      }[];
+    }[];
+  };
+};
+const ChildComponent = ({
+  index,
+  control,
+}: {
+  control: Control<FormData>;
+  index: number;
+}) => {
+  const { fields } = useFieldArray<FormData>({
+    name: `nest.test.${index}.nestedArray` as const,
     control,
-    watch,
-    formState: { isValid },
-  } = useForm({
-    mode: 'onChange',
-    shouldUnregister: true,
   });
-  const test = watch('test');
 
   return (
     <div>
-      <p>{isValid ? 'valid' : 'invalid'}</p>
-      <Controller
-        control={control}
-        rules={{ required: true }}
-        render={({ field }) => (
-          <select {...field} data-testid="select">
-            <option value={''}></option>
-            <option value={'test'}>test</option>
-            <option value={'test1'}>test1</option>
-          </select>
-        )}
-        name={'test'}
-      />
-
-      {test === 'test1' && (
-        <>
-          <Controller
-            control={control}
-            render={({ field }) => <input {...field} />}
-            rules={{ required: true }}
-            name={'first.test'}
-          />
-          <Controller
-            control={control}
-            render={({ field }) => <input {...field} />}
-            rules={{ required: true }}
-            name={'first.test1'}
-          />
-        </>
-      )}
+      {fields.map((item, i) => (
+        <input
+          key={item.id}
+          {...control.register(
+            `nest.test.${index}.nestedArray.${i}.value` as const,
+          )}
+        />
+      ))}
     </div>
   );
 };
+
+const Component = () => {
+  const { register, control } = useForm({
+    defaultValues: {
+      nest: {
+        test: [
+          { value: '1', nestedArray: [{ value: '2' }] },
+          { value: '3', nestedArray: [{ value: '4' }] },
+        ],
+      },
+    },
+  });
+  const { fields, remove, append } = useFieldArray({
+    name: 'nest.test',
+    control,
+  });
+
+  return (
+    <div>
+      {fields.map((item, i) => (
+        <div key={item.id}>
+          <input {...register(`nest.test.${i}.value` as const)} />
+
+          <ChildComponent control={control} index={i} />
+
+          <button
+            type={'button'}
+            onClick={() => remove(i)}
+            data-testid={item.value}
+          >
+            remove
+          </button>
+        </div>
+      ))}
+
+      <button type={'button'} onClick={() => append({ value: 'test' })}>
+        append
+      </button>
+    </div>
+  );
+};
+
+export default Component;
