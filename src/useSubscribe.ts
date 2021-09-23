d0a21758dfcfe01563125f6cd6d2cc29238a4395
Beier Luo <bluebill1049@hotmail.com>
2021-09-23 19:53:03 +1000

    remove extra ref

--- src/useSubscribe.ts
+++ src/useSubscribe.ts
@@ -11,26 +11,23 @@ type Props<T> = {
 export function useSubscribe<T>({ disabled, subject, callback }: Props<T>) {
   const _subscription = React.useRef(subject);
   const _unSubscribe = React.useRef<{ unsubscribe: TearDown }>();
-  const _isSubscribed = React.useRef(false);
 
   if (disabled) {
     if (_subscription.current) {
       _unSubscribe.current && _unSubscribe.current.unsubscribe();
       _subscription.current = _unSubscribe.current = undefined;
-      _isSubscribed.current = false;
     }
   } else {
     if (!_subscription.current) {
       _subscription.current = subject;
     }
 
-    if (!_isSubscribed.current) {
+    if (!_unSubscribe.current) {
       if (_subscription.current) {
         _unSubscribe.current = _subscription.current.subscribe({
           next: callback,
         });
       }
-      _isSubscribed.current = true;
     }
   }
 
