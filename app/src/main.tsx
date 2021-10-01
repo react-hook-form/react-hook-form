2060dcdd31062856d1267ba6a691c31995230fa6
Beier Luo <bluebill1049@hotmail.com>
2021-10-01 17:06:31 +1000

    remover render

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './app';
 
 const container = document.getElementById('root');
-ReactDOM.render(<App />, container);
 // @ts-ignore
 const root = ReactDOM.createRoot(container);
 root.render(<App />);
