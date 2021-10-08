410ab23e62f4bc59f0c6084a051a84cbfd5688be
Bill <bluebill1049@hotmail.com>
2021-10-08 23:27:36 +1100

    ⏮ revert interface & mapped type update (#6747)
    * Revert "🖌️ fix Path and ArrayPath types for defined value type with interface  (#6626)"
    
    This reverts commit ab106ab57f4ee049a4fec89bb83fb8deb1d34e31.
    
    * revert
    
    * fix error
    
    * fix review comment
    
    Co-authored-by: Kotaro Sugawara <kotarella1110@gmail.com>

--- src/types/form.ts
+++ src/types/form.ts
@@ -32,7 +32,9 @@ export type NestedValue<TValue extends object = object> = {
 
 export type UnpackNestedValue<T> = T extends NestedValue<infer U>
   ? U
-  : T extends ReadonlyArray<any> | Record<any, unknown>
+  : T extends Date | FileList | File
+  ? T
+  : T extends object
   ? { [K in keyof T]: UnpackNestedValue<T[K]> }
   : T;
 
--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,7 +1,27 @@
 import { FieldValues } from './fields';
 import { NestedValue } from './form';
 
+/*
+Projects that React Hook Form installed don't include the DOM library need these interfaces to compile.
+React Native applications is no DOM available. The JavaScript runtime is ES6/ES2015 only.
+These definitions allow such projects to compile with only --lib ES6.
+
+Warning: all of these interfaces are empty.
+If you want type definitions for various properties, you need to add `--lib DOM` (via command line or tsconfig.json).
+*/
+
 export type Noop = () => void;
+
+interface File extends Blob {
+  readonly lastModified: number;
+  readonly name: string;
+}
+
+interface FileList {
+  readonly length: number;
+  item(index: number): File | null;
+  [index: number]: File;
+}
 
 export type Primitive =
   | null
@@ -20,31 +40,21 @@ export type LiteralUnion<T extends U, U extends Primitive> =
   | T
   | (U & { _?: never });
 
-export type DeepPartialImpl<T> = T extends NestedValue
+export type DeepPartial<T> = T extends Date | FileList | File | NestedValue
   ? T
-  : T extends ReadonlyArray<any> | Record<any, unknown>
-  ? DeepPartial<T>
-  : T;
-
-export type DeepPartial<T> = {
-  [K in keyof T]?: DeepPartialImpl<T[K]>;
-};
+  : { [K in keyof T]?: DeepPartial<T[K]> };
 
 export type IsAny<T> = boolean extends (T extends never ? true : false)
   ? true
   : false;
 
-export type DeepMapImpl<T, TValue> = IsAny<T> extends true
+export type DeepMap<T, TValue> = IsAny<T> extends true
   ? any
-  : T extends NestedValue
+  : T extends Date | FileList | File | NestedValue
   ? TValue
-  : T extends ReadonlyArray<any> | Record<any, unknown>
-  ? DeepMap<T, TValue>
+  : T extends object
+  ? { [K in keyof T]: DeepMap<NonUndefined<T[K]>, TValue> }
   : TValue;
-
-export type DeepMap<T, TValue> = {
-  [K in keyof T]: DeepMapImpl<NonUndefined<T[K]>, TValue>;
-};
 
 export type IsFlatObject<T extends object> = Extract<
   Exclude<T[keyof T], NestedValue | Date | FileList>,
@@ -59,11 +69,9 @@ type IsTuple<T extends ReadonlyArray<any>> = number extends T['length']
 type TupleKey<T extends ReadonlyArray<any>> = Exclude<keyof T, keyof any[]>;
 type ArrayKey = number;
 
-type PathImpl<K extends string | number, V> = V extends
-  | ReadonlyArray<any>
-  | Record<any, unknown>
-  ? `${K}` | `${K}.${Path<V>}`
-  : `${K}`;
+type PathImpl<K extends string | number, V> = V extends Primitive
+  ? `${K}`
+  : `${K}` | `${K}.${Path<V>}`;
 
 export type Path<T> = T extends ReadonlyArray<infer V>
   ? IsTuple<T> extends true
@@ -77,15 +85,13 @@ export type Path<T> = T extends ReadonlyArray<infer V>
 
 export type FieldPath<TFieldValues extends FieldValues> = Path<TFieldValues>;
 
-type ArrayPathImpl<K extends string | number, V> = IsAny<V> extends true
-  ? `${K}` | `${K}.${ArrayPath<V>}`
+type ArrayPathImpl<K extends string | number, V> = V extends Primitive
+  ? never
   : V extends ReadonlyArray<infer U>
-  ? U extends ReadonlyArray<any> | Record<any, unknown>
-    ? `${K}` | `${K}.${ArrayPath<V>}`
-    : never
-  : V extends Record<any, unknown>
-  ? `${K}.${ArrayPath<V>}`
-  : never;
+  ? U extends Primitive
+    ? never
+    : `${K}` | `${K}.${ArrayPath<V>}`
+  : `${K}.${ArrayPath<V>}`;
 
 export type ArrayPath<T> = T extends ReadonlyArray<infer V>
   ? IsTuple<T> extends true
@@ -158,7 +164,7 @@ export type UnionLike<T> = [T] extends [Date | FileList | File | NestedValue]
   ? T
   : [T] extends [ReadonlyArray<any>]
   ? { [K in keyof T]: UnionLike<T[K]> }
-  : [T] extends [Record<any, unknown>]
+  : [T] extends [object]
   ? PartialBy<
       {
         [K in UnionKeys<T>]: UnionLike<UnionValues<T, K>>;
