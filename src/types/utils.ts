fed4b9ea17fe5f31ec522276be23349da10a3e07
Beier Luo <bluebill1049@hotmail.com>
2021-10-08 11:59:07 +1100

    revert

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,7 +1,25 @@
 import { FieldValues } from './fields';
 import { NestedValue } from './form';
 
-export type Noop = () => void;
+/*
+Projects that React Hook Form installed don't include the DOM library need these interfaces to compile.
+React Native applications is no DOM available. The JavaScript runtime is ES6/ES2015 only.
+These definitions allow such projects to compile with only --lib ES6.
+
+Warning: all of these interfaces are empty.
+If you want type definitions for various properties, you need to add `--lib DOM` (via command line or tsconfig.json).
+*/
+
+interface File extends Blob {
+  readonly lastModified: number;
+  readonly name: string;
+}
+
+interface FileList {
+  readonly length: number;
+  item(index: number): File | null;
+  [index: number]: File;
+}
 
 export type Primitive =
   | null
@@ -20,31 +38,21 @@ export type LiteralUnion<T extends U, U extends Primitive> =
   | T
   | (U & { _?: never });
 
-export type DeepPartialImpl<T> = T extends NestedValue
+export type DeepPartial<T> = T extends Date | FileList | File | NestedValue
   ? T
-  : T extends ReadonlyArray<any> | Record<any, unknown>
-  ? DeepPartial<T>
-  : T;
-
-export type DeepPartial<T> = {
-  [K in keyof T]?: DeepPartialImpl<T[K]>;
-};
+  : { [K in keyof T]?: DeepPartial<T[K]> };
 
 export type IsAny<T> = boolean extends (T extends never ? true : false)
   ? true
   : false;
 
-export type DeepMapImpl<T, TValue> = IsAny<T> extends true
+export type DeepMap<T, TValue> = IsAny<T> extends true
   ? any
-  : T extends NestedValue
+  : T extends Date | FileList | File | NestedValue
   ? TValue
-  : T extends ReadonlyArray<any> | Record<any, unknown>
-  ? DeepMap<T, TValue>
+  : T extends object
+  ? { [K in keyof T]: DeepMap<NonUndefined<T[K]>, TValue> }
   : TValue;
-
-export type DeepMap<T, TValue> = {
-  [K in keyof T]: DeepMapImpl<NonUndefined<T[K]>, TValue>;
-};
 
 export type IsFlatObject<T extends object> = Extract<
   Exclude<T[keyof T], NestedValue | Date | FileList>,
@@ -154,7 +162,7 @@ export type UnionLike<T> = [T] extends [Date | FileList | File | NestedValue]
   ? T
   : [T] extends [ReadonlyArray<any>]
   ? { [K in keyof T]: UnionLike<T[K]> }
-  : [T] extends [Record<any, unknown>]
+  : [T] extends [object]
   ? PartialBy<
       {
         [K in UnionKeys<T>]: UnionLike<UnionValues<T, K>>;
