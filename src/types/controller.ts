65e18362d81f77c5f05d0481766d523baf6d748d
Beier Luo <bluebill1049@hotmail.com>
2021-10-09 10:39:17 +1100

    Revert "Revert "✨  Add support for generic components using `FieldPathWithValue` (#6562)" (#6717)"
    This reverts commit 08883bb4d5bfe6898c0dfd82815afaeafb5e8531.

--- src/types/controller.ts
+++ src/types/controller.ts
@@ -1,39 +1,66 @@
 import * as React from 'react';
 
+import { FieldErrors } from '..';
+
 import { RegisterOptions } from './validator';
 import {
   Control,
   FieldError,
-  FieldPath,
   FieldPathValue,
+  FieldPathWithValue,
   FieldValues,
+  IsAny,
+  NestedValue,
   Noop,
+  Primitive,
   RefCallBack,
   UnpackNestedValue,
   UseFormStateReturn,
 } from './';
 
-export type ControllerFieldState = {
+export type ControllerFieldState<
+  TFieldValues extends FieldValues = FieldValues,
+  TResult = any,
+  TName extends FieldPathWithValue<TFieldValues, TResult> = FieldPathWithValue<
+    TFieldValues,
+    TResult
+  >,
+  FieldValuesAtPath = IsAny<TResult> extends true
+    ? FieldPathValue<TFieldValues, TName>
+    : TResult,
+> = {
   invalid: boolean;
   isTouched: boolean;
   isDirty: boolean;
-  error?: FieldError;
+  error?: FieldValuesAtPath extends NestedValue | Primitive
+    ? FieldError
+    : FieldErrors<FieldValuesAtPath>;
 };
 
 export type ControllerRenderProps<
   TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+  TResult = any,
+  TName extends FieldPathWithValue<TFieldValues, TResult> = FieldPathWithValue<
+    TFieldValues,
+    TResult
+  >,
 > = {
   onChange: (...event: any[]) => void;
   onBlur: Noop;
-  value: UnpackNestedValue<FieldPathValue<TFieldValues, TName>>;
+  value: IsAny<TResult> extends true
+    ? UnpackNestedValue<FieldPathValue<TFieldValues, TName>>
+    : UnpackNestedValue<TResult>;
   name: TName;
   ref: RefCallBack;
 };
 
 export type UseControllerProps<
   TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+  TResult = any,
+  TName extends FieldPathWithValue<TFieldValues, TResult> = FieldPathWithValue<
+    TFieldValues,
+    TResult
+  >,
 > = {
   name: TName;
   rules?: Omit<
@@ -41,30 +68,40 @@ export type UseControllerProps<
     'valueAsNumber' | 'valueAsDate' | 'setValueAs' | 'disabled'
   >;
   shouldUnregister?: boolean;
-  defaultValue?: UnpackNestedValue<FieldPathValue<TFieldValues, TName>>;
+  defaultValue?: IsAny<TResult> extends true
+    ? UnpackNestedValue<FieldPathValue<TFieldValues, TName>>
+    : UnpackNestedValue<TResult>;
   control?: Control<TFieldValues>;
 };
 
 export type UseControllerReturn<
   TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+  TResult = any,
+  TName extends FieldPathWithValue<TFieldValues, TResult> = FieldPathWithValue<
+    TFieldValues,
+    TResult
+  >,
 > = {
-  field: ControllerRenderProps<TFieldValues, TName>;
+  field: ControllerRenderProps<TFieldValues, TResult, TName>;
   formState: UseFormStateReturn<TFieldValues>;
-  fieldState: ControllerFieldState;
+  fieldState: ControllerFieldState<TFieldValues, TResult, TName>;
 };
 
 export type ControllerProps<
   TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+  TResult = any,
+  TName extends FieldPathWithValue<TFieldValues, TResult> = FieldPathWithValue<
+    TFieldValues,
+    TResult
+  >,
 > = {
   render: ({
     field,
     fieldState,
     formState,
   }: {
-    field: ControllerRenderProps<TFieldValues, TName>;
-    fieldState: ControllerFieldState;
+    field: ControllerRenderProps<TFieldValues, TResult, TName>;
+    fieldState: ControllerFieldState<TFieldValues, TResult, TName>;
     formState: UseFormStateReturn<TFieldValues>;
   }) => React.ReactElement;
-} & UseControllerProps<TFieldValues, TName>;
+} & UseControllerProps<TFieldValues, TResult, TName>;
