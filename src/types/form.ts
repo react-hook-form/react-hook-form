ae2cf286811e55d16bfedd67161522f4dbf9d119
Bill <bluebill1049@hotmail.com>
2021-10-13 22:46:10 +1100

    🗄️ fix file name consistency (#6795)

--- src/types/form.ts
+++ src/types/form.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { SubjectType, Subscription } from '../utils/Subject';
+import { SubjectType, Subscription } from '../utils/subject';
 
 import { ErrorOption, FieldError, FieldErrors } from './errors';
 import { EventType } from './events';
