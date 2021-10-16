3919bc83bf1943d05c9c77e39fc342c7663afed5
Beier Luo <bluebill1049@hotmail.com>
2021-10-17 09:09:28 +1100

    include test coverage

--- src/__tests__/type.test.tsx
+++ src/__tests__/type.test.tsx
@@ -1,4 +1,8 @@
+import * as React from 'react';
+
 import { Path } from '../types';
+import { useFieldArray } from '../useFieldArray';
+import { useForm } from '../useForm';
 
 test('should not throw type error with path name', () => {
   type MissingCompanyNamePath = Path<{
@@ -19,4 +23,37 @@ test('should not throw type error with path name', () => {
   ];
 
   test;
+});
+
+test('should not throw type error with nullable field array ', () => {
+  interface FormData {
+    name: string;
+    things?: Array<{ name: string }>;
+  }
+
+  function App() {
+    const { control, register } = useForm<FormData>({
+      defaultValues: { name: 'test' },
+    });
+    const { fields, append } = useFieldArray({ control, name: 'things' });
+
+    return (
+      <div className="App">
+        <input {...register('name')} />
+
+        <button onClick={() => append({ name: '' })}>Add</button>
+
+        {fields.map((field, index) => (
+          <div key={field.id}>
+            <input
+              {...register(`things.${index}.name`)}
+              defaultValue={field.name}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  App;
 });
