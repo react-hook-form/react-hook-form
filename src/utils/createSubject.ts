aadaf7d21bfc7208a0248d089d178b1ecf0333e9
Kotaro Sugawara <kotarella1110@gmail.com>
2021-10-22 19:42:48 +0900

    refactor: reflect feedback comments

--- src/utils/createSubject.ts
+++ src/utils/createSubject.ts
@@ -1,34 +1,85 @@
 import { Noop } from '../types';
 
-export type Observer<TValue> = {
-  next: (value: TValue) => void;
+export type Observer<T> = {
+  next: (value: T) => void;
 };
 
 export type TearDown = Noop;
 
 export type Subscription = {
-  unsubscribe: TearDown;
+  add: (tearDown: TearDown) => void;
+  unsubscribe: () => void;
 };
 
-export type SubjectType<TValue> = {
-  next: (value: TValue) => void;
-  subscribe: (value: Observer<TValue>) => Subscription;
+export type Subject<T> = {
+  next: (value: T) => void;
+  subscribe: (value: Observer<T>) => {
+    unsubscribe: TearDown;
+  };
+  unsubscribe: Noop;
 };
 
-export default function createSubject<TValue>(): SubjectType<TValue> {
-  let observers: Observer<TValue>[] = [];
+function createSubscription() {
+  let tearDowns: TearDown[] = [];
+
+  const add = (tearDown: TearDown) => {
+    tearDowns.push(tearDown);
+  };
+
+  const unsubscribe = () => {
+    for (const teardown of tearDowns) {
+      teardown();
+    }
+    tearDowns = [];
+  };
 
   return {
-    subscribe(observer: Observer<TValue>) {
-      observers.push(observer);
-      return {
-        unsubscribe() {
-          observers = observers.filter((l) => l !== observer);
-        },
-      };
-    },
-    next(value: TValue) {
-      observers.forEach((observer) => observer.next(value));
-    },
+    add,
+    unsubscribe,
+  };
+}
+
+function createSubscriber<T>(
+  observer: Observer<T>,
+  subscription: Subscription,
+): Observer<T> {
+  let closed = false;
+  subscription.add(() => (closed = true));
+
+  const next = (value: T) => {
+    if (!closed) {
+      observer.next(value);
+    }
+  };
+
+  return {
+    next,
+  };
+}
+
+export default function createSubject<T>(): Subject<T> {
+  let observers: Observer<T>[] = [];
+
+  const next = (value: T) => {
+    for (const observer of observers) {
+      observer.next(value);
+    }
+  };
+
+  const subscribe = (observer: Observer<T>) => {
+    const subscription = createSubscription();
+    const subscriber = createSubscriber(observer, subscription);
+    observers.push(subscriber);
+    return subscription;
+  };
+
+  const unsubscribe = () => {
+    observers = [];
+  };
+
+  return {
+    next,
+    subscribe,
+    unsubscribe,
   };
 }
