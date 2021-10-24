10e2ddde28e4e73e6a995058def9793283bb0e04
Kotaro Sugawara <kotarella1110@gmail.com>
2021-10-24 17:43:20 +0900

    test: improve createSubject test

--- src/__tests__/utils/createSubject.test.ts
+++ src/__tests__/utils/createSubject.test.ts
@@ -21,19 +21,38 @@ describe('createSubject', () => {
 
   it('should unsubscribe observers', () => {
     const subject = createSubject();
+    const next1 = jest.fn();
+    const next2 = jest.fn();
+
+    const subscription = subject.subscribe({
+      next: next1,
+    });
+
+    subject.subscribe({
+      next: next2,
+    });
+
+    subscription.unsubscribe();
+
+    subject.next(2);
+
+    expect(next1).not.toBeCalled();
+    expect(next2).toBeCalledWith(2);
+  });
+
+  it('should unsubscribe all observers', () => {
+    const subject = createSubject();
     const next = jest.fn();
 
-    const tearDown = subject.subscribe({
+    subject.subscribe({
       next,
     });
 
-    const tearDown1 = subject.subscribe({
+    subject.subscribe({
       next,
     });
 
-    tearDown.unsubscribe();
-
-    tearDown1.unsubscribe();
+    subject.unsubscribe();
 
     subject.next(2);
     subject.next(2);
