cc5fd648414cf3e5cf1f21af7b54ed590ddb63d1
Daniel Stockman <5605+evocateur@users.noreply.github.com>
2021-12-10 09:37:40 -0800

    fix(esm-interop): Import non-ESM React as default

--- src/useFormContext.tsx
+++ src/useFormContext.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 import omit from './utils/omit';
 import { FieldValues, FormProviderProps, UseFormReturn } from './types';
--- src/useWatch.ts
+++ src/useWatch.ts
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 import generateWatchOutput from './logic/generateWatchOutput';
 import shouldSubscribeByName from './logic/shouldSubscribeByName';
