4f846142f28f249a1056785ada68b3b233861a07
Felix Schorer <felixschorer@users.noreply.github.com>
2021-12-17 11:24:33 +0100

    Implement types for splitting and joining paths

--- src/types/path/internal.ts
+++ src/types/path/internal.ts
@@ -8,3 +8,32 @@ export type TupleKey<T extends ReadonlyArray<any>> = Exclude<
 >;
 
 export type ArrayKey = number;
+
+export type PathString = string;
+
+export type Key = string;
+export type AsKey<T> = Extract<T, Key>;
+
+export type KeyList = Key[];
+export type AsKeyList<T> = Extract<T, KeyList>;
+
+type SplitPathStringTailRecursive<
+  PS extends PathString,
+  KL extends KeyList,
+> = PS extends `${infer K}.${infer R}`
+  ? SplitPathStringTailRecursive<R, [...KL, K]>
+  : [...KL, PS];
+
+export type SplitPathString<PS extends PathString> =
+  SplitPathStringTailRecursive<PS, []>;
+
+type JoinKeyListTailRecursive<
+  KL extends KeyList,
+  PS extends PathString,
+> = KL extends [infer K, ...infer R]
+  ? JoinKeyListTailRecursive<AsKeyList<R>, `${PS}.${AsKey<K>}`>
+  : PS;
+
+export type JoinKeyList<KL extends KeyList> = KL extends [infer K, ...infer R]
+  ? JoinKeyListTailRecursive<AsKeyList<R>, AsKey<K>>
+  : never;
