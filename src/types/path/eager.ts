fde7540894cd98bd44a7712ca7fceb86e202ee0d
Felix Schorer <felixschorer@users.noreply.github.com>
2021-12-18 16:19:27 +0100

    Add doc strings for eager types

--- src/types/path/eager.ts
+++ src/types/path/eager.ts
@@ -9,10 +9,22 @@ import {
   TupleKey,
 } from './internal';
 
+/**
+ * Helper type for recursively constructing paths through a type.
+ * See {@link Path}
+ */
 type PathImpl<K extends string | number, V> = V extends Primitive
   ? `${K}`
   : `${K}` | `${K}.${Path<V>}`;
 
+/**
+ * Type which eagerly collects all paths through a type
+ * @typeParam T - type which should be introspected
+ * @example
+ * ```
+ * Path<{foo: {bar: string}}> = 'foo' | 'foo.bar'
+ * ```
+ */
 export type Path<T> = T extends ReadonlyArray<infer V>
   ? IsTuple<T> extends true
     ? {
@@ -23,8 +35,15 @@ export type Path<T> = T extends ReadonlyArray<infer V>
       [K in keyof T]-?: PathImpl<K & string, T[K]>;
     }[keyof T];
 
+/**
+ * See {@link Path}
+ */
 export type FieldPath<TFieldValues extends FieldValues> = Path<TFieldValues>;
 
+/**
+ * Helper type for recursively constructing paths through a type.
+ * See {@link ArrayPath}
+ */
 type ArrayPathImpl<K extends string | number, V> = V extends Primitive
   ? never
   : V extends ReadonlyArray<infer U>
@@ -33,6 +52,15 @@ type ArrayPathImpl<K extends string | number, V> = V extends Primitive
     : `${K}` | `${K}.${ArrayPath<V>}`
   : `${K}.${ArrayPath<V>}`;
 
+/**
+ * Type which eagerly collects all paths through a type which point to an array
+ * type.
+ * @typeParam T - type which should be introspected
+ * @example
+ * ```
+ * Path<{foo: {bar: string[], baz: number[]}}> = 'foo.bar' | 'foo.baz'
+ * ```
+ */
 export type ArrayPath<T> = T extends ReadonlyArray<infer V>
   ? IsTuple<T> extends true
     ? {
@@ -43,24 +71,53 @@ export type ArrayPath<T> = T extends ReadonlyArray<infer V>
       [K in keyof T]-?: ArrayPathImpl<K & string, T[K]>;
     }[keyof T];
 
+/**
+ * See {@link ArrayPath}
+ */
 export type FieldArrayPath<TFieldValues extends FieldValues> =
   ArrayPath<TFieldValues>;
 
+/**
+ * Type to evaluate the type which the given path points to.
+ * @typeParam T - deeply nested type which is indexed by the path
+ * @typeParam P - path into the deeply nested type
+ * @example
+ * ```
+ * PathValue<{foo: {bar: string}}, 'foo.bar'> = string
+ * PathValue<[number, string], '1'> = string
+ * ```
+ */
 export type PathValue<T, P extends Path<T> | ArrayPath<T>> = EvaluateKeyList<
   T,
   SplitPathString<P>
 >;
 
+/**
+ * See {@link PathValue}
+ */
 export type FieldPathValue<
   TFieldValues extends FieldValues,
   TFieldPath extends FieldPath<TFieldValues>,
 > = PathValue<TFieldValues, TFieldPath>;
 
+/**
+ * See {@link PathValue}
+ */
 export type FieldArrayPathValue<
   TFieldValues extends FieldValues,
   TFieldArrayPath extends FieldArrayPath<TFieldValues>,
 > = PathValue<TFieldValues, TFieldArrayPath>;
 
+/**
+ * Type to evaluate the type which the given paths point to.
+ * @typeParam TFieldValues - field values which are indexed by the paths
+ * @typeParam TPath        - paths into the deeply nested field values
+ * @example
+ * ```
+ * FieldPathValues<{foo: {bar: string}}, ['foo', 'foo.bar']>
+ *   = [{bar: string}, string]
+ * ```
+ */
 export type FieldPathValues<
   TFieldValues extends FieldValues,
   TPath extends FieldPath<TFieldValues>[] | readonly FieldPath<TFieldValues>[],
