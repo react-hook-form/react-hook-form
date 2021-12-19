c56272ea54365ccc4a6a9ed12cd874c9c620edb2
Felix Schorer <felixschorer@users.noreply.github.com>
2021-12-19 20:02:51 +0100

    Remove calls to describe / it

--- src/__tests__/types/path/common.test-d.ts
+++ src/__tests__/types/path/common.test-d.ts
@@ -19,166 +19,166 @@ import {
   type,
 } from '../__fixtures__';
 
-describe('IsTuple', () => {
-  it("should evaluate to true if it's a tuple", () => {
+/** {@link IsTuple} */ {
+  /** it should evaluate to true if it's a tuple */ {
     type Actual = IsTuple<[string, number]>;
     expectType<true>(type<Actual>());
-  });
+  }
 
-  it("should evaluate to false if it's not a tuple", () => {
+  /** it should evaluate to false if it's not a tuple */ {
     type Actual = IsTuple<string[]>;
     expectType<false>(type<Actual>());
-  });
-});
+  }
+}
 
-describe('TupleKey', () => {
-  it('should evaluate to the own keys of the tuple', () => {
+/** {@link TupleKey} */ {
+  /** it should evaluate to the own keys of the tuple */ {
     type Actual = TupleKey<[string, number]>;
     expectType<'0' | '1'>(type<Actual>());
-  });
+  }
 
-  it('should evaluate to never if an array type is passed', () => {
+  /** it should evaluate to never if an array type is passed */ {
     type Actual = TupleKey<string[]>;
     expectType<never>(type<Actual>());
-  });
-});
+  }
+}
 
-describe('AsKey', () => {
-  it('should behave like a noop type when a Key is passed', () => {
+/** {@link AsKey} */ {
+  /** it should behave like a noop type when a Key is passed */ {
     type Actual = AsKey<'foo'>;
     expectType<'foo'>(type<Actual>());
-  });
+  }
 
-  it('should evaluate to never if not a Key is passed', () => {
+  /** it should evaluate to never if not a Key is passed */ {
     type Actual = AsKey<boolean>;
     expectType<never>(type<Actual>());
-  });
-});
+  }
+}
 
-describe('ToKey', () => {
-  it('should behave like a noop type when a Key is passed', () => {
+/** {@link ToKey} */ {
+  /** it should behave like a noop type when a Key is passed */ {
     type Actual = ToKey<'foo'>;
     expectType<'foo'>(type<Actual>());
-  });
+  }
 
-  it('should evaluate to never if not a Key or ArrayKey is passed', () => {
+  /** it should evaluate to never if not a Key or ArrayKey is passed */ {
     type Actual = ToKey<boolean>;
     expectType<never>(type<Actual>());
-  });
+  }
 
-  it('should convert an ArrayKey to a template literal type', () => {
+  /** it should convert an ArrayKey to a template literal type */ {
     type Actual = ToKey<ArrayKey>;
     expectType<`${ArrayKey}`>(type<Actual>());
-  });
-});
+  }
+}
 
-describe('AsKeyList', () => {
-  it('should behave like a noop type when a KeyList is passed', () => {
+/** {@link AsKeyList} */ {
+  /** it should behave like a noop type when a KeyList is passed */ {
     type Actual = AsKeyList<['foo']>;
     expectType<['foo']>(type<Actual>());
-  });
+  }
 
-  it('should evaluate to never if not a KeyList is passed', () => {
+  /** it should evaluate to never if not a KeyList is passed */ {
     type Actual = AsKeyList<[42]>;
     expectType<never>(type<Actual>());
-  });
-});
+  }
+}
 
-describe('SplitPathString', () => {
-  it('should split the PathString', () => {
+/** {@link SplitPathString} */ {
+  /** it should split the PathString */ {
     type Actual = SplitPathString<'foo.bar.0.baz'>;
     expectType<['foo', 'bar', '0', 'baz']>(type<Actual>());
-  });
+  }
 
-  it('should split a PathString which does not contain a "."', () => {
+  /** it should split a PathString which does not contain a "." */ {
     type Actual = SplitPathString<'foo'>;
     expectType<['foo']>(type<Actual>());
-  });
+  }
 
-  it('should split a PathString containing only a "."', () => {
+  /** it should split a PathString containing only a "." */ {
     type Actual = SplitPathString<'.'>;
     expectType<['', '']>(type<Actual>());
-  });
+  }
 
-  it('should be implemented tail recursively', () => {
+  /** it should be implemented tail recursively */ {
     type Actual = SplitPathString<HundredPathString<'foo'>>;
     expectType<HundredTuple<'foo'>>(type<Actual>());
-  });
-});
+  }
+}
 
-describe('JoinKeyList', () => {
-  it('should join the KeyList', () => {
+/** {@link JoinKeyList} */ {
+  /** it should join the KeyList */ {
     type Actual = JoinKeyList<['foo', 'bar', '0', 'baz']>;
     expectType<'foo.bar.0.baz'>(type<Actual>());
-  });
+  }
 
-  it('should join a KeyList of length 1', () => {
+  /** it should join a KeyList of length 1 */ {
     type Actual = JoinKeyList<['foo']>;
     expectType<'foo'>(type<Actual>());
-  });
+  }
 
-  it('should evaluate to never when passed an empty KeyList', () => {
+  /** it should evaluate to never when passed an empty KeyList */ {
     type Actual = JoinKeyList<[]>;
     expectType<never>(type<Actual>());
-  });
+  }
 
-  it('should be implemented tail recursively', () => {
+  /** it should be implemented tail recursively */ {
     type Actual = JoinKeyList<HundredTuple<'foo'>>;
     expectType<HundredPathString<'foo'>>(type<Actual>());
-  });
-});
+  }
+}
 
-describe('EvaluateKey', () => {
-  it('should traverse an object', () => {
+/** {@link EvaluateKey} */ {
+  /** it should traverse an object */ {
     type Actual = EvaluateKey<{ foo: number; bar: string }, 'foo'>;
     expectType<number>(type<Actual>());
-  });
+  }
 
-  it('should traverse a tuple', () => {
+  /** it should traverse a tuple */ {
     type Actual = EvaluateKey<[boolean, string], '1'>;
     expectType<string>(type<Actual>());
-  });
+  }
 
-  it('should traverse an array', () => {
+  /** it should traverse an array */ {
     type Actual = EvaluateKey<boolean[], '42'>;
     expectType<boolean>(type<Actual>());
-  });
+  }
 
-  it('should evaluate to never if the key is not valid', () => {
+  /** it should evaluate to never if the key is not valid */ {
     type Actual = EvaluateKey<{ foo: string }, 'foobar'>;
     expectType<never>(type<Actual>());
-  });
-});
+  }
+}
 
-describe('EvaluateKeyList', () => {
-  it('should traverse an object', () => {
+/** {@link EvaluateKeyList} */ {
+  /** it should traverse an object */ {
     type Actual = EvaluateKeyList<
       InfiniteType<number>,
       ['foo', 'foo', 'value']
     >;
     expectType<number>(type<Actual>());
-  });
+  }
 
-  it('should traverse a tuple', () => {
+  /** it should traverse a tuple */ {
     type Actual = EvaluateKeyList<InfiniteType<boolean>, ['bar', '0', 'value']>;
     expectType<boolean>(type<Actual>());
-  });
+  }
 
-  it('should traverse an array', () => {
+  /** it should traverse an array */ {
     type Actual = EvaluateKeyList<
       InfiniteType<boolean>,
       ['baz', '42', 'value']
     >;
     expectType<boolean>(type<Actual>());
-  });
+  }
 
-  it('should evaluate to never if the path is not valid', () => {
+  /** it should evaluate to never if the path is not valid */ {
     type Actual = EvaluateKeyList<InfiniteType<string>, ['foobar']>;
     expectType<never>(type<Actual>());
-  });
+  }
 
-  it('should be implemented tail recursively', () => {
+  /** it should be implemented tail recursively */ {
     type Actual = EvaluateKeyList<InfiniteType<string>, HundredTuple<'foo'>>;
     expectType<InfiniteType<string>>(type<Actual>());
-  });
-});
+  }
+}
--- src/__tests__/types/path/lazy.test-d.ts
+++ src/__tests__/types/path/lazy.test-d.ts
@@ -10,87 +10,87 @@ import {
 } from '../../../types/path/lazy';
 import { HundredTuple, InfiniteType, type } from '../__fixtures__';
 
-describe('Keys', () => {
-  it('should return the keys of an object', () => {
+/** {@link Keys} */ {
+  /** it should return the keys of an object */ {
     type Actual = Keys<{ foo: string; bar: number }>;
     expectType<'foo' | 'bar'>(type<Actual>());
-  });
+  }
 
-  it('should return the keys of a tuple', () => {
+  /** it should return the keys of a tuple */ {
     type Actual = Keys<[number, string]>;
     expectType<'0' | '1'>(type<Actual>());
-  });
+  }
 
-  it('should return the keys of an array', () => {
+  /** it should return the keys of an array */ {
     type Actual = Keys<string[]>;
     expectType<`${number}`>(type<Actual>());
-  });
+  }
 
-  it('should only return the keys of string properties', () => {
+  /** it should only return the keys of string properties */ {
     type Actual = Keys<{ foo: string; bar: number }, string>;
     expectType<'foo'>(type<Actual>());
-  });
-});
+  }
+}
 
-describe('ValidKeyListPrefix', () => {
-  it('should return the entire path if it is valid', () => {
+/** {@link ValidKeyListPrefix} */ {
+  /** it should return the entire path if it is valid */ {
     type Actual = ValidKeyListPrefix<
       InfiniteType<string>,
       ['foo', 'bar', '0', 'baz', '42']
     >;
     expectType<['foo', 'bar', '0', 'baz', '42']>(type<Actual>());
-  });
+  }
 
-  it('should return the longest valid prefix', () => {
+  /** it should return the longest valid prefix */ {
     type Actual = ValidKeyListPrefix<
       InfiniteType<string>,
       ['foo', 'bar', '0', 'ba', '42']
     >;
     expectType<['foo', 'bar', '0']>(type<Actual>());
-  });
+  }
 
-  it('should return an empty tuple when the path is an empty tuple', () => {
+  /** it should return an empty tuple when the path is an empty tuple */ {
     type Actual = ValidKeyListPrefix<InfiniteType<string>, []>;
     expectType<[]>(type<Actual>());
-  });
+  }
 
-  it('should be implemented tail recursively', () => {
+  /** it should be implemented tail recursively */ {
     type Actual = ValidKeyListPrefix<InfiniteType<string>, HundredTuple<'foo'>>;
     expectType<HundredTuple<'foo'>>(type<Actual>());
-  });
-});
+  }
+}
 
-describe('DropLastElement', () => {
-  it('should remove the last element from a tuple', () => {
+/** {@link DropLastElement} */ {
+  /** it should remove the last element from a tuple */ {
     type Actual = DropLastElement<[0, 1, 2]>;
     expectType<[0, 1]>(type<Actual>());
-  });
+  }
 
-  it('should return the empty tuple when passed the empty tuple', () => {
+  /** it should return the empty tuple when passed the empty tuple */ {
     type Actual = DropLastElement<[]>;
     expectType<[]>(type<Actual>());
-  });
-});
+  }
+}
 
-describe('SuggestParentPath', () => {
-  it('should evaluate to the parent path', () => {
+/** {@link SuggestParentPath} */ {
+  /** it should evaluate to the parent path */ {
     type Actual = SuggestParentPath<['foo', 'bar', 'baz']>;
     expectType<'foo.bar'>(type<Actual>());
-  });
+  }
 
-  it('should evaluate to the never if there is no parent path', () => {
+  /** it should evaluate to the never if there is no parent path */ {
     type Actual = SuggestParentPath<['foo']>;
     expectType<never>(type<Actual>());
-  });
-});
+  }
+}
 
-describe('SuggestChildPaths', () => {
-  it('should suggest paths when the current path is empty', () => {
+/** {@link SuggestChildPaths} */ {
+  /** it should suggest paths when the current path is empty */ {
     type Actual = SuggestChildPaths<InfiniteType<string>, [], unknown>;
     expectType<'foo' | 'bar' | 'baz' | 'value'>(type<Actual>());
-  });
+  }
 
-  it('should suggest paths when the current path is not empty', () => {
+  /** it should suggest paths when the current path is not empty */ {
     type Actual = SuggestChildPaths<
       InfiniteType<string>,
       ['foo', 'foo'],
@@ -99,63 +99,63 @@ describe('SuggestChildPaths', () => {
     expectType<'foo.foo.foo' | 'foo.foo.bar' | 'foo.foo.baz' | 'foo.foo.value'>(
       type<Actual>(),
     );
-  });
+  }
 
-  it('should suggest only matching or traversable paths', () => {
+  /** it should suggest only matching or traversable paths */ {
     type Actual = SuggestChildPaths<InfiniteType<string>, ['foo'], number>;
     expectType<'foo.foo' | 'foo.bar' | 'foo.baz'>(type<Actual>());
-  });
+  }
 
-  it('should suggest paths into a tuple', () => {
+  /** it should suggest paths into a tuple */ {
     type Actual = SuggestChildPaths<InfiniteType<string>, ['bar'], unknown>;
     expectType<`bar.0`>(type<Actual>());
-  });
+  }
 
-  it('should suggest paths into an array', () => {
+  /** it should suggest paths into an array */ {
     type Actual = SuggestChildPaths<InfiniteType<string>, ['baz'], unknown>;
     expectType<`baz.${number}`>(type<Actual>());
-  });
+  }
 
-  it('should evaluate to never if the path does not exist', () => {
+  /** it should evaluate to never if the path does not exist */ {
     type Actual = SuggestChildPaths<InfiniteType<string>, ['foobar'], unknown>;
     expectType<never>(type<Actual>());
-  });
-});
+  }
+}
 
-describe('SuggestPath', () => {
-  it('should suggest all top level paths when the path string is empty', () => {
+/** {@link SuggestPath} */ {
+  /** it should suggest all top level paths when the path string is empty */ {
     type Actual = SuggestPath<InfiniteType<string>, '', unknown>;
     expectType<'foo' | 'bar' | 'baz' | 'value'>(type<Actual>());
-  });
+  }
 
-  it('should not suggest the current path if it is invalid', () => {
+  /** it should not suggest the current path if it is invalid */ {
     type Actual = SuggestPath<InfiniteType<string>, 'foo.foobar', unknown>;
     expectType<'foo' | 'foo.foo' | 'foo.bar' | 'foo.baz' | 'foo.value'>(
       type<Actual>(),
     );
-  });
+  }
 
-  it('should suggest the current path if it is valid', () => {
+  /** it should suggest the current path if it is valid */ {
     type Actual = SuggestPath<InfiniteType<string>, 'foo', unknown>;
     expectType<'foo' | 'foo.foo' | 'foo.bar' | 'foo.baz' | 'foo.value'>(
       type<Actual>(),
     );
-  });
+  }
 
-  it('should suggest the current path and the parent path', () => {
+  /** it should suggest the current path and the parent path */ {
     type Actual = SuggestPath<InfiniteType<string>, 'foo.value', unknown>;
     expectType<'foo' | 'foo.value'>(type<Actual>());
-  });
+  }
 
-  it('should not suggest the current path if it has the wrong value', () => {
+  /** it should not suggest the current path if it has the wrong value */ {
     type Actual = SuggestPath<InfiniteType<string>, 'foo.value', number>;
     expectType<'foo'>(type<Actual>());
-  });
+  }
 
-  it('should not suggest paths which point to the wrong type', () => {
+  /** it should not suggest paths which point to the wrong type */ {
     type Actual = SuggestPath<InfiniteType<string>, 'foo.foo', number>;
     expectType<'foo' | 'foo.foo.foo' | 'foo.foo.bar' | 'foo.foo.baz'>(
       type<Actual>(),
     );
-  });
-});
+  }
+}
