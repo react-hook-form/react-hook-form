fd31895853f7213264a1cff51ad451a10f30e169
Felix Schorer <felixschorer@users.noreply.github.com>
2021-12-20 12:14:45 +0100

    Add type tests for eager types

--- src/__tests__/types/__fixtures__/traversable.ts
+++ src/__tests__/types/__fixtures__/traversable.ts
@@ -1,9 +1,13 @@
-export interface InfiniteType<T> {
-  foo: InfiniteType<T>;
-  bar: [InfiniteType<T>];
-  baz: Array<InfiniteType<T>>;
-  value: T;
+interface Base<T, V> {
+  foo: T;
+  bar: [T];
+  baz: Array<T>;
+  value: V;
 }
+
+export type InfiniteType<T> = Base<InfiniteType<T>, T>;
+
+export type Depth3Type<T> = Base<Base<Base<never, T>, T>, T>;
 
 export interface Nested<T> {
   nested: T;
