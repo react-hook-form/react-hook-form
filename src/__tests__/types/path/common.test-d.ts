0a4bf5e5fa4a6930e776830a19953db1417d0ff8
Felix Schorer <felixschorer@users.noreply.github.com>
2021-12-20 11:51:11 +0100

    Remove Actual type alias for better error reporting from tsd

--- src/__tests__/types/path/common.test-d.ts
+++ src/__tests__/types/path/common.test-d.ts
@@ -21,164 +21,170 @@ import {
 
 /** {@link IsTuple} */ {
   /** it should evaluate to true if it's a tuple */ {
-    type Actual = IsTuple<[string, number]>;
-    expectType<true>(_ as Actual);
+    const actual = _ as IsTuple<[string, number]>;
+    expectType<true>(actual);
   }
 
   /** it should evaluate to false if it's not a tuple */ {
-    type Actual = IsTuple<string[]>;
-    expectType<false>(_ as Actual);
+    const actual = _ as IsTuple<string[]>;
+    expectType<false>(actual);
   }
 }
 
 /** {@link TupleKey} */ {
   /** it should evaluate to the own keys of the tuple */ {
-    type Actual = TupleKey<[string, number]>;
-    expectType<'0' | '1'>(_ as Actual);
+    const actual = _ as TupleKey<[string, number]>;
+    expectType<'0' | '1'>(actual);
   }
 
   /** it should evaluate to never if an array type is passed */ {
-    type Actual = TupleKey<string[]>;
-    expectType<never>(_ as Actual);
+    const actual = _ as TupleKey<string[]>;
+    expectType<never>(actual);
   }
 }
 
 /** {@link AsKey} */ {
   /** it should behave like a noop type when a Key is passed */ {
-    type Actual = AsKey<'foo'>;
-    expectType<'foo'>(_ as Actual);
+    const actual = _ as AsKey<'foo'>;
+    expectType<'foo'>(actual);
   }
 
   /** it should evaluate to never if not a Key is passed */ {
-    type Actual = AsKey<boolean>;
-    expectType<never>(_ as Actual);
+    const actual = _ as AsKey<boolean>;
+    expectType<never>(actual);
   }
 }
 
 /** {@link ToKey} */ {
   /** it should behave like a noop type when a Key is passed */ {
-    type Actual = ToKey<'foo'>;
-    expectType<'foo'>(_ as Actual);
+    const actual = _ as ToKey<'foo'>;
+    expectType<'foo'>(actual);
   }
 
   /** it should evaluate to never if not a Key or ArrayKey is passed */ {
-    type Actual = ToKey<boolean>;
-    expectType<never>(_ as Actual);
+    const actual = _ as ToKey<boolean>;
+    expectType<never>(actual);
   }
 
   /** it should convert an ArrayKey to a template literal type */ {
-    type Actual = ToKey<ArrayKey>;
-    expectType<`${ArrayKey}`>(_ as Actual);
+    const actual = _ as ToKey<ArrayKey>;
+    expectType<`${ArrayKey}`>(actual);
   }
 }
 
 /** {@link AsKeyList} */ {
   /** it should behave like a noop type when a KeyList is passed */ {
-    type Actual = AsKeyList<['foo']>;
-    expectType<['foo']>(_ as Actual);
+    const actual = _ as AsKeyList<['foo']>;
+    expectType<['foo']>(actual);
   }
 
   /** it should evaluate to never if not a KeyList is passed */ {
-    type Actual = AsKeyList<[42]>;
-    expectType<never>(_ as Actual);
+    const actual = _ as AsKeyList<[42]>;
+    expectType<never>(actual);
   }
 }
 
 /** {@link SplitPathString} */ {
   /** it should split the PathString */ {
-    type Actual = SplitPathString<'foo.bar.0.baz'>;
-    expectType<['foo', 'bar', '0', 'baz']>(_ as Actual);
+    const actual = _ as SplitPathString<'foo.bar.0.baz'>;
+    expectType<['foo', 'bar', '0', 'baz']>(actual);
   }
 
   /** it should split a PathString which does not contain a "." */ {
-    type Actual = SplitPathString<'foo'>;
-    expectType<['foo']>(_ as Actual);
+    const actual = _ as SplitPathString<'foo'>;
+    expectType<['foo']>(actual);
   }
 
   /** it should split a PathString containing only a "." */ {
-    type Actual = SplitPathString<'.'>;
-    expectType<['', '']>(_ as Actual);
+    const actual = _ as SplitPathString<'.'>;
+    expectType<['', '']>(actual);
   }
 
   /** it should be implemented tail recursively */ {
-    type Actual = SplitPathString<HundredPathString<'foo'>>;
-    expectType<HundredTuple<'foo'>>(_ as Actual);
+    const actual = _ as SplitPathString<HundredPathString<'foo'>>;
+    expectType<HundredTuple<'foo'>>(actual);
   }
 }
 
 /** {@link JoinKeyList} */ {
   /** it should join the KeyList */ {
-    type Actual = JoinKeyList<['foo', 'bar', '0', 'baz']>;
-    expectType<'foo.bar.0.baz'>(_ as Actual);
+    const actual = _ as JoinKeyList<['foo', 'bar', '0', 'baz']>;
+    expectType<'foo.bar.0.baz'>(actual);
   }
 
   /** it should join a KeyList of length 1 */ {
-    type Actual = JoinKeyList<['foo']>;
-    expectType<'foo'>(_ as Actual);
+    const actual = _ as JoinKeyList<['foo']>;
+    expectType<'foo'>(actual);
   }
 
   /** it should evaluate to never when passed an empty KeyList */ {
-    type Actual = JoinKeyList<[]>;
-    expectType<never>(_ as Actual);
+    const actual = _ as JoinKeyList<[]>;
+    expectType<never>(actual);
   }
 
   /** it should be implemented tail recursively */ {
-    type Actual = JoinKeyList<HundredTuple<'foo'>>;
-    expectType<HundredPathString<'foo'>>(_ as Actual);
+    const actual = _ as JoinKeyList<HundredTuple<'foo'>>;
+    expectType<HundredPathString<'foo'>>(actual);
   }
 }
 
 /** {@link EvaluateKey} */ {
   /** it should traverse an object */ {
-    type Actual = EvaluateKey<{ foo: number; bar: string }, 'foo'>;
-    expectType<number>(_ as Actual);
+    const actual = _ as EvaluateKey<{ foo: number; bar: string }, 'foo'>;
+    expectType<number>(actual);
   }
 
   /** it should traverse a tuple */ {
-    type Actual = EvaluateKey<[boolean, string], '1'>;
-    expectType<string>(_ as Actual);
+    const actual = _ as EvaluateKey<[boolean, string], '1'>;
+    expectType<string>(actual);
   }
 
   /** it should traverse an array */ {
-    type Actual = EvaluateKey<boolean[], '42'>;
-    expectType<boolean>(_ as Actual);
+    const actual = _ as EvaluateKey<boolean[], '42'>;
+    expectType<boolean>(actual);
   }
 
   /** it should evaluate to never if the key is not valid */ {
-    type Actual = EvaluateKey<{ foo: string }, 'foobar'>;
-    expectType<never>(_ as Actual);
+    const actual = _ as EvaluateKey<{ foo: string }, 'foobar'>;
+    expectType<never>(actual);
   }
 }
 
 /** {@link EvaluateKeyList} */ {
   /** it should traverse an object */ {
-    type Actual = EvaluateKeyList<
+    const actual = _ as EvaluateKeyList<
       InfiniteType<number>,
       ['foo', 'foo', 'value']
     >;
-    expectType<number>(_ as Actual);
+    expectType<number>(actual);
   }
 
   /** it should traverse a tuple */ {
-    type Actual = EvaluateKeyList<InfiniteType<boolean>, ['bar', '0', 'value']>;
-    expectType<boolean>(_ as Actual);
+    const actual = _ as EvaluateKeyList<
+      InfiniteType<boolean>,
+      ['bar', '0', 'value']
+    >;
+    expectType<boolean>(actual);
   }
 
   /** it should traverse an array */ {
-    type Actual = EvaluateKeyList<
+    const actual = _ as EvaluateKeyList<
       InfiniteType<boolean>,
       ['baz', '42', 'value']
     >;
-    expectType<boolean>(_ as Actual);
+    expectType<boolean>(actual);
   }
 
   /** it should evaluate to never if the path is not valid */ {
-    type Actual = EvaluateKeyList<InfiniteType<string>, ['foobar']>;
-    expectType<never>(_ as Actual);
+    const actual = _ as EvaluateKeyList<InfiniteType<string>, ['foobar']>;
+    expectType<never>(actual);
   }
 
   /** it should be implemented tail recursively */ {
-    type Actual = EvaluateKeyList<InfiniteType<string>, HundredTuple<'foo'>>;
-    expectType<InfiniteType<string>>(_ as Actual);
+    const actual = _ as EvaluateKeyList<
+      InfiniteType<string>,
+      HundredTuple<'foo'>
+    >;
+    expectType<InfiniteType<string>>(actual);
   }
 }
--- src/__tests__/types/path/lazy.test-d.ts
+++ src/__tests__/types/path/lazy.test-d.ts
@@ -14,188 +14,227 @@ import { _, HundredTuple, InfiniteType, Nested } from '../__fixtures__';
 
 /** {@link Keys} */ {
   /** it should return the keys of an object */ {
-    type Actual = Keys<{ foo: string; bar: number }>;
-    expectType<'foo' | 'bar'>(_ as Actual);
+    const actual = _ as Keys<{ foo: string; bar: number }>;
+    expectType<'foo' | 'bar'>(actual);
   }
 
   /** it should return the keys of a tuple */ {
-    type Actual = Keys<[number, string]>;
-    expectType<'0' | '1'>(_ as Actual);
+    const actual = _ as Keys<[number, string]>;
+    expectType<'0' | '1'>(actual);
   }
 
   /** it should return the keys of an array */ {
-    type Actual = Keys<string[]>;
-    expectType<`${number}`>(_ as Actual);
+    const actual = _ as Keys<string[]>;
+    expectType<`${number}`>(actual);
   }
 
   /** it should only return the keys of string properties */ {
-    type Actual = Keys<{ foo: string; bar: number }, string>;
-    expectType<'foo'>(_ as Actual);
+    const actual = _ as Keys<{ foo: string; bar: number }, string>;
+    expectType<'foo'>(actual);
   }
 }
 
 /** {@link ValidKeyListPrefix} */ {
   /** it should return the entire path if it is valid */ {
-    type Actual = ValidKeyListPrefix<
+    const actual = _ as ValidKeyListPrefix<
       InfiniteType<string>,
       ['foo', 'bar', '0', 'baz', '42']
     >;
-    expectType<['foo', 'bar', '0', 'baz', '42']>(_ as Actual);
+    expectType<['foo', 'bar', '0', 'baz', '42']>(actual);
   }
 
   /** it should return the longest valid prefix */ {
-    type Actual = ValidKeyListPrefix<
+    const actual = _ as ValidKeyListPrefix<
       InfiniteType<string>,
       ['foo', 'bar', '0', 'ba', '42']
     >;
-    expectType<['foo', 'bar', '0']>(_ as Actual);
+    expectType<['foo', 'bar', '0']>(actual);
   }
 
   /** it should return an empty tuple when the path is an empty tuple */ {
-    type Actual = ValidKeyListPrefix<InfiniteType<string>, []>;
-    expectType<[]>(_ as Actual);
+    const actual = _ as ValidKeyListPrefix<InfiniteType<string>, []>;
+    expectType<[]>(actual);
   }
 
   /** it should be implemented tail recursively */ {
-    type Actual = ValidKeyListPrefix<InfiniteType<string>, HundredTuple<'foo'>>;
-    expectType<HundredTuple<'foo'>>(_ as Actual);
+    const actual = _ as ValidKeyListPrefix<
+      InfiniteType<string>,
+      HundredTuple<'foo'>
+    >;
+    expectType<HundredTuple<'foo'>>(actual);
   }
 }
 
 /** {@link DropLastElement} */ {
   /** it should remove the last element from a tuple */ {
-    type Actual = DropLastElement<[0, 1, 2]>;
-    expectType<[0, 1]>(_ as Actual);
+    const actual = _ as DropLastElement<[0, 1, 2]>;
+    expectType<[0, 1]>(actual);
   }
 
   /** it should return the empty tuple when passed the empty tuple */ {
-    type Actual = DropLastElement<[]>;
-    expectType<[]>(_ as Actual);
+    const actual = _ as DropLastElement<[]>;
+    expectType<[]>(actual);
   }
 }
 
 /** {@link SuggestParentPath} */ {
   /** it should evaluate to the parent path */ {
-    type Actual = SuggestParentPath<['foo', 'bar', 'baz']>;
-    expectType<'foo.bar'>(_ as Actual);
+    const actual = _ as SuggestParentPath<['foo', 'bar', 'baz']>;
+    expectType<'foo.bar'>(actual);
   }
 
   /** it should evaluate to the never if there is no parent path */ {
-    type Actual = SuggestParentPath<['foo']>;
-    expectType<never>(_ as Actual);
+    const actual = _ as SuggestParentPath<['foo']>;
+    expectType<never>(actual);
   }
 }
 
 /** {@link SuggestChildPaths} */ {
   /** it should suggest paths when the current path is empty */ {
-    type Actual = SuggestChildPaths<InfiniteType<string>, [], unknown>;
-    expectType<'foo' | 'bar' | 'baz' | 'value'>(_ as Actual);
+    const actual = _ as SuggestChildPaths<InfiniteType<string>, [], unknown>;
+    expectType<'foo' | 'bar' | 'baz' | 'value'>(actual);
   }
 
   /** it should suggest paths when the current path is not empty */ {
-    type Actual = SuggestChildPaths<
+    const actual = _ as SuggestChildPaths<
       InfiniteType<string>,
       ['foo', 'foo'],
       unknown
     >;
     expectType<'foo.foo.foo' | 'foo.foo.bar' | 'foo.foo.baz' | 'foo.foo.value'>(
-      _ as Actual,
+      actual,
     );
   }
 
   /** it should suggest only matching or traversable paths */ {
-    type Actual = SuggestChildPaths<InfiniteType<string>, ['foo'], number>;
-    expectType<'foo.foo' | 'foo.bar' | 'foo.baz'>(_ as Actual);
+    const actual = _ as SuggestChildPaths<
+      InfiniteType<string>,
+      ['foo'],
+      number
+    >;
+    expectType<'foo.foo' | 'foo.bar' | 'foo.baz'>(actual);
   }
 
   /** it should suggest paths into a tuple */ {
-    type Actual = SuggestChildPaths<InfiniteType<string>, ['bar'], unknown>;
-    expectType<`bar.0`>(_ as Actual);
+    const actual = _ as SuggestChildPaths<
+      InfiniteType<string>,
+      ['bar'],
+      unknown
+    >;
+    expectType<`bar.0`>(actual);
   }
 
   /** it should suggest paths into an array */ {
-    type Actual = SuggestChildPaths<InfiniteType<string>, ['baz'], unknown>;
-    expectType<`baz.${number}`>(_ as Actual);
+    const actual = _ as SuggestChildPaths<
+      InfiniteType<string>,
+      ['baz'],
+      unknown
+    >;
+    expectType<`baz.${number}`>(actual);
   }
 
   /** it should evaluate to never if the path does not exist */ {
-    type Actual = SuggestChildPaths<InfiniteType<string>, ['foobar'], unknown>;
-    expectType<never>(_ as Actual);
+    const actual = _ as SuggestChildPaths<
+      InfiniteType<string>,
+      ['foobar'],
+      unknown
+    >;
+    expectType<never>(actual);
   }
 }
 
 /** {@link SuggestPaths} */
 {
   /** it should suggest all top level paths when the path is empty */ {
-    type Actual = SuggestPaths<InfiniteType<string>, [], unknown>;
-    expectType<'foo' | 'bar' | 'baz' | 'value'>(_ as Actual);
+    const actual = _ as SuggestPaths<InfiniteType<string>, [], unknown>;
+    expectType<'foo' | 'bar' | 'baz' | 'value'>(actual);
   }
 
   /** it should suggest the sibling paths if the path is valid */ {
-    type Actual = SuggestPaths<InfiniteType<string>, ['fo'], unknown>;
-    expectType<'foo' | 'bar' | 'baz' | 'value'>(_ as Actual);
+    const actual = _ as SuggestPaths<InfiniteType<string>, ['fo'], unknown>;
+    expectType<'foo' | 'bar' | 'baz' | 'value'>(actual);
   }
 
   /** it should suggest the child paths if the path is valid */ {
-    type Actual = SuggestPaths<InfiniteType<string>, ['foo'], unknown>;
-    expectType<'foo.foo' | 'foo.bar' | 'foo.baz' | 'foo.value'>(_ as Actual);
+    const actual = _ as SuggestPaths<InfiniteType<string>, ['foo'], unknown>;
+    expectType<'foo.foo' | 'foo.bar' | 'foo.baz' | 'foo.value'>(actual);
   }
 
   /** it should suggest the parent path */ {
-    type Actual = SuggestPaths<InfiniteType<string>, ['foo', 'value'], unknown>;
-    expectType<'foo'>(_ as Actual);
+    const actual = _ as SuggestPaths<
+      InfiniteType<string>,
+      ['foo', 'value'],
+      unknown
+    >;
+    expectType<'foo'>(actual);
   }
 
   /** it should not suggest paths which point to the wrong type */ {
-    type Actual = SuggestPaths<InfiniteType<string>, ['foo', 'foo'], number>;
-    expectType<'foo' | 'foo.foo.foo' | 'foo.foo.bar' | 'foo.foo.baz'>(
-      _ as Actual,
-    );
+    const actual = _ as SuggestPaths<
+      InfiniteType<string>,
+      ['foo', 'foo'],
+      number
+    >;
+    expectType<'foo' | 'foo.foo.foo' | 'foo.foo.bar' | 'foo.foo.baz'>(actual);
   }
 }
 
 /** {@link AutoCompletePath} */ {
   /** it should suggest all top level paths when the path is empty */ {
-    type Actual = AutoCompletePath<InfiniteType<string>, '', unknown>;
-    expectType<'foo' | 'bar' | 'baz' | 'value'>(_ as Actual);
+    const actual = _ as AutoCompletePath<InfiniteType<string>, '', unknown>;
+    expectType<'foo' | 'bar' | 'baz' | 'value'>(actual);
   }
 
   /** it should not suggest the current path if it is invalid */ {
-    type Actual = AutoCompletePath<InfiniteType<string>, 'foo.foobar', unknown>;
-    expectType<'foo' | 'foo.foo' | 'foo.bar' | 'foo.baz' | 'foo.value'>(
-      _ as Actual,
-    );
+    const actual = _ as AutoCompletePath<
+      InfiniteType<string>,
+      'foo.foobar',
+      unknown
+    >;
+    expectType<'foo' | 'foo.foo' | 'foo.bar' | 'foo.baz' | 'foo.value'>(actual);
   }
 
   /** it should suggest the current path if it is valid */ {
-    type Actual = AutoCompletePath<InfiniteType<string>, 'foo', unknown>;
-    expectType<'foo' | 'foo.foo' | 'foo.bar' | 'foo.baz' | 'foo.value'>(
-      _ as Actual,
-    );
+    const actual = _ as AutoCompletePath<InfiniteType<string>, 'foo', unknown>;
+    expectType<'foo' | 'foo.foo' | 'foo.bar' | 'foo.baz' | 'foo.value'>(actual);
   }
 
   /** it should suggest the current path and the parent path */ {
-    type Actual = AutoCompletePath<InfiniteType<string>, 'foo.value', unknown>;
-    expectType<'foo' | 'foo.value'>(_ as Actual);
+    const actual = _ as AutoCompletePath<
+      InfiniteType<string>,
+      'foo.value',
+      unknown
+    >;
+    expectType<'foo' | 'foo.value'>(actual);
   }
 
   /** it should not suggest the current path if it has the wrong value */ {
-    type Actual = AutoCompletePath<InfiniteType<string>, 'foo.value', number>;
-    expectType<'foo'>(_ as Actual);
+    const actual = _ as AutoCompletePath<
+      InfiniteType<string>,
+      'foo.value',
+      number
+    >;
+    expectType<'foo'>(actual);
   }
 
   /** it should suggest paths which point to the correct type */ {
-    type Actual = AutoCompletePath<InfiniteType<string>, 'foo.foo', string>;
+    const actual = _ as AutoCompletePath<
+      InfiniteType<string>,
+      'foo.foo',
+      string
+    >;
     expectType<
       'foo' | 'foo.foo.foo' | 'foo.foo.bar' | 'foo.foo.baz' | 'foo.foo.value'
-    >(_ as Actual);
+    >(actual);
   }
 
   /** it should not suggest paths which point to the wrong type */ {
-    type Actual = AutoCompletePath<InfiniteType<string>, 'foo.foo', number>;
-    expectType<'foo' | 'foo.foo.foo' | 'foo.foo.bar' | 'foo.foo.baz'>(
-      _ as Actual,
-    );
+    const actual = _ as AutoCompletePath<
+      InfiniteType<string>,
+      'foo.foo',
+      number
+    >;
+    expectType<'foo' | 'foo.foo.foo' | 'foo.foo.bar' | 'foo.foo.baz'>(actual);
   }
 
   /** TS should be able to infer the generic */ {
