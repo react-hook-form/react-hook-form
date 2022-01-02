03b52945961c33f1d631dea2c2a782db7dcb2fba
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-01-02 20:00:40 +0100

    fix isPrimitive utility

--- src/utils/isPrimitive.ts
+++ src/utils/isPrimitive.ts
@@ -1,7 +1,3 @@
 import { Primitive } from '../types';
 
-import isNullOrUndefined from './isNullOrUndefined';
-import { isObjectType } from './isObject';
-
-export default (value: unknown): value is Primitive =>
-  isNullOrUndefined(value) || !isObjectType(value);
+export default (value: unknown): value is Primitive => Object(value) !== value;
