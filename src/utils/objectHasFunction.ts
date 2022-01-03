d16700c9d921468c5e3f7dc3696d8a399800bd2d
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-01-03 21:27:29 +0100

    rewrite objectHasFunction utility

--- src/utils/objectHasFunction.ts
+++ src/utils/objectHasFunction.ts
@@ -1,10 +1,3 @@
 import isFunction from './isFunction';
 
-export default <T>(data: T): boolean => {
-  for (const key in data) {
-    if (isFunction(data[key])) {
-      return true;
-    }
-  }
-  return false;
-};
+export default <T>(data: T) => Object.values(data).some(isFunction);
