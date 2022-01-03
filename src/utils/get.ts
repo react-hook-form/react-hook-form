144666f42cafd8e5e192b4a4926df3cb7a6a59b4
Adrian Tarasiuk <adrian.tarasiuk23@gmail.com>
2022-01-03 12:09:49 +0100

    add early return to get utility

--- src/utils/get.ts
+++ src/utils/get.ts
@@ -1,22 +1,21 @@
 import compact from './compact';
 import isNullOrUndefined from './isNullOrUndefined';
-import isObject from './isObject';
 import isUndefined from './isUndefined';
 
 export default <T>(obj: T, path: string, defaultValue?: unknown): any => {
-  if (isObject(obj) && path) {
-    const result = compact(path.split(/[,[\].]+?/)).reduce(
-      (result, key) =>
-        isNullOrUndefined(result) ? result : result[key as keyof {}],
-      obj,
-    );
-
-    return isUndefined(result) || result === obj
-      ? isUndefined(obj[path as keyof T])
-        ? defaultValue
-        : obj[path as keyof T]
-      : result;
+  if (!obj || !path) {
+    return defaultValue;
   }
 
-  return undefined;
+  const result = compact(path.split(/[,[\].]+?/)).reduce(
+    (result, key) =>
+      isNullOrUndefined(result) ? result : result[key as keyof {}],
+    obj,
+  );
+
+  return isUndefined(result) || result === obj
+    ? isUndefined(obj[path as keyof T])
+      ? defaultValue
+      : obj[path as keyof T]
+    : result;
 };
