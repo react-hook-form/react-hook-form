9af4deb8ba8ad93b6f612810dabef4c59c4375be
Felix Schorer <felixschorer@users.noreply.github.com>
2022-01-13 17:13:53 +0100

    Add test for Either types

---
+++ src/__tests__/types/path/either.test-d.ts
@@ -0,0 +1,98 @@
+import { expectType } from 'tsd';
+
+import { Branded, PathString } from '../../../types';
+import {
+  FieldArrayPath,
+  FieldPath,
+  TypedFieldArrayPath,
+  TypedFieldPath,
+} from '../../../types/path/either';
+import { _, InfiniteType } from '../__fixtures__';
+
+/** {@link TypedFieldPath} */ {
+  /** it should infer branded types */ {
+    const fn = <P extends PathString>(
+      path: TypedFieldPath<InfiniteType<number>, P, number>,
+    ): P => path as never;
+
+    const actual = fn(
+      _ as Branded.TypedFieldPath<InfiniteType<number>, number>,
+    );
+    expectType<Branded.TypedFieldPath<InfiniteType<number>, number>>(actual);
+  }
+
+  /** it should infer string types */ {
+    const fn = <P extends PathString>(
+      path: TypedFieldPath<InfiniteType<number>, P, number>,
+    ): P => path as never;
+
+    const actual = fn('foo.value');
+    expectType<'foo.value'>(actual);
+  }
+}
+
+/** {@link FieldPath} */ {
+  /** it should infer branded types */ {
+    const fn = <P extends PathString>(
+      path: FieldPath<InfiniteType<number>, P>,
+    ): P => path as never;
+
+    const actual = fn(_ as Branded.FieldPath<InfiniteType<number>>);
+    expectType<Branded.FieldPath<InfiniteType<number>>>(actual);
+  }
+
+  /** it should infer string types */ {
+    const fn = <P extends PathString>(
+      path: FieldPath<InfiniteType<number>, P>,
+    ): P => path as never;
+
+    const actual = fn('foo.value');
+    expectType<'foo.value'>(actual);
+  }
+}
+
+/** {@link TypedFieldArrayPath} */ {
+  /** it should infer branded types */ {
+    type Foo = { foo: string };
+    const fn = <P extends PathString>(
+      path: TypedFieldArrayPath<InfiniteType<Foo[]>, P, Foo>,
+    ): P => path as never;
+
+    const actual = fn(
+      _ as Branded.TypedFieldArrayPath<InfiniteType<Foo[]>, Foo>,
+    );
+    expectType<Branded.TypedFieldArrayPath<InfiniteType<Foo[]>, Foo>>(actual);
+  }
+
+  /** it should infer string types */ {
+    type Foo = { foo: string };
+    const fn = <P extends PathString>(
+      path: TypedFieldArrayPath<InfiniteType<Foo[]>, P, Foo>,
+    ): P => path as never;
+
+    const actual = fn('foo.value');
+    expectType<'foo.value'>(actual);
+  }
+}
+
+/** {@link FieldArrayPath} */ {
+  /** it should infer branded types */ {
+    type Foo = { foo: string };
+    const fn = <P extends PathString>(
+      path: FieldArrayPath<InfiniteType<Foo[]>, P>,
+    ): P => path as never;
+
+    const actual = fn(_ as Branded.FieldArrayPath<InfiniteType<Foo[]>>);
+    expectType<Branded.FieldArrayPath<InfiniteType<Foo[]>>>(actual);
+  }
+
+  /** it should infer string types */ {
+    type Foo = { foo: string };
+    const fn = <P extends PathString>(
+      path: FieldArrayPath<InfiniteType<Foo[]>, P>,
+    ): P => path as never;
+
+    const actual = fn('foo.value');
+    expectType<'foo.value'>(actual);
+  }
+}
