0eb8632c4c7d82bd956e22bab589ee058dad0451
Felix Schorer <felixschorer@users.noreply.github.com>
2022-01-13 18:17:05 +0100

    Add tests for value types

---
+++ src/__tests__/types/path/value.test-d.ts
@@ -0,0 +1,62 @@
+import { expectType } from 'tsd';
+
+import { Branded } from '../../../types';
+import {
+  FieldPathSetValue,
+  FieldPathValue,
+  FieldPathValues,
+} from '../../../types/path/value';
+import { _ } from '../__fixtures__';
+
+/** {@link FieldPathValue} */ {
+  /** it should get the type of a branded path */ {
+    const actual = _ as FieldPathValue<{}, Branded.TypedFieldPath<{}, number>>;
+    expectType<number>(actual);
+  }
+
+  /** it should evaluate to unknown if the branded path is not applicable */ {
+    const actual = _ as FieldPathValue<
+      { foo: string },
+      Branded.TypedFieldPath<{}, number>
+    >;
+    expectType<unknown>(actual);
+  }
+
+  /** it should get the type of a string path */ {
+    const actual = _ as FieldPathValue<{ foo: number }, 'foo'>;
+    expectType<number>(actual);
+  }
+}
+
+/** {@link FieldPathSetValue} */ {
+  /** it should get the set type of a branded path */ {
+    const actual = _ as FieldPathSetValue<
+      {},
+      Branded.TypedFieldPath<{}, number>
+    >;
+    expectType<number>(actual);
+  }
+
+  /** it should evaluate to never if the branded path is not applicable */ {
+    const actual = _ as FieldPathSetValue<
+      { foo: string },
+      Branded.TypedFieldPath<{}, number>
+    >;
+    expectType<never>(actual);
+  }
+
+  /** it should get the set type of a string path */ {
+    const actual = _ as FieldPathSetValue<{ foo: number }, 'foo'>;
+    expectType<number>(actual);
+  }
+}
+
+/** {@link FieldPathValues} */ {
+  /** it should resolve all paths */ {
+    const actual = _ as FieldPathValues<
+      { foo: string; bar: number },
+      ['foo', 'bar']
+    >;
+    expectType<[string, number]>(actual);
+  }
+}
