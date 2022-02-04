9d684ea52a41bc9b65304bcafbd6eef702cec14c
Beier Luo <bluebill1049@hotmail.com>
2022-02-04 12:08:24 +1100

    fix #7741 isValidating reset when async validations are still running

--- src/__tests__/useForm/formState.test.tsx
+++ src/__tests__/useForm/formState.test.tsx
@@ -386,6 +386,66 @@ describe('formState', () => {
     screen.getByText('isNotSubmitSuccessful');
   });
 
+  it('should update isValidating to true when other validation still running', async () => {
+    function App() {
+      const [stateValidation, setStateValidation] = React.useState(false);
+      const {
+        register,
+        formState: { isValidating },
+      } = useForm({ mode: 'all' });
+
+      return (
+        <div>
+          <p>isValidating: {String(isValidating)}</p>
+          <p>stateValidation: {String(stateValidation)}</p>
+          <form>
+            <input
+              {...register('lastName', {
+                required: true,
+                validate: () => {
+                  setStateValidation(true);
+                  return new Promise((resolve) => {
+                    setTimeout(() => {
+                      setStateValidation(false);
+                      resolve(true);
+                    }, 5000);
+                  });
+                },
+              })}
+              placeholder="async"
+            />
+
+            <input
+              {...register('firstName', { required: true })}
+              placeholder="required"
+            />
+          </form>
+        </div>
+      );
+    }
+
+    render(<App />);
+
+    fireEvent.change(screen.getByPlaceholderText('async'), {
+      target: { value: 'test' },
+    });
+    fireEvent.change(screen.getByPlaceholderText('required'), {
+      target: { value: 'test' },
+    });
+
+    screen.getByText('isValidating: true');
+    screen.getByText('stateValidation: true');
+
+    await actComponent(async () => {
+      jest.runAllTimers();
+    });
+
+    await actComponent(async () => {
+      screen.getByText('isValidating: false');
+      screen.getByText('stateValidation: false');
+    });
+  });
+
   it('should update correct isValid formState with dynamic fields', async () => {
     const Component = () => {
       const {
