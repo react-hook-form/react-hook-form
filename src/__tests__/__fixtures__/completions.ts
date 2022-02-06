6f96301f49e4c963b2c7b5c9ee6e196276be0b65
Felix Schorer <felixschorer@users.noreply.github.com>
2022-02-06 11:31:25 +0100

    Add comments for completions.ts

--- src/__tests__/__fixtures__/completions.ts
+++ src/__tests__/__fixtures__/completions.ts
@@ -2,10 +2,21 @@ import fs from 'fs';
 import * as ts from 'typescript';
 
 interface CompletionArgs {
+  /** cursor position within the code snippet */
   position: number;
+  /**
+   * Additional options for
+   * {@link typescript#LanguageService.getCompletionsAtPosition}
+   */
   options: ts.GetCompletionsAtPositionOptions | undefined;
 }
 
+/**
+ * Function to retrieve an array of {@link CompletionArgs} from a template
+ * literal.
+ * @param strings - the string parts of the template literal
+ * @param options - the interpolated options within the template literal
+ */
 function getCompletionArgs(
   strings: TemplateStringsArray,
   options: Array<ts.GetCompletionsAtPositionOptions | undefined>,
@@ -31,12 +42,55 @@ function getCompletionArgs(
   return completionArgs;
 }
 
-export function completionsFactory(dirname: string) {
+interface Completion {
+  /** the string which is displayed in the IntelliSense completion dropdown */
+  name: string;
+  /** the kind of IntelliSense completion */
+  kind: ts.ScriptElementKind;
+}
+
+/**
+ * Template tag function to retrieve IntelliSense completions for all the
+ * positions in the code snippet where an interpolation was inserted.
+ */
+export type CompletionsTag = (
+  strings: TemplateStringsArray,
+  ...options: Array<ts.GetCompletionsAtPositionOptions | undefined>
+) => Completion[][];
+
+/**
+ * Factory function to create a {@link CompletionsTag} template tag function.
+ *
+ * The interpolations within the template literal mark the positions, for which
+ * IntelliSense completions should be retrieved, and can be used to pass extra
+ * options to {@link typescript#LanguageService.getCompletionsAtPosition}.
+ *
+ * Think of the interpolations as cursor positions within that snippet.
+ *
+ * The template tag function returns an array of completions for each of the
+ * interpolations within the snippet.
+ *
+ * @param dirname - the directory which should be used for relative imports
+ * @example
+ * ```
+ * const completions = completionsFactory(__dirname)
+ *
+ * const [result] = completions`
+ *  import {useWatch, Control} from '..'
+ *  declare const control: Control<{foo: string}>
+ *  useWatch({
+ *    control,
+ *    name: '${undefined}'
+ *  })`
+ * ```
+ */
+export function completionsFactory(dirname: string): CompletionsTag {
   const scriptName = '__intelliSenseTestSnippet__.tsx';
 
   let scriptVersion = 0;
   let scriptContent = '';
 
+  // See https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API#incremental-build-support-using-the-language-services
   const servicesHost: ts.LanguageServiceHost = {
     getScriptFileNames: () => [scriptName],
     getScriptVersion: (fileName) =>
@@ -65,10 +119,7 @@ export function completionsFactory(dirname: string) {
     ts.createDocumentRegistry(),
   );
 
-  return (
-    strings: TemplateStringsArray,
-    ...options: Array<ts.GetCompletionsAtPositionOptions | undefined>
-  ) => {
+  return (strings, ...options) => {
     scriptVersion++;
     scriptContent = strings.join('');
 
