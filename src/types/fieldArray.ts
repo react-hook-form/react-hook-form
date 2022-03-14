d5795a3c2c9603cc3aecbf55ce5c9777b7ec6e4e
Bill <bluebill1049@hotmail.com>
2022-03-14 11:57:58 +1100

    📖 fix replace API TSDoc for fieldArray

--- src/types/fieldArray.ts
+++ src/types/fieldArray.ts
@@ -218,7 +218,7 @@ export type UseFieldArrayUpdate<
 ) => void;
 
 /**
- * 	Replace the entire field array values.
+ * Replace the entire field array values.
  *
  * @remarks
  * [API](https://react-hook-form.com/api/usefieldarray) • [Demo](https://codesandbox.io/s/calc-i231d)
