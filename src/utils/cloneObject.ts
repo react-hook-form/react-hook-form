1dba5bf50816e516788437ca10908f8299c1e06b
Ed Jones <edjones1999@gmail.com>
2022-03-22 10:46:21 +1100

    fix: Use `structuredClone` polyfill as replacement for shallow clone

--- src/utils/cloneObject.ts
+++ src/utils/cloneObject.ts
@@ -1,26 +1,5 @@
-import isFunction from './isFunction';
-import isObject from './isObject';
+import structuredClone from 'core-js-pure/actual/structured-clone';
 
 export default function cloneObject<T>(data: T): T {
-  let copy: any;
-  const isArray = Array.isArray(data);
-
-  if (data instanceof Date) {
-    copy = new Date(data);
-  } else if (data instanceof Set) {
-    copy = new Set(data);
-  } else if (isArray || isObject(data)) {
-    copy = isArray ? [] : {};
-    for (const key in data) {
-      if (isFunction(data[key])) {
-        copy = data;
-        break;
-      }
-      copy[key] = cloneObject(data[key]);
-    }
-  } else {
-    return data;
-  }
-
-  return copy;
+  return structuredClone(data);
 }
