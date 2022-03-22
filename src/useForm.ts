feba871871184d8699b2cd5ff228b58634ff9233
Beier Luo <bluebill1049@hotmail.com>
2022-03-23 08:46:01 +1100

    fix #8071 clean up at useForm useEffect

--- src/useForm.ts
+++ src/useForm.ts
@@ -104,6 +104,7 @@ export function useForm<
       control._subjects.state.next({});
     }
     control._removeUnmounted();
+    return () => control._removeUnmounted();
   });
 
   _formControl.current.formState = getProxyFormState(
