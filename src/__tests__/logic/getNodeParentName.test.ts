4a7e23a418b1d27dc147fd5d7420b71ca03a1364
Your Name <josef@pool.fi>
2022-04-11 17:15:40 +0300

    strict getNodeParentName

--- src/__tests__/logic/getNodeParentName.test.ts
+++ src/__tests__/logic/getNodeParentName.test.ts
@@ -3,11 +3,14 @@ import getFieldArrayParentName from '../../logic/getNodeParentName';
 describe('getFieldArrayParentName', () => {
   it('should return parent name when name is field array', () => {
     expect(getFieldArrayParentName('test.0')).toBe('test');
+    expect(getFieldArrayParentName('test[0]')).toBe('test');
     expect(getFieldArrayParentName('test.0.data.0')).toBe('test');
     expect(getFieldArrayParentName('test.data.0')).toBe('test.data');
   });
 
   it('should return empty string when name is not field array', () => {
     expect(getFieldArrayParentName('test')).toBe('test');
+    expect(getFieldArrayParentName('test0')).toBe('test0');
+    expect(getFieldArrayParentName('te1st')).toBe('te1st');
   });
 });
