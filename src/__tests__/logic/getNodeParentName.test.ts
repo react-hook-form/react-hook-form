1ed4777572cef5f3c93a10dd8970facd4e7b7bac
Josef Gullström <jgullstr@users.noreply.github.com>
2022-04-12 14:31:47 +0300

    🔨 support field array parents with numbers in name (#8167)
    * strict getNodeParentName
    
    * Update getNodeParentName.test.ts
    
    * Update getNodeParentName.ts
    
    Co-authored-by: Bill <bluebill1049@hotmail.com>

--- src/__tests__/logic/getNodeParentName.test.ts
+++ src/__tests__/logic/getNodeParentName.test.ts
@@ -1,13 +1,16 @@
-import getFieldArrayParentName from '../../logic/getNodeParentName';
+import getNodeParentName from '../../logic/getNodeParentName';
 
-describe('getFieldArrayParentName', () => {
+describe('getNodeParentName', () => {
   it('should return parent name when name is field array', () => {
-    expect(getFieldArrayParentName('test.0')).toBe('test');
-    expect(getFieldArrayParentName('test.0.data.0')).toBe('test');
-    expect(getFieldArrayParentName('test.data.0')).toBe('test.data');
+    expect(getNodeParentName('test.0')).toBe('test');
+    expect(getNodeParentName('test1.1')).toBe('test1');
+    expect(getNodeParentName('test.0.data.0')).toBe('test');
+    expect(getNodeParentName('test.data.0')).toBe('test.data');
   });
 
   it('should return empty string when name is not field array', () => {
-    expect(getFieldArrayParentName('test')).toBe('test');
+    expect(getNodeParentName('test')).toBe('test');
+    expect(getNodeParentName('test0')).toBe('test0');
+    expect(getNodeParentName('te1st')).toBe('te1st');
   });
 });
