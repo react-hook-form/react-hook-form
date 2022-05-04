bef5730d8ead96b9f4baf98975bfba3c85fc9b7a
Bill <bluebill1049@hotmail.com>
2022-05-05 08:38:01 +1000

    Update cloneObject.ts

--- src/utils/cloneObject.ts
+++ src/utils/cloneObject.ts
@@ -9,9 +9,9 @@ export default function cloneObject<T>(data: T): T {
     copy = new Date(data);
   } else if (data instanceof Set) {
     copy = new Set(data);
-  } else if (data instanceof Blob) {
+  } else if (globalThis.Blob && data instanceof Blob) {
     copy = data;
-  } else if (data instanceof FileList) {
+  } else if (globalThis.FileList && data instanceof FileList) {
     copy = data;
   } else if (isArray || isObject(data)) {
     copy = isArray ? [] : {};
