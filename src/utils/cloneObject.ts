16338f3ab66bd64546d09ad62dfff6b64732bf19
Bill <bluebill1049@hotmail.com>
2022-05-05 08:17:13 +1000

    skip filelist and blob for the current usage coverage

--- src/utils/cloneObject.ts
+++ src/utils/cloneObject.ts
@@ -9,10 +9,6 @@ export default function cloneObject<T>(data: T): T {
     copy = new Date(data);
   } else if (data instanceof Set) {
     copy = new Set(data);
-  } else if (globalThis.Blob && data instanceof Blob) {
-    copy = data;
-  } else if (globalThis.FileList && data instanceof FileList) {
-    copy = data;
   } else if (isArray || isObject(data)) {
     copy = isArray ? [] : {};
     for (const key in data) {
