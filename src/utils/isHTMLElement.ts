fd091328c38a00992cf0a8cc0dea66ecc2a2a2b3
Bill <bluebill1049@hotmail.com>
2022-06-01 09:06:51 +1000

    Update isHTMLElement.ts

--- src/utils/isHTMLElement.ts
+++ src/utils/isHTMLElement.ts
@@ -1,7 +1,6 @@
 export default (value: unknown): value is HTMLElement => {
   const owner = value ? ((value as HTMLElement).ownerDocument as Document) : 0;
-  const ElementClass = owner && owner.defaultView
-    ? owner.defaultView.HTMLElement
-    : HTMLElement;
+  const ElementClass =
+    owner && owner.defaultView ? owner.defaultView.HTMLElement : HTMLElement;
   return value instanceof ElementClass;
 };
