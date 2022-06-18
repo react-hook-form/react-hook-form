df3443f68f47fdd4a845947d2cd88d5fa37f307e
Beier Luo <bluebill1049@hotmail.com>
2022-06-18 15:26:01 +1000

    fix all type test

--- src/__tests__/useFieldArray/replace.test.tsx
+++ src/__tests__/useFieldArray/replace.test.tsx
@@ -234,7 +234,7 @@ describe('replace', () => {
               })}
             />
           ))}
-          <p>{errors.test?.[0].firstName?.message}</p>
+          <p>{errors?.test?.[0]?.firstName?.message}</p>
           <button
             type={'button'}
             onClick={() =>
