c9d2f220a44990615e2f5586db739401866b4481
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-07-13 17:19:37 -0400

    Refactoring

---
+++ scripts/README/Previews/Helper.js
@@ -0,0 +1,19 @@
+
+import { toProfile , toAvatar } from 'Link';
+
+
+export default function generate(helper){
+    
+    const { github , avatar } = helper;
+    
+    const lines = [
+        `<a href = '${ toProfile(github) }'>` ,
+        `    <img` ,
+        `        width = 25` ,
+        `        src = '${ toAvatar(avatar) }'` ,
+        `    />` ,
+        `</a>`
+    ]
+    
+    return lines.join('\n');
+}
---
+++ scripts/README/Previews/mod.ts
@@ -0,0 +1,6 @@
+
+import individuals from './Individual.js'
+import companies from './Company.js'
+import helpers from './Company.js'
+
+export default { individuals , companies , helpers }
