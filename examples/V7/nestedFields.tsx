734ca4c33e4f963bbacf599033116974ce65297b
Software Engineer <89561014+HarmonyEarth@users.noreply.github.com>
2022-08-12 18:57:46 -0400

    📖 fix nested fields example (#8840)
    * Fixed nested fields example
    
    * Update nestedFields.tsx
    
    Co-authored-by: Bill <bluebill1049@hotmail.com>

--- examples/V7/nestedFields.tsx
+++ examples/V7/nestedFields.tsx
@@ -1,41 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { useForm } from 'react-hook-form';
-import FormSection1 from './formSection1';
-import FormSection2 from './formSection2';
+import React from "react";
+import { useForm } from "react-hook-form";
 
-export default function Section1({ register }) {
+export function FormSection1({ register }) {
   return (
     <>
       <div>
         <label>First name</label>
         <input
           type="text"
-          {...register('firstName', { required: true, maxLength: 80 })}
+          {...register("firstName", { required: true, maxLength: 80 })}
         />
       </div>
       <div>
         <label>Last name</label>
         <input
           type="text"
-          {...register('lastName', { required: true, maxLength: 100 })}
+          {...register("lastName", { required: true, maxLength: 100 })}
         />
       </div>
     </>
   );
 }
 
-export default function formSection2({ register }) {
+export function FormSection2({ register }) {
   return (
     <>
       <div>
         <label>Email</label>
         <input
-          type="text"
-          {...register('email', {
-            required: true,
-            pattern:
-              /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          type="email"
+          {...register("email", {
+            required: true
           })}
         />
       </div>
@@ -43,16 +38,16 @@ export default function formSection2({ register }) {
         <label>Mobile number</label>
         <input
           type="tel"
-          {...register('mobileNumber', {
+          {...register("mobileNumber", {
             required: true,
             maxLength: 11,
-            minLength: 8,
+            minLength: 8
           })}
         />
       </div>
       <div>
         <label>Title</label>
-        <select {...register('title', { required: true })}>
+        <select {...register("title", { required: true })}>
           <option value="Mr">Mr</option>
           <option value="Mrs">Mrs</option>
           <option value="Miss">Miss</option>
@@ -65,12 +60,12 @@ export default function formSection2({ register }) {
         <input
           type="radio"
           value="Yes"
-          {...register('developer', { required: true })}
+          {...register("developer", { required: true })}
         />
         <input
           type="radio"
           value="No"
-          {...register('developer', { required: true })}
+          {...register("developer", { required: true })}
         />
       </div>
     </>
@@ -78,11 +73,10 @@ export default function formSection2({ register }) {
 }
 
 export default function App() {
-  const { register, errors, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm();
   const onSubmit = (data) => {
     alert(JSON.stringify(data));
   };
-  console.log(errors);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
