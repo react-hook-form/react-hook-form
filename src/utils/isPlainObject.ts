287c75e774fec01b8dac75c0308d275b2d8e9f79
Beier Luo <bluebill1049@hotmail.com>
2022-09-22 20:32:42 +1000

    improve the logic and variable name

--- src/utils/isPlainObject.ts
+++ src/utils/isPlainObject.ts
@@ -1,10 +1,11 @@
 // Credit and thanks goes to: https://github.com/jonschlinkert/is-plain-object
 import isObject from './isObject';
 
-export function isPlainObject(o: object) {
-  const prototypeCopy = o.constructor.prototype;
+export function isPlainObject(tempObject: object) {
+  const prototypeCopy =
+    tempObject.constructor && tempObject.constructor.prototype;
 
-  return !(
-    !isObject(prototypeCopy) || !prototypeCopy.hasOwnProperty('isPrototypeOf')
+  return (
+    isObject(prototypeCopy) && prototypeCopy.hasOwnProperty('isPrototypeOf')
   );
 }
