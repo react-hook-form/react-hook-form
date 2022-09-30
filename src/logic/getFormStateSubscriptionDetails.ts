56e229a666a3213f459ddef64cd9479cf769a07a
Beier Luo <bluebill1049@hotmail.com>
2022-09-30 14:13:33 +1000

    remove optional chain syntax

--- src/logic/getFormStateSubscriptionDetails.ts
+++ src/logic/getFormStateSubscriptionDetails.ts
@@ -3,8 +3,8 @@ import { FormStateSubscription } from '../types';
 export default (subscription?: FormStateSubscription) => ({
   isDirty: !!(subscription && subscription.isDirty),
   dirtyFields: !!(subscription && subscription.dirtyFields),
-  touchedFields: !!(subscription && subscription?.touchedFields),
-  isValidating: !!(subscription && subscription?.isValidating),
-  isValid: !!(subscription && subscription?.isValidating),
-  errors: !!(subscription && subscription?.errors),
+  touchedFields: !!(subscription && subscription.touchedFields),
+  isValidating: !!(subscription && subscription.isValidating),
+  isValid: !!(subscription && subscription.isValidating),
+  errors: !!(subscription && subscription.errors),
 });
