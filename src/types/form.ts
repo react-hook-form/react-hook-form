29bd29635069e0807b4e83d95706042ffcecdbcb
Beier Luo <bluebill1049@hotmail.com>
2022-10-27 16:33:31 +1100

    fix type

--- src/types/form.ts
+++ src/types/form.ts
@@ -128,7 +128,10 @@ export type FormState<TFieldValues extends FieldValues> = {
   isValidating: boolean;
   isValid: boolean;
   submitCount: number;
-  defaultValues: Readonly<DeepPartial<TFieldValues>> | TFieldValues | undefined;
+  defaultValues?:
+    | Readonly<DeepPartial<TFieldValues>>
+    | TFieldValues
+    | undefined;
   dirtyFields: Partial<Readonly<FieldNamesMarkedBoolean<TFieldValues>>>;
   touchedFields: Partial<Readonly<FieldNamesMarkedBoolean<TFieldValues>>>;
   errors: FieldErrors<TFieldValues>;
