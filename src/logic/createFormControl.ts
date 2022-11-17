34d5de49c7ea048d86b99f30477abafd696b0541
Beier Luo <bluebill1049@hotmail.com>
2022-11-17 19:39:19 +1100

    fix type cast

--- src/logic/createFormControl.ts
+++ src/logic/createFormControl.ts
@@ -21,6 +21,7 @@ import {
   SetFieldValue,
   SetValueConfig,
   Subjects,
+  UnPackDefaultValues,
   UseFormClearErrors,
   UseFormGetFieldState,
   UseFormGetValues,
@@ -591,7 +592,8 @@ export function createFormControl<
         true,
       );
 
-    options.shouldValidate && trigger(name as Path<TFieldValues>);
+    options.shouldValidate &&
+      trigger(name as Path<UnPackDefaultValues<TFieldValues>>);
   };
 
   const setValues = <
