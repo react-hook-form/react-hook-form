f0a46581fb5a45ed958b82cdb8e9c5211cde9963
Beier (Bill) <bluebill1049@hotmail.com>
2023-01-04 08:22:13 +1100

    Update validateField.ts

--- src/logic/validateField.ts
+++ src/logic/validateField.ts
@@ -11,6 +11,7 @@ import isCheckBoxInput from '../utils/isCheckBoxInput';
 import isEmptyObject from '../utils/isEmptyObject';
 import isFileInput from '../utils/isFileInput';
 import isFunction from '../utils/isFunction';
+import isHTMLElement from '../utils/isHTMLElement';
 import isMessage from '../utils/isMessage';
 import isNullOrUndefined from '../utils/isNullOrUndefined';
 import isObject from '../utils/isObject';
@@ -24,7 +25,6 @@ import getCheckboxValue from './getCheckboxValue';
 import getRadioValue from './getRadioValue';
 import getValidateError from './getValidateError';
 import getValueAndMessage from './getValueAndMessage';
-import isHTMLElement from '../utils/isHTMLElement';
 
 export default async <T extends NativeFieldValue>(
   field: Field,
