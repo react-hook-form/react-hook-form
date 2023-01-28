044652e6ed417ee3320fd5911a4df6736a93f688
Beier Luo <bluebill1049@hotmail.com>
2023-01-28 18:12:24 +1100

    support react native with render prop

--- src/form.tsx
+++ src/form.tsx
@@ -9,7 +9,10 @@ type Props<
   U extends FieldValues | undefined = undefined,
 > = Partial<{
   control: Control<T>;
-  children: React.ReactNode | React.ReactNode[];
+  children?: React.ReactNode | React.ReactNode[];
+  render?: (props: {
+    submit: (e?: React.FormEvent) => void;
+  }) => React.ReactNode | React.ReactNode[];
   onSubmit: U extends FieldValues ? SubmitHandler<U> : SubmitHandler<T>;
   resolve: (response: Response) => void;
   reject: (response?: Response) => void;
@@ -31,57 +34,61 @@ export function Form<
     action,
     method,
     headers,
+    render,
     reject,
     resolve,
     validateStatus,
     ...rest
   } = props;
 
+  const submit = control.handleSubmit(
+    onSubmit ||
+      (async (data) => {
+        try {
+          const response = await fetch(String(action), {
+            method: method || 'post',
+            headers: {
+              'Content-Type': 'application/json',
+              ...headers,
+            },
+            body: JSON.stringify(data),
+          });
+
+          if (
+            validateStatus
+              ? !validateStatus(response.status)
+              : response.status < 200 || response.status >= 300
+          ) {
+            control.setError(SERVER_ERROR_TYPE, {
+              type: String(response.status),
+            });
+            reject && reject(response);
+          } else {
+            resolve && resolve(response);
+          }
+        } catch (e: unknown) {
+          control.setError(SERVER_ERROR_TYPE, {
+            type: 'error',
+          });
+          reject && reject();
+        }
+      }),
+  );
+
   React.useEffect(() => {
     setMounted(true);
   }, []);
 
-  return (
+  return render ? (
+    render({
+      submit,
+    })
+  ) : (
     <form
       noValidate={mounted}
       action={action}
       method={method}
-      onSubmit={
-        onSubmit
-          ? control.handleSubmit(onSubmit)
-          : (e) => {
-              control.handleSubmit(async (data) => {
-                try {
-                  const response = await fetch(String(action), {
-                    method: method || 'post',
-                    headers: {
-                      'Content-Type': 'application/json',
-                      ...headers,
-                    },
-                    body: JSON.stringify(data),
-                  });
-
-                  if (
-                    validateStatus
-                      ? !validateStatus(response.status)
-                      : response.status < 200 || response.status >= 300
-                  ) {
-                    control.setError(SERVER_ERROR_TYPE, {
-                      type: String(response.status),
-                    });
-                    reject && reject(response);
-                  } else {
-                    resolve && resolve(response);
-                  }
-                } catch (e: unknown) {
-                  control.setError(SERVER_ERROR_TYPE, {
-                    type: 'error',
-                  });
-                  reject && reject();
-                }
-              })(e);
-            }
-      }
+      onSubmit={submit}
       {...rest}
     >
       {children}
