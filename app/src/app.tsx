642ad247b4719c170d884499d3a9e35b00d18f99
bill <bluebill1049@hotmail.com>
2023-06-01 12:56:13 +1000

    fix import inconsistency

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AutoUnregister from './autoUnregister';
 import Basic from './basic';
--- app/src/watchUseFieldArrayNested.tsx
+++ app/src/watchUseFieldArrayNested.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { Controller, useFieldArray, useForm, Control } from 'react-hook-form';
 
 type FormValues = {
