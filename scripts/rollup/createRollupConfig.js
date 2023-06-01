1aaa5c47dd76d96a1d869028a48f7c6707fe61f2
moshyfawn <email@moshyfawn.dev>
2023-06-01 19:44:22 -0400

    build: use up-to-date terser plugin

--- scripts/rollup/createRollupConfig.js
+++ scripts/rollup/createRollupConfig.js
@@ -1,7 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 import external from 'rollup-plugin-peer-deps-external';
 import sourcemaps from 'rollup-plugin-sourcemaps';
-import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
 export function createRollupConfig(options, callback) {
