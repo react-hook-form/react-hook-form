3ca17019c86535a331e32b465019ec25bbbf02fb
Beier Luo <bluebill1049@hotmail.com>
2023-08-01 09:11:59 +1000

    Revert "🧑‍🎓 close #10585 update controller value on each render (#10586)"
    This reverts commit 27ac86d4819a0a231a9669317c62649d8f65d27d.

--- src/logic/createFormControl.ts
+++ src/logic/createFormControl.ts
@@ -949,10 +949,6 @@ export function createFormControl<
     });
     _names.mount.add(name);
 
-    if (!isUndefined(options.value)) {
-      set(_formValues, name, options.value);
-    }
-
     field
       ? disabledIsDefined &&
         set(
