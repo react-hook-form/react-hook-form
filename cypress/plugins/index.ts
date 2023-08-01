109a12e5fb2f24dc64631a3b77f3570eebba86de
Jason Laster <jason.laster.11@gmail.com>
2023-07-31 17:58:38 -0700

    📼 add Replay Test Suites (#10681)
    Co-authored-by: Beier (Bill) <bluebill1049@hotmail.com>

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -8,10 +8,18 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
+import replay from '@replayio/cypress';
+
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-module.exports = () => {
+module.exports = (
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions,
+) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  replay(on, config);
+  return config;
 };
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,4 @@ import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+require('@replayio/cypress/support');
