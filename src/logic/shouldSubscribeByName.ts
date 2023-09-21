de4a917f801bf72434ecc199c9fa2773471199f3
nitschSB <39341455+nitschSB@users.noreply.github.com>
2023-09-21 23:10:39 +0200

    🪖 simplify logic for subscription by name (#10947)
    Co-authored-by: Beier (Bill) <bluebill1049@hotmail.com>

--- src/logic/shouldSubscribeByName.ts
+++ src/logic/shouldSubscribeByName.ts
@@ -5,18 +5,14 @@ export default <T extends string | string[] | undefined>(
   signalName?: string,
   exact?: boolean,
 ) =>
-  exact && signalName
-    ? name === signalName ||
-      (Array.isArray(name) &&
-        name.some(
-          (currentName) => currentName && exact && currentName === signalName,
-        ))
-    : !name ||
-      !signalName ||
-      name === signalName ||
-      convertToArrayPayload(name).some(
-        (currentName) =>
-          currentName &&
-          (currentName.startsWith(signalName) ||
-            signalName.startsWith(currentName)),
-      );
+  !name ||
+  !signalName ||
+  name === signalName ||
+  convertToArrayPayload(name).some(
+    (currentName) =>
+      currentName &&
+      (exact
+        ? currentName === signalName
+        : currentName.startsWith(signalName) ||
+          signalName.startsWith(currentName)),
+  );
