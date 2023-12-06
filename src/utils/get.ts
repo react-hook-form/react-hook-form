704c3fa2e99fa098286664447f004aa85b5771d3
pmhxxhsj <minho7952@naver.com>
2023-12-07 00:38:56 +0900

    refactor: Matching get and set util conventions
    - obj => object
    - set util change arrow function

--- src/utils/get.ts
+++ src/utils/get.ts
@@ -3,20 +3,20 @@ import isNullOrUndefined from './isNullOrUndefined';
 import isObject from './isObject';
 import isUndefined from './isUndefined';
 
-export default <T>(obj: T, path?: string, defaultValue?: unknown): any => {
-  if (!path || !isObject(obj)) {
+export default <T>(object: T, path?: string, defaultValue?: unknown): any => {
+  if (!path || !isObject(object)) {
     return defaultValue;
   }
 
   const result = compact(path.split(/[,[\].]+?/)).reduce(
     (result, key) =>
       isNullOrUndefined(result) ? result : result[key as keyof {}],
-    obj,
+    object,
   );
 
-  return isUndefined(result) || result === obj
-    ? isUndefined(obj[path as keyof T])
+  return isUndefined(result) || result === object
+    ? isUndefined(object[path as keyof T])
       ? defaultValue
-      : obj[path as keyof T]
+      : object[path as keyof T]
     : result;
 };
