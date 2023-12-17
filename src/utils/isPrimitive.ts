bf3c1b0308c8a3ad2f4affdc9549de444c572e89
Kuesung Park <gueit214@naver.com>
2023-12-17 15:58:15 +0900

    refactor : redundant unll/undefined checks

--- src/utils/isPrimitive.ts
+++ src/utils/isPrimitive.ts
@@ -3,5 +3,4 @@ import { Primitive } from '../types';
 import isNullOrUndefined from './isNullOrUndefined';
 import { isObjectType } from './isObject';
 
-export default (value: unknown): value is Primitive =>
-  isNullOrUndefined(value) || !isObjectType(value);
+export default (value: unknown): value is Primitive => !isObjectType(value);
