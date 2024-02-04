4613f3e0a306b622bc985304301a83a7ac34fcf1
deep.golani <deep.golani@bfhl.in>
2024-02-04 11:07:12 +0530

    added removed code

--- src/utils/insert.ts
+++ src/utils/insert.ts
@@ -1,5 +1,11 @@
 import convertToArrayPayload from './convertToArrayPayload';
 
+export default function insert<T>(data: T[], index: number): (T | undefined)[];
+export default function insert<T>(
+  data: T[],
+  index: number,
+  value: T | T[],
+): T[];
 export default function insert<T>(
   data: T[],
   index: number,
