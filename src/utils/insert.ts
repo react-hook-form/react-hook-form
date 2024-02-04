02274e8c4d3648398b1c35b0d780a8985db6a2d4
deep.golani <deep.golani@bfhl.in>
2024-02-04 11:05:24 +0530

    remove unused functions

--- src/utils/insert.ts
+++ src/utils/insert.ts
@@ -1,11 +1,5 @@
 import convertToArrayPayload from './convertToArrayPayload';
 
-export default function insert<T>(data: T[], index: number): (T | undefined)[];
-export default function insert<T>(
-  data: T[],
-  index: number,
-  value: T | T[],
-): T[];
 export default function insert<T>(
   data: T[],
   index: number,
