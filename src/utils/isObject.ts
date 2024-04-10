d7a647f0a58366405edde8f68438528b916238f9
Yummy-sk <kunshup2000@gmail.com>
2024-04-10 16:58:20 +0900

    chore: exclude set object from isObject function

--- src/utils/isObject.ts
+++ src/utils/isObject.ts
@@ -1,5 +1,6 @@
 import isDateObject from './isDateObject';
 import isNullOrUndefined from './isNullOrUndefined';
+import isSetObject from './isSetObject';
 
 export const isObjectType = (value: unknown): value is object =>
   typeof value === 'object';
@@ -8,4 +9,5 @@ export default <T extends object>(value: unknown): value is T =>
   !isNullOrUndefined(value) &&
   !Array.isArray(value) &&
   isObjectType(value) &&
-  !isDateObject(value);
+  !isDateObject(value) &&
+  !isSetObject(value);
