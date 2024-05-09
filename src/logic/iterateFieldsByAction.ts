2d4160dc8d0db1a52dbad7bb216300e6dcebfee3
CJ Young <chrjyoung@gmail.com>
2024-05-08 23:46:55 -0400

    feat: allow `iterateFieldsByAction` to iterate over multiple refs

--- src/logic/iterateFieldsByAction.ts
+++ src/logic/iterateFieldsByAction.ts
@@ -7,15 +7,27 @@ const iterateFieldsByAction = (
   action: (ref: Ref, name: string) => 1 | undefined | void,
   fieldsNames?: Set<InternalFieldName> | InternalFieldName[] | 0,
   abortEarly?: boolean,
+  runOnAllRefs?: boolean,
 ) => {
+  const iterationAdjustedAction = (refs: HTMLInputElement[], key: string) => {
+    return refs
+      .slice(0, runOnAllRefs ? undefined : 1)
+      .map((ref: Ref) => action(ref, key))
+      .some(Boolean);
+  };
+
   for (const key of fieldsNames || Object.keys(fields)) {
     const field = get(fields, key);
 
     if (field) {
       const { _f, ...currentField } = field;
-
       if (_f) {
-        if (_f.refs && _f.refs[0] && action(_f.refs[0], key) && !abortEarly) {
+        if (
+          _f.refs &&
+          _f.refs[0] &&
+          iterationAdjustedAction(_f.refs, key) &&
+          !abortEarly
+        ) {
           break;
         } else if (_f.ref && action(_f.ref, _f.name) && !abortEarly) {
           break;
