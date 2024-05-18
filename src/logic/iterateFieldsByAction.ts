d660b2358d4a102f5bdd8373c87bb41533d488e6
Beier (Bill) <bluebill1049@hotmail.com>
2024-05-18 16:00:56 +1000

    Update iterateFieldsByAction.ts

--- src/logic/iterateFieldsByAction.ts
+++ src/logic/iterateFieldsByAction.ts
@@ -6,8 +6,7 @@ const iterateFieldsByAction = (
   fields: FieldRefs,
   action: (ref: Ref, name: string) => 1 | undefined | void,
   fieldsNames?: Set<InternalFieldName> | InternalFieldName[] | 0,
-  abortEarly = true,
-  iterateRefs?: boolean,
+  abortEarly?: boolean,
 ) => {
   for (const key of fieldsNames || Object.keys(fields)) {
     const field = get(fields, key);
@@ -16,22 +15,9 @@ const iterateFieldsByAction = (
       const { _f, ...currentField } = field;
 
       if (_f) {
-        if (iterateRefs && _f.refs) {
-          let shouldAbort = false;
-          for (const ref of _f.refs) {
-            shouldAbort = (!!action(ref, key) && abortEarly) || shouldAbort;
-          }
-          if (shouldAbort) {
-            break;
-          }
-        } else if (
-          _f.refs &&
-          _f.refs[0] &&
-          action(_f.refs[0], key) &&
-          abortEarly
-        ) {
+        if (_f.refs && _f.refs[0] && action(_f.refs[0], key) && !abortEarly) {
           break;
-        } else if (_f.ref && action(_f.ref, _f.name) && abortEarly) {
+        } else if (_f.ref && action(_f.ref, _f.name) && !abortEarly) {
           break;
         } else {
           iterateFieldsByAction(currentField, action);
