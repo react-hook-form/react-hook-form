0245d65462efefa47c4e31ff0036f2b720d8f367
bill <bluebill1049@hotmail.com>
2024-08-18 17:27:36 +1000

    optimise the logic

--- src/logic/hasPromiseValidation.ts
+++ src/logic/hasPromiseValidation.ts
@@ -1,22 +1,17 @@
 import { Field, Validate } from '../types';
+import isFunction from '../utils/isFunction';
 import isObject from '../utils/isObject';
 
 const ASYNC_FUNCTION = 'AsyncFunction';
 
-export default (fieldReference: Field['_f']) => {
-  if (!fieldReference || !fieldReference.validate) {
-    return false;
-  }
-
-  if (fieldReference.validate.constructor.name === ASYNC_FUNCTION) {
-    return true;
-  }
-
-  return !!(
-    isObject(fieldReference.validate) &&
-    Object.values(fieldReference.validate).find(
-      (validateFunction: Validate<unknown, unknown>) =>
-        validateFunction.constructor.name === ASYNC_FUNCTION,
-    )
+export default (fieldReference: Field['_f']) =>
+  (!fieldReference || !fieldReference.validate) &&
+  !!(
+    (isFunction(fieldReference.validate) &&
+      fieldReference.validate.constructor.name === ASYNC_FUNCTION) ||
+    (isObject(fieldReference.validate) &&
+      Object.values(fieldReference.validate).find(
+        (validateFunction: Validate<unknown, unknown>) =>
+          validateFunction.constructor.name === ASYNC_FUNCTION,
+      ))
   );
-};
