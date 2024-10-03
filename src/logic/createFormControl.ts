27ba0ebae226a22755271394e83b40a151c6e714
cryptobox <pray.happy76@gmail.com>
2024-10-03 21:10:46 +0900

    fix: ensure Invalid Date is evaluated correctly

--- src/logic/createFormControl.ts
+++ src/logic/createFormControl.ts
@@ -708,6 +708,7 @@ export function createFormControl<
     const _updateIsFieldValueUpdated = (fieldValue: any): void => {
       isFieldValueUpdated =
         Number.isNaN(fieldValue) ||
+        (fieldValue instanceof Date && isNaN(fieldValue.getTime())) ||
         deepEqual(fieldValue, get(_formValues, name, fieldValue));
     };
 
