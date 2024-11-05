98a305238a88c1b9d317d4a196237b4ec8d3bfab
Wonsuk Choi <sukvvon@gmail.com>
2024-11-05 12:04:12 +0900

    chore(eslint.config.mjs): apply eslint --fix to resolve formatting issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,26 @@
 // @ts-check
 
-import tseslint from 'typescript-eslint';
-import reactPlugin from 'eslint-plugin-react';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import pluginCypress from 'eslint-plugin-cypress/flat';
-import reactHookPlugin from "eslint-plugin-react-hooks";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { fixupPluginRules } from '@eslint/compat';
+import pluginCypress from 'eslint-plugin-cypress/flat';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import reactPlugin from 'eslint-plugin-react';
+import reactHookPlugin from 'eslint-plugin-react-hooks';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ["app", "examples", "node_modules", "dist", "coverage", "src/types/global.d.ts","!.*.js", "reports", "scripts/README"],
+    ignores: [
+      'app',
+      'examples',
+      'node_modules',
+      'dist',
+      'coverage',
+      'src/types/global.d.ts',
+      '!.*.js',
+      'reports',
+      'scripts/README',
+    ],
   },
   reactPlugin.configs.flat.recommended,
   ...tseslint.configs.recommended,
@@ -19,7 +29,7 @@ export default tseslint.config(
   {
     plugins: {
       'react-hooks': fixupPluginRules(reactHookPlugin),
-      "simple-import-sort": simpleImportSort
+      'simple-import-sort': simpleImportSort,
     },
     languageOptions: {
       ecmaVersion: 2020,
