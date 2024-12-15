5a961592fed5da98bc9cf9e84700bdd82eaf05c1
Beier (Bill) <bluebill1049@hotmail.com>
2024-12-15 11:01:17 +1100

    ⚛️ upgrade e2e automation app to react 19 (#12482)
    * ⚛️ fix #12478 issue should unregister input with controller
    
    * fix with automation issue
    
    * upgrade app to react 19
    
    * upgrade to react 19
    
    * update all automation tests left with useFieldArray
    
    * WIP: left with last test failing
    
    * fix last automation tests
    
    * pass the test with isValid update
    
    * revert back the change
    
    * fix automation issue
    
    * Revert "fix automation issue"
    
    This reverts commit 04876a4335823ad10836404d1f69b501c4f44ffb.
    
    * fix automation test
    
    * fix blur issue with useEffect validate function
    
    * fix automation tests

--- cypress/e2e/formStateWithSchema.cy.ts
+++ cypress/e2e/formStateWithSchema.cy.ts
@@ -279,7 +279,7 @@ describe('form state with schema validation', () => {
         isValid: true,
       }),
     );
-    cy.get('#renderCount').contains('14');
+    cy.get('#renderCount').contains('15');
   });
 
   it('should reset dirty value when inputs reset back to default with onSubmit mode', () => {
@@ -495,7 +495,7 @@ describe('form state with schema validation', () => {
         isValid: false,
       }),
     );
-    cy.get('#renderCount').contains('8');
+    cy.get('#renderCount').contains('9');
   });
 
   it('should reset dirty value when inputs reset back to default with onChange mode', () => {
