cfb64af45747aed36790835a3f687e6030f80525
bill <bluebill1049@hotmail.com>
2025-04-21 12:20:46 +1000

    🐞 fix #12763 import warning with named exports

--- src/useDeepEqualEffect.ts
+++ src/useDeepEqualEffect.ts
@@ -1,17 +1,17 @@
-import { DependencyList, EffectCallback, useEffect, useRef } from 'react';
+import * as React from 'react';
 
 import deepEqual from './utils/deepEqual';
 
-export const useDeepEqualEffect = <T extends DependencyList>(
-  effect: EffectCallback,
+export const useDeepEqualEffect = <T extends React.DependencyList>(
+  effect: React.EffectCallback,
   deps: T,
 ) => {
-  const ref = useRef<T>(deps);
+  const ref = React.useRef<T>(deps);
 
   if (!deepEqual(deps, ref.current)) {
     ref.current = deps;
   }
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(effect, ref.current);
+  React.useEffect(effect, ref.current);
 };
