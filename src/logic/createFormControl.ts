5b7412314b381e7855ffa16c5601be2d7c9bd7f8
Bill <bluebill1049@hotmail.com>
2025-06-21 20:11:09 +1000

    🐞 fix #12900 issue with formData reference clone (#12906)
    * 🐞 fix #12900 issue with formData reference clone
    
    * use unset instead of set

--- src/logic/createFormControl.ts
+++ src/logic/createFormControl.ts
@@ -1234,14 +1234,14 @@ export function createFormControl<
       if (_options.resolver) {
         const { errors, values } = await _runSchema();
         _formState.errors = errors;
-        fieldValues = values as TFieldValues;
+        fieldValues = cloneObject(values) as TFieldValues;
       } else {
         await executeBuiltInValidation(_fields);
       }
 
       if (_names.disabled.size) {
         for (const name of _names.disabled) {
-          set(fieldValues, name, undefined);
+          unset(fieldValues, name);
         }
       }
 
