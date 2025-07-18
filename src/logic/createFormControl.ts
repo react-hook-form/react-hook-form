834230aea661f3bdd269661d5381c5ad2f7e51ce
Aditya pradhan <pradhanadi741@gmail.com>
2025-07-19 01:00:52 +0530

    fix: prevent onBlur for readOnly fields

--- src/logic/createFormControl.ts
+++ src/logic/createFormControl.ts
@@ -766,12 +766,16 @@ export function createFormControl<
 
       set(_formValues, name, fieldValue);
 
-      if (isBlurEvent) {
-        field._f.onBlur && field._f.onBlur(event);
-        delayErrorCallback && delayErrorCallback(0);
-      } else if (field._f.onChange) {
-        field._f.onChange(event);
-      }
+     if (isBlurEvent) {
+  const target = event.target as HTMLInputElement;
+  if (!target.readOnly) {
+    field._f.onBlur && field._f.onBlur(event);
+    delayErrorCallback && delayErrorCallback(0);
+  }
+} else if (field._f.onChange) {
+  field._f.onChange(event);
+}
+
 
       const fieldState = updateTouchAndDirty(name, fieldValue, isBlurEvent);
 
