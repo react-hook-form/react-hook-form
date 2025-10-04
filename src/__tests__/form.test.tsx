bfea2919afeb39ba37a43aab71015d6e3e9bc970
bill <bluebill1049@hotmail.com>
2025-10-04 11:18:00 +1000

    Revert "remove msw"
    This reverts commit c610f5a959180a6f7b35f902c239f3023e79b5f4.

--- src/__tests__/form.test.tsx
+++ src/__tests__/form.test.tsx
@@ -2,59 +2,70 @@ import 'whatwg-fetch';
 
 import React from 'react';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { http, HttpResponse } from 'msw';
+import { setupServer } from 'msw/node';
 
 import { Form } from '../form';
 import { useForm } from '../useForm';
 import { FormProvider } from '../useFormContext';
 
-describe('Form', () => {
-  beforeEach(() => {
-    jest.spyOn(global, 'fetch').mockImplementation(async (input, init) => {
-      const url = typeof input === 'string' ? input : input.url;
-
-      switch (url) {
-        case '/success':
-          return new Response(JSON.stringify({ message: 'ok' }), {
-            status: 200,
-            headers: { 'Content-Type': 'application/json' },
-          });
-        case '/error':
-          return new Response(null, { status: 500 });
-        case '/status':
-          return new Response(null, { status: 201 });
-        case '/get':
-          return new Response(null, { status: 200 });
-        case '/json':
-          if (
-            init?.headers &&
-            (init.headers as Record<string, string>)['content-type'] ===
-              'application/json'
-          ) {
-            return new Response(null, { status: 200 });
-          }
-          return new Response(null, { status: 500 });
-        case '/formData':
-          if (init?.body instanceof FormData) {
-            return new Response(null, { status: 200 });
-          }
-          return new Response(null, { status: 500 });
-        default:
-          return new Response(null, { status: 404 });
-      }
+const server = setupServer(
+  http.post('/success', () => {
+    return HttpResponse.json({ message: 'ok' });
+  }),
+  http.post('/error', () => {
+    return new Response(null, {
+      status: 500,
     });
-  });
+  }),
+  http.post('/status', () => {
+    return new HttpResponse(null, { status: 201 });
+  }),
+  http.post('/get', () => {
+    return new HttpResponse(null, { status: 200 });
+  }),
+  http.post('/json', ({ request }) => {
+    if (request.headers.get('content-type') === 'application/json') {
+      return new HttpResponse(null, { status: 200 });
+    }
 
-  afterEach(() => {
-    jest.restoreAllMocks();
-  });
+    return new HttpResponse(null, { status: 500 });
+  }),
+  http.post('/formData', async ({ request }) => {
+    if (
+      request.headers.get('content-type')?.startsWith('multipart/form-data') &&
+      request.headers.get('content-type')?.includes('boundary=')
+    ) {
+      try {
+        await request.formData();
+        return new HttpResponse(null, { status: 200 });
+      } catch {
+        return new HttpResponse(null, { status: 500 });
+      }
+    }
+
+    return new HttpResponse(null, { status: 500 });
+  }),
+);
+
+describe('Form', () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
 
   it('should support render with both form tag and headless', () => {
-    const WithContext = () => (
-      <>
-        <Form />
-        <Form render={() => null} />
-      </>
-    );
+    const WithContext = () => {
+      return (
+        <>
+          <Form />
+          <Form
+            render={() => {
+              return null;
+            }}
+          />
+        </>
+      );
+    };
 
     const App = () => {
       const methods = useForm();
@@ -63,7 +74,13 @@ describe('Form', () => {
           <Form control={methods.control}>
             <input />
           </Form>
-          <Form control={methods.control} render={() => null} />
+          <Form
+            control={methods.control}
+            render={() => {
+              return null;
+            }}
+          />
+
           <FormProvider {...methods}>
             <WithContext />
           </FormProvider>
@@ -89,7 +106,12 @@ describe('Form', () => {
         <Form
           encType={'application/json'}
           action={'/success'}
-          onSubmit={onSubmit}
+          onSubmit={({ data, formData, formDataJson }) => {
+            data;
+            formData;
+            formDataJson;
+            onSubmit();
+          }}
           control={control}
           onError={onError}
           onSuccess={async ({ response }) => {
@@ -115,6 +137,222 @@ describe('Form', () => {
       expect(onError).not.toHaveBeenCalled();
       screen.getByText('submitSuccessful');
       screen.getByText('ok');
+    });
+  });
+
+  it('should handle error request callback', async () => {
+    const onSubmit = jest.fn();
+    const onSuccess = jest.fn();
+
+    const App = () => {
+      const {
+        control,
+        formState: { isSubmitSuccessful, errors },
+      } = useForm();
+
+      return (
+        <Form
+          encType={'application/json'}
+          action={'/error'}
+          onSubmit={onSubmit}
+          control={control}
+        >
+          <button>Submit</button>
+          <p>{isSubmitSuccessful ? 'submitSuccessful' : 'submitFailed'}</p>
+          {errors.root?.server && 'This is a server error'}
+          <p>{errors.root?.server?.type}</p>
+        </Form>
+      );
+    };
+
+    render(<App />);
+
+    fireEvent.click(screen.getByRole('button'));
+
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalled();
+      expect(onSuccess).not.toHaveBeenCalled();
+      screen.getByText('This is a server error');
+      screen.getByText('500');
+      screen.getByText('submitFailed');
+    });
+  });
+
+  it('should validate custom status code', async () => {
+    const App = () => {
+      const {
+        control,
+        formState: { isSubmitSuccessful },
+      } = useForm();
+
+      return (
+        <Form
+          encType={'application/json'}
+          action={'/status'}
+          control={control}
+          validateStatus={(status) => status === 200}
+        >
+          <button>Submit</button>
+          <p>{isSubmitSuccessful ? 'submitSuccessful' : 'submitFailed'}</p>
+        </Form>
+      );
+    };
+
+    render(<App />);
+
+    fireEvent.click(screen.getByRole('button'));
+
+    await waitFor(() => {
+      screen.getByText('submitFailed');
+    });
+  });
+
+  it('should support other request type', async () => {
+    const App = () => {
+      const {
+        control,
+        formState: { isSubmitSuccessful },
+      } = useForm();
+
+      return (
+        <Form encType={'application/json'} action={'/get'} control={control}>
+          <button>Submit</button>
+          <p>{isSubmitSuccessful ? 'submitSuccessful' : 'submitFailed'}</p>
+        </Form>
+      );
+    };
+
+    render(<App />);
+
+    fireEvent.click(screen.getByRole('button'));
+
+    await waitFor(() => {
+      screen.getByText('submitSuccessful');
+    });
+  });
+
+  it('should support render props for react native', async () => {
+    const App = () => {
+      const {
+        control,
+        formState: { isSubmitSuccessful },
+      } = useForm();
+
+      return (
+        <Form
+          control={control}
+          render={({ submit }) => {
+            return (
+              <>
+                <button onClick={() => submit()}>Submit</button>
+                <p>
+                  {isSubmitSuccessful ? 'submitSuccessful' : 'submitFailed'}
+                </p>
+              </>
+            );
+          }}
+        />
+      );
+    };
+
+    render(<App />);
+
+    fireEvent.click(screen.getByRole('button'));
+
+    await waitFor(() => {
+      screen.getByText('submitSuccessful');
+    });
+  });
+
+  it('should support fetcher prop with external request', async () => {
+    const fetcher = jest.fn();
+    const App = () => {
+      const {
+        control,
+        formState: { isSubmitSuccessful },
+      } = useForm();
+
+      return (
+        <Form
+          control={control}
+          onSubmit={async () => {
+            await fetcher();
+          }}
+        >
+          <button>Submit</button>
+          <p>{isSubmitSuccessful ? 'submitSuccessful' : 'submitFailed'}</p>
+        </Form>
+      );
+    };
+
+    render(<App />);
+
+    fireEvent.click(screen.getByRole('button'));
+
+    await waitFor(() => {
+      screen.getByText('submitSuccessful');
+
+      expect(fetcher).toHaveBeenCalled();
+    });
+  });
+
+  it('should include application/json header with encType supplied', async () => {
+    const onSuccess = jest.fn();
+    const App = () => {
+      const {
+        control,
+        formState: { isSubmitSuccessful },
+      } = useForm();
+
+      return (
+        <Form
+          encType={'application/json'}
+          action={'/json'}
+          control={control}
+          onSuccess={onSuccess}
+        >
+          <button>Submit</button>
+          <p>{isSubmitSuccessful ? 'submitSuccessful' : 'submitFailed'}</p>
+        </Form>
+      );
+    };
+
+    render(<App />);
+
+    fireEvent.click(screen.getByRole('button'));
+
+    await waitFor(() => {
+      expect(onSuccess).toHaveBeenCalled();
+    });
+  });
+
+  it('should support explicit "multipart/form-data" encType', async () => {
+    const onSuccess = jest.fn();
+    const App = () => {
+      const {
+        control,
+        formState: { isSubmitSuccessful },
+      } = useForm();
+
+      return (
+        <Form
+          encType={'multipart/form-data'}
+          action={'/formData'}
+          control={control}
+          onSuccess={onSuccess}
+        >
+          <button>Submit</button>
+          <p>{isSubmitSuccessful ? 'submitSuccessful' : 'submitFailed'}</p>
+        </Form>
+      );
+    };
+
+    render(<App />);
+
+    fireEvent.click(screen.getByRole('button'));
+
+    await waitFor(() => {
+      expect(onSuccess).toHaveBeenCalled();
     });
   });
 });
