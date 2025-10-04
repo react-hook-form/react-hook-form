781827f5e1301840d956c2eb3c53d0134963bb0e
bill <bluebill1049@hotmail.com>
2025-10-04 12:16:03 +1000

    save some bytes

--- src/watch.tsx
+++ src/watch.tsx
@@ -64,7 +64,7 @@ export const Watch = <
   control,
   names,
   render,
-}: WatchProps<TFieldNames, TFieldValues, TContext, TTransformedValues>) => {
-  const values = useWatch({ control, name: names });
-  return render(values as GetValues<TFieldValues, TFieldNames>);
-};
+}: WatchProps<TFieldNames, TFieldValues, TContext, TTransformedValues>) =>
+  render(
+    useWatch({ control, name: names }) as GetValues<TFieldValues, TFieldNames>,
+  );
