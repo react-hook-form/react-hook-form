f4ee350ef3760b4b5e726d077ca7433f4c3622b1
see2ever <see2ever@gmail.com>
2025-11-18 17:46:16 +0800

    refactor: stringToPath function for improved parsing
    Keys like `a.[1.23].b` are originally intended to construct objects like `{a: {'1.23': b}}`. However, currently, `1.23` is being parsed as an array:
    
    `{a: {1: [empty* 22, b]}}`, which does not match the original intent.
    
    This PR ensures that the content inside `[]` remains unchanged and is used directly as the key.

--- src/utils/stringToPath.ts
+++ src/utils/stringToPath.ts
@@ -1,4 +1,10 @@
 import compact from './compact';
 
 export default (input: string): string[] =>
-  compact(input.replace(/["|']|\]/g, '').split(/\.|\[/));
+  compact(
+    input
+      .replace(/["']/g, '')
+      .split(/(?=\[)|\]/g)
+      .map((x) => (x.startsWith('[') ? x.slice(1) : x.split('.')))
+      .flat(),
+  );
