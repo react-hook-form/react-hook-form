e9d13a386b0a8f496da73286793f0446198d7413
bill <bluebill1049@hotmail.com>
2025-12-21 17:19:52 +1100

    revert back function

--- src/logic/getValidateError.ts
+++ src/logic/getValidateError.ts
@@ -2,20 +2,16 @@ import type { FieldError, Ref, ValidateResult } from '../types';
 import isBoolean from '../utils/isBoolean';
 import isString from '../utils/isString';
 
-export function hasError(result: ValidateResult) {
-  return (
-    isString(result) ||
-    (Array.isArray(result) && result.every(isString)) ||
-    (isBoolean(result) && !result)
-  );
-}
-
 export default function getValidateError(
   result: ValidateResult,
   ref: Ref,
   type = 'validate',
 ): FieldError | void {
-  if (hasError(result)) {
+  if (
+    isString(result) ||
+    (Array.isArray(result) && result.every(isString)) ||
+    (isBoolean(result) && !result)
+  ) {
     return {
       type,
       message: isString(result) ? result : '',
