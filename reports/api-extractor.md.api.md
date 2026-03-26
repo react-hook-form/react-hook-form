5fa275b246502b53daea0f34e080b9d24261f33d
KimJunYoung <candymask0712@gmail.com>
2026-03-27 02:08:24 +0900

    🐞 fix: add opt-in delayError option for setValue (#12992)

--- reports/api-extractor.md.api.md
+++ reports/api-extractor.md.api.md
@@ -608,6 +608,7 @@ export type SetValueConfig = Partial<{
     shouldValidate: boolean;
     shouldDirty: boolean;
     shouldTouch: boolean;
+    delayError: boolean;
 }>;
 
 // @public (undocumented)
@@ -1009,7 +1010,7 @@ export type WatchValue<TFieldName, TFieldValues extends FieldValues = FieldValue
 
 // Warnings were encountered during analysis:
 //
-// src/types/form.ts:509:3 - (ae-forgotten-export) The symbol "Subscription" needs to be exported by the entry point index.d.ts
+// src/types/form.ts:510:3 - (ae-forgotten-export) The symbol "Subscription" needs to be exported by the entry point index.d.ts
 
 // (No @packageDocumentation comment for this package)
 
--- src/logic/createFormControl.ts
+++ src/logic/createFormControl.ts
@@ -31,6 +31,7 @@ import type {
   SetFieldValue,
   SetValueConfig,
   Subjects,
+  TriggerConfig,
   UseFormClearErrors,
   UseFormGetFieldState,
   UseFormGetValues,
@@ -757,7 +758,13 @@ export function createFormControl<
         true,
       );
 
-    options.shouldValidate && trigger(name as Path<TFieldValues>);
+    options.shouldValidate &&
+      trigger(
+        name as Path<TFieldValues>,
+        {
+          delayError: options.delayError,
+        } as TriggerConfig & { delayError?: boolean },
+      );
   };
 
   const setValues = <
@@ -1002,7 +1009,10 @@ export function createFormControl<
     return;
   };
 
-  const trigger: UseFormTrigger<TFieldValues> = async (name, options = {}) => {
+  const trigger: UseFormTrigger<TFieldValues> = async (
+    name,
+    options: TriggerConfig & { delayError?: boolean } = {},
+  ) => {
     let isValid;
     let validationResult;
     const fieldNames = convertToArrayPayload(name) as InternalFieldName[];
@@ -1035,6 +1045,18 @@ export function createFormControl<
         name,
         eventType: EVENTS.TRIGGER,
       });
+    }
+
+    if (options.delayError && _options.delayError && isString(name)) {
+      const error = get(_formState.errors, name);
+      if (error) {
+        unset(_formState.errors, name);
+        delayErrorCallback = debounce(() => updateErrors(name, error));
+        delayErrorCallback(_options.delayError);
+      } else {
+        clearTimeout(timer);
+        delayErrorCallback = null;
+      }
     }
 
     _subjects.state.next({
