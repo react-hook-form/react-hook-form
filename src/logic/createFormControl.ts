d256aeb87841c13ecd06250ea182f644ed931dbe
Aponia <ap0nia.dev@gmail.com>
2026-04-04 15:21:04 -0700

    🚨 fix: safely access field._f during register  (#13365)
    * fix: 🐛 safely access field._f during register
    
    * test: 🚨 can register array property before array root
    
    ---------
    
    Co-authored-by: Aponia <aponia@Elysias-MacBook-Pro.local>

--- src/logic/createFormControl.ts
+++ src/logic/createFormControl.ts
@@ -1277,7 +1277,7 @@ export function createFormControl<
     const disabledIsDefined =
       isBoolean(options.disabled) || isBoolean(_options.disabled);
     const shouldRevalidateRemount =
-      !_names.registerName.has(name) && field && !field._f.mount;
+      !_names.registerName.has(name) && field && field._f && !field._f.mount;
 
     set(_fields, name, {
       ...(field || {}),
