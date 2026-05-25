478004028687b51630bd4755616dfa417bcad5b6
Kai Tanaka <275430420+quyentonndbs@users.noreply.github.com>
2026-05-25 06:42:35 +0000

    chore: improve react-hook-form maintenance path

--- src/__tests__/utils/compact.test.ts
+++ src/__tests__/utils/compact.test.ts
@@ -8,4 +8,13 @@ describe('filterOutFalsy', () => {
     expect(filterOutFalsy([1, 2, undefined, 4])).toEqual([1, 2, 4]);
     expect(filterOutFalsy([0, 1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
   });
+
+  it('should correctly handle sparse arrays', () => {
+    const sparse = [1, , undefined, 2];
+    expect(filterOutFalsy(sparse)).toEqual([1, 2]);
+  });
+
+  it('should return an empty array when all values are falsy', () => {
+    expect(filterOutFalsy([0, '', null, false, NaN, undefined])).toEqual([]);
+  });
 });
