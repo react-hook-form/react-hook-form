1e2bde46fc6d68e6ecea10102b1cdd144c4e4b45
bill <bluebill1049@hotmail.com>
2026-07-18 17:19:33 +1000

    update app

--- app/src/changeInputType.tsx
+++ app/src/changeInputType.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 import { useForm, ValidationMode } from 'react-hook-form';
 
 const ChangeInputType = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    getValues,
-    resetField,
-  } = useForm<{
+  const { register, handleSubmit, watch, getValues, resetField } = useForm<{
     type: string;
     value: string | number | boolean;
   }>({
@@ -17,18 +11,18 @@ const ChangeInputType = () => {
       value: 'hello',
     },
   });
-  
-  const type = watch("type");
-  
+
+  const type = watch('type');
+
   React.useEffect(() => {
-      const typeToValue = {
-          text: "hello",
-          number: 42,
-          checkbox: true,
-      }
-      resetField("value", {
-          defaultValue: typeToValue[type] ?? "unknown",
-      });
+    const typeToValue = {
+      text: 'hello',
+      number: 42,
+      checkbox: true,
+    };
+    resetField('value', {
+      defaultValue: typeToValue[type] ?? 'unknown',
+    });
   }, [type]);
 
   return (
@@ -37,18 +31,12 @@ const ChangeInputType = () => {
         console.log(d);
       })}
     >
-      <input
-        {...register('type', { required: true })}
-        placeholder="type"
-      />
-      <input
-        {...register('value', { required: true })}
-        type={type}
-      />
+      <input {...register('type', { required: true })} placeholder="type" />
+      <input {...register('value', { required: true })} type={type} />
       <div id="state">
         {JSON.stringify({
           type,
-          value: getValues("value"),
+          value: getValues('value'),
         })}
       </div>
     </form>
