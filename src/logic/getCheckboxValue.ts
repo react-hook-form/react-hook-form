5606794c4ccb63e9aebd9b1a05c24cdf5f168220
bill <bluebill1049@hotmail.com>
2026-08-15 09:41:37 +1000

    fix typescript error ignore on getCheckboxValues

--- src/logic/getCheckboxValue.ts
+++ src/logic/getCheckboxValue.ts
@@ -13,23 +13,29 @@ const defaultResult: CheckboxFieldResult = {
 const validResult = { value: true, isValid: true };
 
 export default (options?: HTMLInputElement[]): CheckboxFieldResult => {
-  if (Array.isArray(options)) {
-    if (options.length > 1) {
-      const values = options
-        .filter((option) => option && option.checked && !option.disabled)
-        .map((option) => option.value);
-      return { value: values, isValid: !!values.length };
-    }
-
-    return options[0].checked && !options[0].disabled
-      ? // @ts-expect-error expected to work in the browser
-        options[0].attributes && !isUndefined(options[0].attributes.value)
-        ? isUndefined(options[0].value) || options[0].value === ''
-          ? validResult
-          : { value: options[0].value, isValid: true }
-        : validResult
-      : defaultResult;
+  if (!Array.isArray(options)) {
+    return defaultResult;
   }
 
-  return defaultResult;
+  if (options.length > 1) {
+    const values = options
+      .filter((option) => option && option.checked && !option.disabled)
+      .map((option) => option.value);
+
+    return { value: values, isValid: !!values.length };
+  }
+
+  const option = options[0];
+
+  if (!option || !option.checked || option.disabled) {
+    return defaultResult;
+  }
+
+  if (!option.hasAttribute('value')) {
+    return validResult;
+  }
+
+  return isUndefined(option.value) || option.value === ''
+    ? validResult
+    : { value: option.value, isValid: true };
 };
