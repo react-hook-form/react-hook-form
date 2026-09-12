5804e5b1c2084940be3e04a2929027a3d4e9ec65
soroush5 <soroush5@users.noreply.github.com>
2026-09-12 22:30:09 +0330

    style: single-line type import in remove test

--- src/__tests__/useFieldArray/remove.test.tsx
+++ src/__tests__/useFieldArray/remove.test.tsx
@@ -10,11 +10,7 @@ import {
 
 import { VALIDATION_MODE } from '../../constants';
 import { Controller } from '../../controller';
-import type {
-  Control,
-  DeepMap,
-  FieldError,
-} from '../../types';
+import type { Control, DeepMap, FieldError } from '../../types';
 import { useController } from '../../useController';
 import { useFieldArray } from '../../useFieldArray';
 import { useForm } from '../../useForm';
