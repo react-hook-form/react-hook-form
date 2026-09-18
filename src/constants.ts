a33c64a47ea9520b44556665ff6748f61c0432a1
bill <bluebill1049@hotmail.com>
2026-09-18 21:24:03 +1000

    fix

--- src/constants.ts
+++ src/constants.ts
@@ -1,59 +1,38 @@
-const blur = 'blur';
-const focusout = 'focusout';
-const change = 'change';
-const submit = 'submit';
-const trigger = 'trigger';
-const valid = 'valid';
-
 export const EVENTS = {
-  BLUR: blur,
-  FOCUS_OUT: focusout,
-  CHANGE: change,
-  SUBMIT: submit,
-  TRIGGER: trigger,
-  VALID: valid,
+  BLUR: 'blur',
+  FOCUS_OUT: 'focusout',
+  CHANGE: 'change',
+  SUBMIT: 'submit',
+  TRIGGER: 'trigger',
+  VALID: 'valid',
 } as const;
-
-const onBlur = 'onBlur';
-const onChange = 'onChange';
-const onSubmit = 'onSubmit';
-const onTouched = 'onTouched';
-const all = 'all';
 
 export const VALIDATION_MODE = {
-  onBlur,
-  onChange,
-  onSubmit,
-  onTouched,
-  all,
+  onBlur: 'onBlur',
+  onChange: 'onChange',
+  onSubmit: 'onSubmit',
+  onTouched: 'onTouched',
+  all: 'all',
 } as const;
 
-const max = 'max';
-const min = 'min';
-const maxLength = 'maxLength';
-const minLength = 'minLength';
-const pattern = 'pattern';
-const required = 'required';
-const validate = 'validate';
-
 export const INPUT_VALIDATION_RULES = {
-  max,
-  min,
-  maxLength,
-  minLength,
-  pattern,
-  required,
-  validate,
+  max: 'max',
+  min: 'min',
+  maxLength: 'maxLength',
+  minLength: 'minLength',
+  pattern: 'pattern',
+  required: 'required',
+  validate: 'validate',
 } as const;
 
 export const REGISTER_VALIDATION_RULES = [
-  required,
-  min,
-  max,
-  minLength,
-  maxLength,
-  pattern,
-  validate,
+  INPUT_VALIDATION_RULES.required,
+  INPUT_VALIDATION_RULES.min,
+  INPUT_VALIDATION_RULES.max,
+  INPUT_VALIDATION_RULES.minLength,
+  INPUT_VALIDATION_RULES.maxLength,
+  INPUT_VALIDATION_RULES.pattern,
+  INPUT_VALIDATION_RULES.validate,
 ] as const;
 
 export const FORM_ERROR_TYPE = 'form';
